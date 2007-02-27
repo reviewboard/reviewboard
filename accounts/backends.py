389f3e4314811fffc1ff01b66ebc6eed0248f304
David Trowbridge <trowbrds@gmail.com>
2007-02-27 07:13:59 +0000

    When logging in a user over NIS, create a User object if one doesn't already exist, and populate its information

--- accounts/backends.py
+++ accounts/backends.py
@@ -12,11 +12,26 @@ class NISBackend:
         user = None
 
         try:
-            original_crypted = nis.match(username, 'passwd').split(":")[1]
+            passwd = nis.match(username, 'passwd').split(':')
+            original_crypted = passwd[1]
             new_crypted = crypt.crypt(password, original_crypted[:2])
 
             if original_crypted == new_crypted:
-                user = User.objects.get(username=username)
+                try:
+                    user = User.objects.get(username=username)
+                except User.DoesNotExist:
+                    # Create a new user.
+                    first_name, last_name = passwd[4].split(' ', 2)
+                    email = '%s@%s' % (username, settings.NIS_EMAIL_DOMAIN)
+
+                    user = User(username=username,
+                                password='',
+                                first_name=first_name,
+                                last_name=last_name,
+                                email=email)
+                    user.is_staff = False
+                    user.is_superuser = False
+                    user.save()
 
         except nis.error:
             pass
