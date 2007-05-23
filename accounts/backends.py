74beb343f4a8c92816859b155e5187ab2e478fc2
David Trowbridge <trowbrds@gmail.com>
2007-05-23 18:07:57 +0000

    Patch by Patrick Farrell to add an LDAP backend for authentication.
    Reviewed at http://reviewboard.chipx86.com/r/16/

--- accounts/backends.py
+++ accounts/backends.py
@@ -48,3 +48,63 @@ class NISBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+class LDAPBackend:
+    """
+    Authenticate against a user on an LDAP server.
+    """
+
+    def authenticate(self, username, password):
+        try:
+            import ldap
+            ldapo = ldap.initialize(settings.LDAP_URI)
+            ldapo.set_option(ldap.OPT_PROTOCOL_VERSION, '3')
+            ldapo.simple_bind_s(settings.LDAP_UID_MASK % username, password)
+
+            return self.get_or_create_user(username)
+
+        except ImportError:
+            pass
+        except ldap.INVALID_CREDENTIALS:
+            pass
+
+    def get_or_create_user(self, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            try:
+                import ldap
+                ldapo = ldap.initialize(settings.LDAP_URI)
+                ldapo.set_option(ldap.OPT_PROTOCOL_VERSION, '3')
+                ldapo.simple_bind_s(settings.LDAP_ANON_BIND_UID, settings.LDAP_ANON_BIND_PASSWD)
+
+                passwd = ldapo.search_s(settings.LDAP_UID_MASK % username,
+                                        ldap.SCOPE_SUBTREE, "objectclass=*")
+
+                first_name, last_name = passwd[0][1]['cn'][0].split(' ', 1)
+                email = '%s@%s' % (username, settings.LDAP_EMAIL_DOMAIN)
+
+                user = User(username=username,
+                            password='',
+                            first_name=first_name,
+                            last_name=last_name,
+                            email=email)
+                user.is_staff = False
+                user.is_superuser = False
+                user.save()
+            except ImportError:
+                pass
+            except ldap.INVALID_CREDENTIALS:
+                # I'd really like to warn the user that their ANON_BIND_UID
+                # and ANON_BIND_PASSWD are wrong, but I don't know how
+                pass
+            except ldap.LDAPError:
+                pass
+        return user
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
