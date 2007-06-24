43a67ee78c2fce7a4580bf200f25fb4f2bedf52f
Christian Hammond <chipx86@chipx86.com>
2007-06-24 08:14:00 +0000

    Added improved fixtures for unit testing. We now have a variety of users, groups, review requests, reviews, comments, repositories and SCM Tools to test against, broken up into various fixture files. Existing e-mail unit tests have been updated appropriately. Details will go up on the wiki later.

--- reviews/tests.py
+++ reviews/tests.py
@@ -18,7 +18,7 @@ from reviewboard.reviews.models import Group, ReviewRequest, Review
 
 class EmailTests(TestCase):
     """Tests the e-mail support."""
-    fixtures = ['email_test']
+    fixtures = ['test_users', 'test_reviewrequests', 'test_scmtools']
 
     def setUp(self):
         settings.SEND_REVIEW_MAIL = True
@@ -26,64 +26,59 @@ class EmailTests(TestCase):
 
     def testNewReviewRequestEmail(self):
         """Testing sending an e-mail when creating a new review request."""
-        review_request = ReviewRequest.objects.get(submitter__username="doc")
+        review_request = ReviewRequest.objects.get(
+            summary="Made e-mail improvements")
         mail_review_request(review_request.submitter, review_request)
 
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject,
-                         "Review Request: Make cleaned data work with " +
-                         "older and newer newforms")
-        self.assertValidRecipients(["grumpy", "doc"], ["reviewboard"])
+                         "Review Request: Made e-mail improvements")
+        self.assertValidRecipients(["grumpy", "doc"], [])
 
     def testReviewEmail(self):
         """Testing sending an e-mail when replying to a review request."""
-        review = Review.objects.get(pk=1)
-        self.assertEqual(review.user.username, "grumpy")
+        review_request = ReviewRequest.objects.get(
+            summary="Add permission checking for JSON API")
+        review = Review.objects.get(review_request=review_request,
+                                    user__username="doc",
+                                    base_reply_to__isnull=True)
+        self.assertEqual(review.body_top, "Test")
         mail_review(review.user, review)
 
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject,
-                         "Re: Review Request: Make cleaned data work with " +
-                         "older and newer newforms")
-        self.assertValidRecipients(["grumpy", "doc"], ["reviewboard"])
+                         "Re: Review Request: Add permission checking " +
+                         "for JSON API")
+        self.assertValidRecipients(["admin", "doc", "dopey", "grumpy"], [])
 
     def testReviewReplyEmail(self):
         """Testing sending an e-mail when replying to a review."""
-        base_review = Review.objects.get(pk=1)
+        review_request = ReviewRequest.objects.get(
+            summary="Add permission checking for JSON API")
+        base_review = Review.objects.get(review_request=review_request,
+                                         user__username="doc",
+                                         base_reply_to__isnull=True)
         reply = Review.objects.get(base_reply_to=base_review,
                                    user__username="dopey")
         mail_reply(reply.user, reply)
 
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject,
-                         "Re: Review Request: Make cleaned data work with " +
-                         "older and newer newforms")
-        self.assertValidRecipients(["grumpy", "doc", "dopey"], ["reviewboard"])
+                         "Re: Review Request: Add permission checking " +
+                         "for JSON API")
+        self.assertValidRecipients(["admin", "doc", "dopey", "admin"], [])
 
     def testUpdateReviewRequestEmail(self):
         """Testing sending an e-mail when updating a review request."""
-        review_request = ReviewRequest.objects.get(submitter__username="doc")
+        review_request = ReviewRequest.objects.get(
+            summary="Update for cleaned_data changes")
         review_request.email_message_id = "junk"
         mail_review_request(review_request.submitter, review_request)
 
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject,
-                         "Re: Review Request: Make cleaned data work with " +
-                         "older and newer newforms")
-        self.assertValidRecipients(["grumpy", "doc", "dopey"], ["reviewboard"])
-
-    def testDiffUpdateEmail(self):
-        """Testing sending an e-mail when replying to a review."""
-        base_review = Review.objects.get(pk=1)
-        reply = Review.objects.get(base_reply_to=base_review,
-                                   user__username="dopey")
-        mail_reply(reply.user, reply)
-
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject,
-                         "Re: Review Request: Make cleaned data work with " +
-                         "older and newer newforms")
-        self.assertValidRecipients(["grumpy", "doc", "dopey"], ["reviewboard"])
+                         "Re: Review Request: Update for cleaned_data changes")
+        self.assertValidRecipients(["dopey", "doc"], ["devgroup"])
 
     def assertValidRecipients(self, user_list, group_list):
         recipient_list = mail.outbox[0].to
@@ -91,11 +86,13 @@ class EmailTests(TestCase):
 
         for user in user_list:
             self.assert_(get_email_address_for_user(
-                User.objects.get(username=user)) in recipient_list)
+                User.objects.get(username=user)) in recipient_list,
+                "user %s was not found in the recipient list" % user)
 
         for group in Group.objects.filter(name__in=group_list):
             for address in get_email_addresses_for_group(group):
-                self.assert_(address in recipient_list)
+                self.assert_(address in recipient_list,
+                    "group %s was not found in the recipient list" % address)
 
 
 class CondenseTagTest(TagTest):
