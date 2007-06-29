266d3b6ea4e4df5fdde048d55bd93d6dda77cb5d
David Trowbridge <trowbrds@gmail.com>
2007-06-29 08:38:09 +0000

    Add useful little scripts for profiling

---
+++ contrib/profiling/print_info.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import pstats
+import sys
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Usage: %s [file.prof]" % sys.argv[0])
+    sys.exit(1)
+
+s = pstats.Stats(sys.argv[1])
+s.sort_stats('time')
+
+print '==== Largest 10% ===='
+s.print_stats(.1)
+
+print '==== Largest 1% of Callers ===='
+s.print_callers(.01)
