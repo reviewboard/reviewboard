d9684c0d9ffb1e303ef2149bba84984752f9ff8e
David Trowbridge <trowbrds@gmail.com>
2008-01-26 04:58:41 +0000

    Patch from Jonathan Ellis to add e-mail and password fields to the account preferences when running with BUILTIN_AUTH.
    Reviewed at http://reviews.review-board.org/r/205/

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,6 @@
 from django import newforms as forms
+from django.conf import settings
+from django.newforms import widgets
 from django.utils.translation import ugettext as _
 
 from reviewboard.reviews.models import Group
@@ -8,12 +10,22 @@ class PreferencesForm(forms.Form):
     redirect_to = forms.CharField(required=False, widget=forms.HiddenInput)
     groups = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
                                        required=False)
-    first_name = forms.CharField(required=False)
-    last_name = forms.CharField(required=False)
     syntax_highlighting = forms.BooleanField(required=False,
         label=_("Enable syntax highlighting in the diff viewer"))
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+    email = forms.EmailField(required=settings.BUILTIN_AUTH)
+    password1 = forms.CharField(required=False, widget=widgets.PasswordInput())
+    password2 = forms.CharField(required=False, widget=widgets.PasswordInput())
 
     def __init__(self, *args, **kwargs):
         forms.Form.__init__(self, *args, **kwargs)
         self.fields['groups'].choices = \
             [(g.id, g.display_name) for g in Group.objects.all()]
+
+    def clean_password2(self):
+        p1 = self.cleaned_data['password1']
+        p2 = self.cleaned_data['password2']
+        if p1 != p2:
+            raise forms.ValidationError('passwords do not match')
+        return p2
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
+import time
+from sha import sha
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.models import get_hexdigest
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
@@ -22,9 +25,17 @@ def user_preferences(request, template_name='accounts/prefs.html'):
         form = PreferencesForm(request.POST)
 
         if form.is_valid():
+            if settings.BUILTIN_AUTH:
+                if form.cleaned_data['password1']:
+                    salt = sha(str(time.time())).hexdigest()[:5]
+                    hash = sha(salt + form.cleaned_data['password1'])
+                    newpassword = 'sha1$%s$%s' % (salt, hash.hexdigest())
+                    request.user.password = newpassword
+
+                request.user.first_name = form.cleaned_data['first_name']
+                request.user.last_name = form.cleaned_data['last_name']
+                request.user.email = form.cleaned_data['email']
             request.user.group_set = form.cleaned_data['groups']
-            request.user.first_name = form.cleaned_data['first_name']
-            request.user.last_name = form.cleaned_data['last_name']
             request.user.save()
 
             profile.first_time_setup_done = True
@@ -36,9 +47,11 @@ def user_preferences(request, template_name='accounts/prefs.html'):
 
     else:
         form = PreferencesForm({
+            'settings': settings,
             'redirect_to': redirect_to,
             'first_name': request.user.first_name,
             'last_name': request.user.last_name,
+            'email': request.user.email,
             'syntax_highlighting': profile.syntax_highlighting,
             'groups': [g.id for g in request.user.group_set.all()],
         })
