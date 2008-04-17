94b7fef5f18bdc89472c713e6602401ea6d0f419
Christian Hammond <chipx86@chipx86.com>
2008-04-17 08:18:27 +0000

    Add unit tests for reports to make sure the pages load. We should probably verify output later, but at least we'll catch some breakages.

---
+++ reports/tests.py
@@ -0,0 +1,51 @@
+import os
+import unittest
+
+from django.conf import settings
+from django.test import TestCase
+
+
+class ViewTests(TestCase):
+    """Tests for views in reviewboard.reports.views"""
+    fixtures = ['test_users', 'test_reviewrequests', 'test_scmtools']
+
+    def setUp(self):
+        settings.REQUIRE_SITEWIDE_LOGIN = False
+
+    def testReviewRequestReport(self):
+        """Testing review_request report"""
+        response = self.client.get("/reports/admin/review_request/moinmoin/")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get("/reports/admin/review_request/text/")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get("/reports/admin/review_request/abc/")
+        self.assertEqual(response.status_code, 404)
+
+    def testReviewReport(self):
+        """Testing review report"""
+        response = self.client.get("/reports/admin/review/moinmoin/")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get("/reports/admin/review/text/")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get("/reports/admin/review/abc/")
+        self.assertEqual(response.status_code, 404)
+
+    def testStatusReport(self):
+        """Testing status_report"""
+        response = self.client.get("/reports/admin/status_report/moinmoin/")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get("/reports/admin/status_report/text/")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get("/reports/admin/status_report/abc/")
+        self.assertEqual(response.status_code, 404)
+
+    def testReportList(self):
+        """Testing report_list"""
+        response = self.client.get("/reports/")
+        self.assertEqual(response.status_code, 200)
