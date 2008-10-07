1b9e912d6ecf5322540d7ec79f8a6bd8a90f6192
David Trowbridge <trowbrds@gmail.com>
2008-10-07 07:42:21 +0000

    Work around the lack of urllib2.quote in Python 2.4
    urllib2.quote was introduced in Python 2.5. Before then, people had to use
    urllib.quote. The Hg SCMTool was using urllib2. This change fixes it to try
    urllib2, then default to urllib if quote doesn't exist.
    
    This is based off a patch from Benoît Allard at
    http://reviews.review-board.org/r/550/
    
    Fixes bug 653.

--- scmtools/hg.py
+++ scmtools/hg.py
@@ -1,4 +1,8 @@
 import urllib2
+try:
+    from urllib2 import quote as urllib_quote
+except ImportError:
+    from urllib import quote as urllib_quote
 
 from reviewboard.diffviewer.parser import DiffParser, DiffParserError
 from reviewboard.scmtools.core import \
@@ -99,7 +103,7 @@ class HgWebClient:
             authhandler = urllib2.HTTPBasicAuthHandler(passman)
             opener = urllib2.build_opener(authhandler)
             f = opener.open('%s/raw-file/%s/%s' %
-                            (self.url, rev, urllib2.quote(path)))
+                            (self.url, rev, urllib_quote(path)))
             return f.read()
         except Exception, e:
             raise FileNotFoundError(path, rev, str(e))
