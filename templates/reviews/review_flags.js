603dbc81d639391a8c873a8e1096d661e814448b
Christian Hammond <chipx86@chipx86.com>
2009-02-06 02:48:37 +0000

    Fix a bug where the review request summary shown in the Review dialog was being HTML-escaped but really needed to be JavaScript-escaped.
    Fixes bug #860.

--- templates/reviews/review_flags.js
+++ templates/reviews/review_flags.js
@@ -7,7 +7,7 @@
   var gBugTrackerURL = "{{review_request.repository.bug_tracker}}";
   var gReviewRequestPath = '{{review_request.get_absolute_url}}';
   var gReviewRequestId = "{{review_request.id}}";
-  var gReviewRequestSummary = "{{review_request.summary}}";
+  var gReviewRequestSummary = "{{review_request.summary|escapejs}}";
   var gReviewPending = {% if review %}true{% else %}false{% endif %};
 {% ifuserorperm review_request.submitter "reviews.can_edit_reviewrequest" %}
 {% ifequal review_request.status 'P' %}
