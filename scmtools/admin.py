7470103b1f2e9e3e2c8fd3269b9f7a519bbca79b
Christian Hammond <chipx86@chipx86.com>
2009-05-10 10:25:58 +0000

    Improve the repository form in the administration UI.
    This change improves the repository form by separating the fields into
    fieldsets, adding help text when appropriate, increasing the width of
    the fields, and marking the password field as being a password field
    (masking the typed text).
    
    Thanks to Sharan Rao for providing a version of this patch as well! We
    seem to have fixed bug #942 at the same time.
    
    Fixes bug #942

--- scmtools/admin.py
+++ scmtools/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
+from reviewboard.scmtools.forms import RepositoryForm
 from reviewboard.scmtools.models import Repository, Tool
 
 
 class RepositoryAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'path')
+    fieldsets = (
+        (_('General Information'), {
+            'fields': ('name', 'path', 'mirror_path', 'tool', 'bug_tracker'),
+            'classes': ('wide',),
+        }),
+        (_('Authentication'), {
+            'fields': ('username', 'password'),
+        }),
+        (_('Advanced'), {
+            'fields': ('encoding',),
+        }),
+    )
+    form = RepositoryForm
 
 
 class ToolAdmin(admin.ModelAdmin):
