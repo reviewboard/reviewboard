b05c89b8281ccb09068c36b570832fd37bedb1ec
Christian Hammond <chipx86@chipx86.com>
2009-06-21 00:14:45 +0000

    Migrate the existing release notes from NEWS into docs/releasenotes/reviewboard/.
    This will be the new home for release notes in the future. This has the
    advantage of producing organized, human-readable release notes that will
    be automatically published to the web.
    
    NEWS has been updated to point to the new location.

---
+++ docs/releasenotes/_ext/extralinks.py
@@ -0,0 +1,37 @@
+"""
+Sphinx plugins for special links in the Release Notes.
+"""
+from docutils import nodes, utils
+
+
+def setup(app):
+    app.add_config_value('bugtracker_url', '', True)
+    app.add_role('bug', bug_role)
+
+
+def bug_role(role, rawtext, text, linenum, inliner, options={}, content=[]):
+    try:
+        bugnum = int(text)
+        if bugnum <= 0:
+            raise ValueError
+    except ValueError:
+        msg = inliner.reporter.error(
+            'Bug number must be a number greater than or equal to 1; '
+            '"%s" is invalid.' % text,
+            line=linenum)
+        prb = inliner.problematic(rawtext, rawtext, msg)
+        return [prb], [msg]
+
+    bugtracker_url = inliner.document.settings.env.config.bugtracker_url
+
+    if not bugtracker_url or not '%s' in bugtracker_url:
+        msg = inliner.reporter.error('bugtracker_url must be configured.',
+                                     line=linenum)
+        prb = inliner.problematic(rawtext, rawtext, msg)
+        return [prb], [msg]
+
+    ref = bugtracker_url % bugnum
+    node = nodes.reference(rawtext, 'Bug #' + utils.unescape(text),
+                           refuri=ref, **options)
+
+    return [node], []
