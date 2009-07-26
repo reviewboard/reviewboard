d6337dd1edc5b31599ae7000d6c15648296114d6
David Trowbridge <trowbrds@gmail.com>
2009-07-26 21:52:50 +0000

    Fix git and mtn executable checks.
    Copy and paste error -- we were checking for cvs for both git and mtn. Patch
    from Dan Savilonis.
    
    Reviewed at http://reviews.review-board.org/r/934/diff/

--- scmtools/mtn.py
+++ scmtools/mtn.py
@@ -67,7 +67,7 @@ class MonotoneDiffParser(DiffParser):
 
 class MonotoneClient:
     def __init__(self, path):
-        if not is_exe_in_path('cvs'):
+        if not is_exe_in_path('mtn'):
             # This is technically not the right kind of error, but it's the
             # pattern we use with all the other tools.
             raise ImportError
