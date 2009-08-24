a57e5d9d085b30c7c114a3787f6d6cda47d2f878
Christian Hammond <chipx86@chipx86.com>
2009-08-24 01:02:36 -0700

    Move the Review Board app directories and source code into a reviewboard/ directory.
    There's a few important changes for developers. manage.py and devserver.sh
    have moved. You can now find manage.py in the reviewboard/ directory, and
    devserver.sh in the contrib/internal/ directory. settings_local.py.tmpl has
    also moved to contrib/conf/, though the custom settings_local.py should still
    be placed in the top-level directory.
    
    As a help to new developers, devserver.sh now provides setup instructions for
    getting a tree ready, and checks to make sure everything is in place before
    calling off to Django.

--- devserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./manage.py runserver 0.0.0.0:8080 --adminmedia=htdocs/media/admin/
--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ from setuptools.command.build_py import build_py
 from distutils.command.install_data import install_data
 from distutils.command.install import INSTALL_SCHEMES
 
+from reviewboard import get_package_version, is_release
+
 
 # Make sure we're actually in the directory containing setup.py.
 root_dir = os.path.dirname(__file__)
@@ -30,37 +32,6 @@ if root_dir != "":
 # http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
-
-
-class fixed_build_py(build_py):
-    """
-    A version of setuptools's build_py that works around a bug when the
-    package dir is an empty string. While distutils documents that an empty
-    string should be used when the package contents are in the same directory
-    as setup.py, setuptools has a bug that breaks with this when installing
-    package data from that directory.
-
-    We temporarily set the package directory to "." so that _get_data_files
-    can extract the path without breaking.
-    """
-    def __init__(self, *args, **kwargs):
-        build_py.__init__(self, *args, **kwargs)
-        self.workaround_pkgdir = False
-
-    def get_package_dir(self, package):
-        pkg_dir = build_py.get_package_dir(self, package)
-
-        if self.workaround_pkgdir and pkg_dir == "":
-            return "."
-
-        return pkg_dir
-
-    def _get_data_files(self):
-        self.workaround_pkgdir = True
-        results = build_py._get_data_files(self)
-        self.workaround_pkgdir = False
-
-        return results
 
 
 class osx_install_data(install_data):
@@ -85,52 +56,6 @@ else:
     cmdclasses = {'install_data': install_data}
 
 
-# We only want to do this if it's not an sdist build, which means there will
-# be a PKG-INFO.
-if not os.path.exists("PKG-INFO"):
-    if os.path.exists("reviewboard"):
-        shutil.rmtree("reviewboard")
-
-    print "Copying tree to staging area..."
-    shutil.copytree(".", "reviewboard", True)
-
-    # Clean up things that shouldn't be in there...
-    shutil.rmtree("reviewboard/ReviewBoard.egg-info", ignore_errors=True)
-    shutil.rmtree("reviewboard/build", ignore_errors=True)
-    shutil.rmtree("reviewboard/dist", ignore_errors=True)
-    os.unlink("reviewboard/setup.py")
-    os.unlink("reviewboard/ez_setup.py")
-
-    if os.path.exists("reviewboard/settings_local.py"):
-        os.unlink("reviewboard/settings_local.py")
-
-    if os.path.exists("reviewboard/settings_local.pyc"):
-        os.unlink("reviewboard/settings_local.pyc")
-
-# Since we don't actually keep our directories in a reviewboard directory
-# like we really should, we have to fake it. Prepend "reviewboard." here,
-# set package_dir below, and make sure to exclude our svn:externals
-# dependencies.
-rb_dirs = []
-
-for dirname in os.listdir("."):
-    if os.path.isdir(dirname) and dirname != "reviewboard":
-        rb_dirs.append(dirname)
-        rb_dirs.append(dirname + ".*")
-
-packages = [
-    package_name
-    for package_name in find_packages(
-        exclude=["reviewboard.djblets",          "reviewboard.djblets.*",
-                 "reviewboard.django_evolution", "reviewboard.django_evolution.*"] +
-                rb_dirs)
-]
-
-
-# Import this now, since reviewboard is in the right place now.
-from reviewboard import get_package_version, is_release
-
-
 if is_release():
     download_url = "http://downloads.review-board.org/releases/"
 else:
@@ -148,7 +73,7 @@ setup(name="ReviewBoard",
       author_email="reviewboard@googlegroups.com",
       maintainer="Christian Hammond",
       maintainer_email="chipx86@chipx86.com",
-      packages=packages,
+      packages=find_packages(),
       entry_points = {
           'console_scripts': [
               'rb-site = reviewboard.cmdline.rbsite:main',
