de45e1d1cc8dd8b7eaa313ebdd94cfb308a1f68a
David Trowbridge <trowbrds@gmail.com>
2009-08-24 22:55:48 -0700

    Make the login box prettier
    One of the areas we've long ignored is our login and settings pages. This change
    is the first small step towards a revamp. This isn't super pretty yet because I
    suck at making web pages pretty right now, but it's an improvement in the
    design.

--- reviewboard/accounts/urls.py
+++ reviewboard/accounts/urls.py
@@ -4,12 +4,12 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns("reviewboard.accounts.views",
     url(r'^register/$', 'account_register', name="register"),
-    url(r'^preferences/$', 'user_preferences',
-        name="user-preferences"),
+    url(r'^preferences/$', 'user_preferences', name="user-preferences"),
 )
 
 urlpatterns += patterns("djblets.auth.views",
-    url(r'^login/$', 'login',
+    url(r'^login/$',
+        'login',
         {'next_page': settings.SITE_ROOT + 'dashboard/'},
         name="login"),
 )
