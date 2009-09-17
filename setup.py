9fd92875a26b1075df7dacf6f5aea9a15cdf6b43
Christian Hammond <chipx86@chipx86.com>
2009-09-17 13:47:50 -0700

    New CSS fixes need Djblets 0.5.2.

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(name=PACKAGE_NAME,
       install_requires=[
           'Django>=1.1',
           'django_evolution',
-          'Djblets>=0.5.2',
+          'Djblets>=0.5.3',
           'Pygments>=0.10',
           'flup',
           'pytz'
