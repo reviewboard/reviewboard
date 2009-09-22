4c2796e4ffd08dfbc3f366d7a0aa1a8a653105b3
Christian Hammond <chipx86@chipx86.com>
2009-09-21 23:02:33 -0700

    Bump to 1.1 alpha 2 dev.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -4,7 +4,7 @@
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (1, 1, 0, 'alpha', 1, True)
+VERSION = (1, 1, 0, 'alpha', 2, False)
 
 
 def get_version_string():
