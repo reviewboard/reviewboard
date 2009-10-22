995acb10550a3194dbbae2704b5a22dbfe07707a
Christian Hammond <chipx86@chipx86.com>
2009-10-22 01:50:34 -0700

    Release Review Board 1.0.5.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -4,7 +4,7 @@
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (1, 0, 5, 'alpha', 0, False)
+VERSION = (1, 0, 5, 'final', 0, True)
 
 
 def get_version_string():
