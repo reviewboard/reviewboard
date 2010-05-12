b1b52444242bdedbec36518a8b16bbd2933dc3bf
Christian Hammond <chipx86@chipx86.com>
2010-05-12 00:30:34 -0700

    Release Review Board 1.0.8.

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(name=PACKAGE_NAME,
       install_requires=[
           'Django>=1.1.1',
           'django_evolution',
-          'Djblets>=0.5.8',
+          'Djblets>=0.6.0',
           'Pygments>=0.10',
           'flup',
           'pytz'
