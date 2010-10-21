ea630b59ed8aa6c92ad871b3d542d4d1c43d4e41
Christian Hammond <chipx86@chipx86.com>
2010-10-21 01:43:42 -0700

    Fix a broken database evolution with the new LocalSite code.
    The Repository.local_site evolution was mistakenly referring
    to Repository.site, breaking the evolution. This has been
    fixed to use the right name.

--- reviewboard/scmtools/evolutions/localsite.py
+++ reviewboard/scmtools/evolutions/localsite.py
@@ -2,5 +2,6 @@ from django_evolution.mutations import *
 from django.db import models
 
 MUTATIONS = [
-    AddField('Repository', 'site', models.ForeignKey, null=True, related_model='site.LocalSite')
+    AddField('Repository', 'local_site', models.ForeignKey,
+             null=True, related_model='site.LocalSite')
 ]
