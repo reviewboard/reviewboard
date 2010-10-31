47fab8ba7670b7d5faaf18c3997e95224fa1fb56
Christian Hammond <chipx86@chipx86.com>
2010-10-30 23:13:35 -0700

    Set the database information in prepare-dev.py.
    prepare-dev.py wasn't updating the database name after the changes for
    Django 1.2's new DATABASES variable. It now scans for the right
    variable and updates it.

--- contrib/internal/prepare-dev.py
+++ contrib/internal/prepare-dev.py
@@ -28,8 +28,8 @@ def create_settings():
                 ])
 
                 out_fp.write('SECRET_KEY = "%s"\n' % secret_key)
-            elif line.startswith("DATABASE_NAME = "):
-                out_fp.write('DATABASE_NAME = "%s"\n' %
+            elif line.strip().startswith("'NAME': "):
+                out_fp.write("        'NAME': '%s',\n" %
                              os.path.abspath("reviewboard.db"))
             else:
                 out_fp.write(line)
