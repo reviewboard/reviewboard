fa271de837cec4b6a789f4b5de726e17156fb6ed
David Trowbridge <trowbrds@gmail.com>
2011-01-26 22:47:21 -0800

    Remove the deprecated webapi.
    We'll have had a fairly long run of 1.5 with the deprecated webapi, and it
    doesn't support some of our new features like local site partitions. We've
    broken all our dependencies on it now that rbtools and the web UI speak the REST
    API, so we can remove it.

--- reviewboard/admin/middleware.py
+++ reviewboard/admin/middleware.py
@@ -10,7 +10,6 @@ from django.core.handlers.wsgi import WSGIRequest
 from reviewboard.admin.checks import check_updates_required
 from reviewboard.admin.siteconfig import load_site_config
 from reviewboard.admin.views import manual_updates_required
-from reviewboard.webapi.json import service_not_configured
 
 
 class LoadSettingsMiddleware(object):
@@ -38,9 +37,6 @@ class CheckUpdatesRequiredMiddleware(object):
 
         if (check_updates_required() and
             not path_info.startswith(settings.MEDIA_URL)):
-            if path_info.startswith(settings.SITE_ROOT + "api/"):
-                return service_not_configured(request)
-
             return manual_updates_required(request)
 
         # Let another handler handle this.
