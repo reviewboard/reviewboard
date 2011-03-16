4ec978676555df39539bf695c1442bf9045f52b2
Mengyun Kong <kongmengyun@gmail.com>
2011-03-15 22:37:07 -0700

    Fix bug 1571
    On a "New Review Request" page, the text fields for paths and filenames are too
    short. By setting the widths of related table and classes width 100% to fit the
    screen, and the size of the fileinput to be long enough under 800x600, they
    become longer.
    
    Testing done: It works well in firefox (800x600) and Chromium.
    Untested in IE and safari as well as for SVN repository.
    Fixes bug 1571.
    Reviewed at http://reviews.reviewboard.org/r/2058/

--- reviewboard/reviews/forms.py
+++ reviewboard/reviews/forms.py
@@ -97,19 +97,19 @@ class NewReviewRequestForm(forms.Form):
         required=False,
         help_text=_("The absolute path in the repository the diff was "
                     "generated in."),
-        widget=forms.TextInput(attrs={'size': '35'}))
+        widget=forms.TextInput(attrs={'size': '62'}))
     diff_path = forms.FileField(
         label=_("Diff"),
         required=False,
         help_text=_("The new diff to upload."),
-        widget=forms.FileInput(attrs={'size': '35'}))
+        widget=forms.FileInput(attrs={'size': '62'}))
     parent_diff_path = forms.FileField(
         label=_("Parent Diff"),
         required=False,
         help_text=_("An optional diff that the main diff is based on. "
                     "This is usually used for distributed revision control "
                     "systems (Git, Mercurial, etc.)."),
-        widget=forms.FileInput(attrs={'size': '35'}))
+        widget=forms.FileInput(attrs={'size': '62'}))
     repository = forms.ModelChoiceField(
         label=_("Repository"),
         queryset=Repository.objects.none(),
