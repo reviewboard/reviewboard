cc328ca0ff8dac87ba7f5a88fe26151f3e64edeb
Crystal Lok Koo <crystal.koo@gmail.com>
2011-03-31 16:36:08 -0700

    Quick Search: Search Resource
    This is the resource used by quick search. It provides information on users
    (username, first name, last name), groups (name, display name), and review
    requests (id, summaries).
    
    Testing done: I tested it through the /search/?q=___ and it works properly.
    Also, with the autocomplete, it also returns the data I want.
    
    Reviewed at http://reviews.reviewboard.org/r/2198/

--- reviewboard/webapi/resources.py
+++ reviewboard/webapi/resources.py
@@ -4975,6 +4975,83 @@ class ReviewRequestResource(WebAPIResource):
 review_request_resource = ReviewRequestResource()
 
 
+class SearchResource(WebAPIResource, DjbletsUserResource):
+    """
+    Provides information on users, groups and review requests.
+
+    This is the resource for the autocomplete widget for
+    quick search. This resource helps filter for
+    users, groups and review requests.
+    """
+    name = 'search'
+    singleton = True
+
+    @webapi_check_local_site
+    @webapi_check_login_required
+    def get(self, request, local_site_name=None, fullname=None, q=None,
+            displayname=None, id=None, *args, **kwargs):
+        """Returns information on users, groups and review requests.
+
+        This is used by the autocomplete widget for quick search to
+        get information on users, groups and review requests. This
+        function returns users' first name, last name and username,
+        groups' name and display name, and review requests' ID and
+        summary.
+        """
+        search_q = request.GET.get('q', None)
+        local_site = _get_local_site(local_site_name)
+        if local_site:
+            query = local_site.users.filter(is_active=True)
+        else:
+            query = self.model.objects.filter(is_active=True)
+
+        if search_q:
+            q = (Q(username__istartswith=search_q) |
+                 Q(first_name__istartswith=search_q) |
+                 Q(last_name__istartswith=search_q))
+
+            if request.GET.get('fullname', None):
+                q = q | (Q(first_name__istartswith=search_q) |
+                         Q(last_name__istartswith=search_q))
+
+            query = query.filter(q)
+
+        search_q = request.GET.get('q', None)
+        local_site = _get_local_site(local_site_name)
+        query_groups = Group.objects.filter(local_site=local_site)
+
+        if search_q:
+            q = (Q(name__istartswith=search_q) |
+                  Q(display_name__istartswith=search_q))
+
+            if request.GET.get('displayname', None):
+                q = q | Q(display_name__istartswith=search_q)
+
+            query_groups = query_groups.filter(q)
+
+        search_q = request.GET.get('q', None)
+        query_review_requests = ReviewRequest.objects.filter(local_site=local_site)
+
+        if search_q:
+            q = (Q(id__istartswith=search_q) |
+                  Q(summary__icontains=search_q))
+
+            if request.GET.get('id', None):
+                q = q | Q(id__istartswith=search_q)
+
+            query_review_requests = query_review_requests.filter(q)
+
+        return 200, {
+            self.name: {
+                'users': query,
+                'groups': query_groups,
+                'review_requests': query_review_requests,
+            },
+        }
+
+search_resource = SearchResource()
+
+
 class ServerInfoResource(WebAPIResource):
     """Information on the Review Board server.
 
@@ -5087,6 +5164,7 @@ class RootResource(DjbletsRootResource):
             repository_resource,
             review_group_resource,
             review_request_resource,
+            search_resource,
             server_info_resource,
             session_resource,
             user_resource,
