a9ac1973c71c13634165a3fc1e553985489acb5e
Christian Hammond <chipx86@chipx86.com>
2011-06-23 01:09:33 -0700

    Added missing unit tests for private profiles.
    Along with my private profiles change, I added some unit tests to ensure
    things wouldn't break. These didn't get added, though. Oops.

---
+++ reviewboard/accounts/tests.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+
+class ProfileTests(TestCase):
+    """Testing the Profile model."""
+    fixtures = ['test_users']
+
+    def test_is_profile_visible_with_public(self):
+        """Testing User.is_profile_public with public profiles."""
+        user1 = User.objects.get(username='admin')
+        user2 = User.objects.get(username='doc')
+
+        self.assertTrue(user1.is_profile_visible(user2))
+
+    def test_is_profile_visible_with_private(self):
+        """Testing User.is_profile_public with private profiles."""
+        user1 = User.objects.get(username='admin')
+        user2 = User.objects.get(username='doc')
+
+        profile = user1.get_profile()
+        profile.is_private = True
+        profile.save()
+
+        self.assertFalse(user1.is_profile_visible(user2))
+        self.assertTrue(user1.is_profile_visible(user1))
+
+        user2.is_staff = True
+        self.assertTrue(user1.is_profile_visible(user2))
