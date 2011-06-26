0021be080e77f9074a7b62aa0f29ae8c0b1a5cb5
Christian Hammond <chipx86@chipx86.com>
2011-06-26 02:14:33 -0700

    Add a manual updates page for database errors.
    One of the most "help" pages for Review Board administrators is shown when
    there's a database error. We tell them that the database just needs to be
    synced/evolved, but the true cause may be that the database is corrupted,
    locked, or something else.
    
    Now we show a page saying there's a database error, recommending some
    steps, and showing the actual error text to help diagnose the problem.
    
    The old, confusing error page has been removed.

--- reviewboard/admin/checks.py
+++ reviewboard/admin/checks.py
@@ -34,6 +34,7 @@ import os
 import sys
 
 from django.conf import settings
+from django.db import DatabaseError
 from django.utils.translation import gettext as _
 from djblets.util.filesystem import is_exe_in_path
 from djblets.siteconfig.models import SiteConfiguration
@@ -58,15 +59,31 @@ def check_updates_required():
         site_dir = os.path.dirname(settings.HTDOCS_ROOT)
         devel_install = (os.path.exists(os.path.join(settings.LOCAL_ROOT,
                                                      'manage.py')))
+        siteconfig = None
 
-        siteconfig = SiteConfiguration.objects.get_current()
+        # Check if we can access a SiteConfiguration. There should always
+        # be one, unless the user has erased stuff by hand.
+        #
+        # This also checks for any sort of errors in talking to the database.
+        # This could be due to the database being down, or corrupt, or
+        # tables locked, or an empty database, or other cases. We want to
+        # catch this before getting the point where plain 500 Internal Server
+        # Errors appear.
+        try:
+            siteconfig = SiteConfiguration.objects.get_current()
+        except (DatabaseError, SiteConfiguration.DoesNotExist), e:
+            _updates_required.append((
+                'admin/manual-updates/database-error.html', {
+                    'error': e,
+                }
+            ))
 
         # Check if the version running matches the last stored version.
         # Only do this for non-debug installs, as it's really annoying on
         # a developer install.:
         cur_version = get_version_string()
 
-        if siteconfig.version != cur_version:
+        if siteconfig and siteconfig.version != cur_version:
             _updates_required.append((
                 'admin/manual-updates/version-mismatch.html', {
                     'current_version': cur_version,
@@ -78,7 +95,7 @@ def check_updates_required():
 
         # Check if the site has moved and the old media directory no longer
         # exists.
-        if not os.path.exists(settings.MEDIA_ROOT):
+        if siteconfig and not os.path.exists(settings.MEDIA_ROOT):
             new_media_root = os.path.join(settings.HTDOCS_ROOT, "media")
 
             if os.path.exists(new_media_root):
@@ -113,16 +130,6 @@ def check_updates_required():
                     'server_user': getpass.getuser(),
                     'expected_data_dir': os.path.join(site_dir, 'data'),
                 }
-            ))
-
-        try:
-            from reviewboard.changedescs.models import ChangeDescription
-            ChangeDescription.objects.count()
-        except:
-            # We were unable to load this, so it's likely that the user
-            # hasn't run syncdb yet.
-            _updates_required.append((
-                "admin/manual-updates/run-syncdb.html", {}
             ))
 
         if not is_exe_in_path('patch'):
