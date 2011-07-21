8837a9d2a2ff81b94d963df68ec954b3a3d05508
Christian Hammond <chipx86@chipx86.com>
2011-07-21 11:15:50 -0700

    Log more debug info from the release script on error.
    If there was a problem registering the release with the site, the
    release script could fail. This is an attempt to get better debug info out
    of it.

--- contrib/internal/release.py
+++ contrib/internal/release.py
@@ -171,9 +171,19 @@ def register_release():
     }
 
     print 'Posting release to reviewboard.org'
-    f = urllib2.urlopen(urllib2.Request(url=RELEASES_API_URL, data=content,
-                                        headers=headers))
-    f.read()
+    try:
+        f = urllib2.urlopen(urllib2.Request(url=RELEASES_API_URL, data=content,
+                                            headers=headers))
+        f.read()
+    except urllib2.HTTPError, e:
+        print "Error uploading. Got HTTP code %d:" % e.code
+        print e.read()
+    except urllib2.URLError, e:
+        try:
+            print "Error uploading. Got URL error:" % e.code
+            print e.read()
+        except AttributeError:
+            pass
 
 
 def main():
