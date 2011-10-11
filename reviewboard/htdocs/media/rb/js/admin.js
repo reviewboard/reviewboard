31cffdc9e93937f94afabc971fe6202cfc93a29e
Christian Hammond <chipx86@chipx86.com>
2011-10-11 03:12:49 -0700

    Remove the fade-in of the new admin UI and improve loading overall.
    The new admin UI faded in on load. This was kind of neat but also a bit
    overkill, especially after the first couple times. This seems to have
    been partially there to work around an issue involving collapsed states
    for widgets.
    
    If a widget started collapsed, we were temporarily hiding it, giving
    flot time to draw (it won't draw in a display: none, which the FAQ even
    points out), then showing it. This was happening during the fade-in
    time.
    
    I've worked around this by triggering an event telling widgets when
    they've been shown. We can use this to draw the first time we get the
    signal when flot is involved. This allows us to just initially show the
    widgets, default the collapsed ones to actually being collapsed.
    
    Along with this, I've changed the collapse/expand animation to slide the
    content, rather than fade in/out.

--- reviewboard/htdocs/media/rb/js/admin.js
+++ reviewboard/htdocs/media/rb/js/admin.js
@@ -1,33 +1,29 @@
-function refreshWidgets() {
-    var sideWidth = $("#admin-actions").outerWidth(),
-        centerWidth = $("#admin-widgets").outerWidth(),
-        winWidth = $(window).width();
-
-    $(".admin-extras")
-        .css('width', winWidth - (sideWidth + centerWidth) - 50)
-        .masonry('reload');
-}
-
-function dashboardFadeIn() {
-    $('.admin-extras').masonry('reload', function() {
-        $("#dashboard-view").animate({
-            opacity: 1
-        }, "fast");
-    });
-}
-
 $(window).load(function() {
-    var hiddenWidgets = $(".widget-hidden .widget-content");
+    $(".admin-widget").each(function() {
+        var widget = $(this);
 
-    if (hiddenWidgets.length > 0) {
-        hiddenWidgets.slideUp("fast", dashboardFadeIn);
-    } else {
-        dashboardFadeIn();
-    }
+        if (widget.hasClass("widget-hidden")) {
+            widget.trigger("widget-hidden");
+        } else {
+            widget.trigger("widget-shown");
+        }
+    });
 });
 
 $(document).ready(function() {
-    $('.admin-extras').masonry({
+    var adminExtras = $(".admin-extras");
+
+    function refreshWidgets() {
+        var sideWidth = $("#admin-actions").outerWidth(),
+            centerWidth = $("#admin-widgets").outerWidth(),
+            winWidth = $(window).width();
+
+        adminExtras
+            .css('width', winWidth - (sideWidth + centerWidth) - 50)
+            .masonry('reload');
+    }
+
+    adminExtras.masonry({
         itemSelector: '.admin-widget'
     });
 
@@ -39,24 +35,23 @@ $(document).ready(function() {
         var widgetBox = $(this).parent(),
             widgetBoxId = widgetBox.attr('id');
 
-        widgetBox.fadeTo('fast', 0, function() {
-            widgetBox.find(".widget-content").slideToggle('fast', function() {
-                var collapsed;
+        widgetBox.find(".widget-content").slideToggle('fast', function() {
+            var collapsed;
 
-                $('.admin-extras').masonry('reload');
-                widgetBox.fadeTo("fast", 1);
+            adminExtras.masonry('reload');
 
-                if (widgetBox.hasClass("widget-hidden")) {
-                    widgetBox.removeClass("widget-hidden")
-                    collapsed = 0;
-                } else {
-                    widgetBox.addClass("widget-hidden");
-                    collapsed = 1;
-                }
+            if (widgetBox.hasClass("widget-hidden")) {
+                widgetBox.removeClass("widget-hidden")
+                collapsed = 0;
+                widgetBox.trigger("widget-shown");
+            } else {
+                widgetBox.addClass("widget-hidden");
+                widgetBox.trigger("widget-hidden");
+                collapsed = 1;
+            }
 
-                $.post("widget-toggle/?widget=" + widgetBoxId +
-                       "&collapse=" + collapsed);
-             });
-        });
+            $.post("widget-toggle/?widget=" + widgetBoxId +
+                   "&collapse=" + collapsed);
+         });
     });
 });
