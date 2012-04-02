0a95fbe77eddae4093c9d824c7631d87a2ee91cd
Christian Hammond <chipx86@chipx86.com>
2012-04-02 02:34:02 -0700

    Disable the Python 2.4 build in the release script.
    We won't be supporting Python 2.4 anymore, so disable building it in the
    release script.

--- contrib/internal/release.py
+++ contrib/internal/release.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
 from reviewboard import __version__, __version_info__, is_release
 
 
-PY_VERSIONS = ["2.4", "2.5", "2.6", "2.7"]
+PY_VERSIONS = ["2.5", "2.6", "2.7"]
 
 LATEST_PY_VERSION = PY_VERSIONS[-1]
 
