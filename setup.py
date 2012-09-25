9a8a7ad029d42a6b287159d765de091ebc54ff1c
Christian Hammond <chipx86@chipx86.com>
2012-09-24 17:25:06 -0700

    Require Djblets 0.6.23.

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ setup(name=PACKAGE_NAME,
       install_requires=[
           'Django==1.3.3',
           'django_evolution>=0.6.5',
-          'Djblets>0.6.22,<0.7alpha0.dev',
+          'Djblets==0.6.23',
           'Pygments>=1.4',
           'flup',
           'paramiko>=1.7.6',
