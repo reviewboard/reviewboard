1136824ab60dbb8774ba5cb8d011e898f9286e06
Karl Leuschen <k.leuschen@dal.ca>
2012-09-30 08:27:02 -0700

    Add a missing file
    I forgot to add this file for b64dd03 (I being David Trowbridge). Whoops.

---
+++ reviewboard/admin/validation.py
@@ -0,0 +1,25 @@
+from django import forms
+
+
+def validate_bug_tracker(input_url):
+    """
+    Validates that an issue tracker URI string contains one `%s` Python format
+    specification type (no other types are supported).
+    """
+    try:
+        # Ignore escaped `%`'s
+        test_url = input_url.replace('%%', '')
+
+        if test_url.find('%s') == -1:
+            raise TypeError
+
+        # Ensure an arbitrary value can be inserted into the URL string
+        test_url = test_url % 1
+    except (TypeError, ValueError):
+        raise forms.ValidationError(["%s has invalid format specification "
+                                     "type(s). Use only one '%%s' to mark the "
+                                     "location of the bug id. If the URI "
+                                     "contains encoded values (e.g. '%%20'), "
+                                     "prepend the encoded values with an "
+                                     "additional '%%'."
+                                     % input_url])
