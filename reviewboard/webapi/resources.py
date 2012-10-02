12394eeea09697db9d9e67e2263cc9302349078b
Sampson Chen <sampson.chen@gmail.com>
2012-10-01 23:52:20 -0700

    Fix webapi list of review requests when using ?shipit=0
    When accessing the /api/review-requests resource with a shipit=0 parameter, it
    would return the wrong results (often returning review requests that did have
    reviews with ship-its). This code was initially written to do a join with the
    reviews table and query the shipit parameter of that model, which was checking
    whether *all* of the associated review requests didn't have ship-its (which
    wasn't completely incorrect but not what a user really wants when passing
    shipit=0).
    
    To fix this, instead of excluding review requests with reviews that have ship
    its, I've changed it to add a test for the shipit_count field on the
    ReviewRequest model. I've also updated the unit tests for this API to use the
    same query to test against.
    
    While updating the unit tests, I discovered a couple issues. All of the
    shipit_count fields in the fixtures were zero, despite there being the presence
    of some reviews with a ship-it. I've updated the test_reviewrequests fixture to
    fix this issue. I also had to add the test_site fixture to the
    test_get_reviewrequests_with_ship_it_* tests in order for the data to be valid.
    
    Fixes issue 2635.
    Reviewed at http://reviews.reviewboard.org/r/3371/

--- reviewboard/webapi/resources.py
+++ reviewboard/webapi/resources.py
@@ -5803,7 +5803,6 @@ class ReviewRequestResource(WebAPIResource):
 
         if is_list:
             q = Q()
-            exclude_q = Q()
 
             if 'to-groups' in request.GET:
                 for group_name in request.GET.get('to-groups').split(','):
@@ -5838,9 +5837,9 @@ class ReviewRequestResource(WebAPIResource):
                 ship_it = request.GET.get('ship-it')
 
                 if ship_it in ('1', 'true', 'True'):
-                    q = q & Q(reviews__ship_it=True)
+                    q = q & Q(shipit_count__gt=0)
                 elif ship_it in ('0', 'false', 'False'):
-                    exclude_q = exclude_q & Q(reviews__ship_it=True)
+                    q = q & Q(shipit_count=0)
 
             if 'time-added-from' in request.GET:
                 date = self._parse_date(request.GET['time-added-from'])
@@ -5872,9 +5871,6 @@ class ReviewRequestResource(WebAPIResource):
                                                  status=status,
                                                  local_site=local_site,
                                                  extra_query=q)
-
-            if exclude_q:
-                queryset = queryset.exclude(exclude_q)
 
             return queryset
         else:
