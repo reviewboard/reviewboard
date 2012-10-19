56d6934b229a80af50dd6cdfb9d5cc0c6f2b63d9
Jason Veatch <jtveatch@gmail.com>
2012-10-19 00:09:58 -0700

    Display the "Add Comment" link after canceling an editor.
    The "Add Comment" link was only being reshown after submitting a change.
    If canceling, the link wasn't being shown again. We now force it to fade
    in when canceling.
    
    Fixes bug #2696
    
    Reviewed at http://reviews.reviewboard.org/r/3415/

--- reviewboard/htdocs/media/rb/js/reviews.js
+++ reviewboard/htdocs/media/rb/js/reviews.js
@@ -572,6 +572,7 @@ $.fn.commentSection = function(review_id, context_id, context_type) {
                 })
                 .bind("cancel", function(e) {
                     gEditCount--;
+                    addCommentLink.fadeIn();
                     removeCommentFormIfEmpty(self);
                 });
         });
