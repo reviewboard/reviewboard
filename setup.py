864664d16d3f0aca854e9094ffa141782576b1f9
Christian Hammond <chipx86@chipx86.com>
2012-10-21 16:16:41 -0700

    Bump our Django requirement to 1.3.4.
    This bumps to the latest security release of Django.

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(name=PACKAGE_NAME,
       },
       cmdclass=cmdclasses,
       install_requires=[
-          'Django==1.3.3',
+          'Django==1.3.4',
           'django_evolution>=0.6.5',
           'Djblets==0.6.23',
           'Pygments>=1.4',
