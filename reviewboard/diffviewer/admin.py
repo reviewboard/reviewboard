4a60ef891d44cb639f70fd9b69e78d2f4cd43b63
Christian Hammond <chipx86@chipx86.com>
2013-01-30 01:29:04 -0800

    Added nice styling of diffs in the FileDiff admin UI.
    The FileDiff admin UI previously just showed diffs with no whitespace
    preservation, which was useless. We now style it with Pygments, which is
    nicer to look at.

--- reviewboard/diffviewer/admin.py
+++ reviewboard/diffviewer/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import DiffLexer
 
 from reviewboard.diffviewer.models import FileDiff, DiffSet, DiffSetHistory
 
@@ -16,6 +20,23 @@ class FileDiffAdmin(admin.ModelAdmin):
                     'dest_file', 'dest_detail')
     raw_id_fields = ('diffset',)
     readonly_fields = ('diff', 'parent_diff')
+
+    def diff(self, filediff):
+        return self._style_diff(filediff.diff)
+    diff.label = _('Diff')
+    diff.allow_tags = True
+
+    def parent_diff(self, filediff):
+        return self._style_diff(filediff.parent_diff)
+    parent_diff.label = _('Parent diff')
+    parent_diff.allow_tags = True
+
+    def _style_diff(self, diff):
+        # NOTE: Django wraps the contents in a <p>, but browsers will
+        #       be sad about that, because it contains a <pre>. Chrome,
+        #       for instance, will move it out into its own node. Be
+        #       consistent and just make that happen for them.
+        return '</p>%s<p>' % highlight(diff, DiffLexer(), HtmlFormatter())
 
 
 class FileDiffInline(admin.StackedInline):
