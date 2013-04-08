b691e5a9dea7a70874a629d103b465dee621c0db
Christian Hammond <chipx86@chipx86.com>
2013-04-08 01:45:08 -0700

    Bump to 1.8 alpha.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -4,7 +4,7 @@
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (1, 7, 6, 0, 'final', 0, True)
+VERSION = (1, 8, 0, 0, 'alpha', 0, False)
 
 
 def get_version_string():
