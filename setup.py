53813c550d4eaaed4110b622e4510317aee23c41
Christian Hammond <chipx86@chipx86.com>
2013-04-19 23:10:55 -0700

    Release Review Board 1.7.7.

--- setup.py
+++ setup.py
@@ -165,7 +165,7 @@ setup(name=PACKAGE_NAME,
       install_requires=[
           'Django>=1.4.5,<1.5',
           'django_evolution>=0.6.7',
-          'Djblets>=0.7.11,<0.8',
+          'Djblets>=0.7.12,<0.8',
           'django-pipeline>=1.2.24,<1.3',
           'docutils',
           'markdown>=2.2.1',
