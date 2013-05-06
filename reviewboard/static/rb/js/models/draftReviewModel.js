5c4514acf2a9e3d8214a8e82855c311de4ad77a8
Christian Hammond <chipx86@chipx86.com>
2013-05-06 02:29:51 -0700

    Add a publish function to DraftReview.
    This is used to publish a review. It will emit the "publishing" signal
    when called and "published" when the publish finished.

--- reviewboard/static/rb/js/models/draftReviewModel.js
+++ reviewboard/static/rb/js/models/draftReviewModel.js
@@ -6,4 +6,41 @@
  * special resource exists at /reviews/draft/ which will redirect to the
  * existing draft if one exists, and return 404 if not.
  */
-RB.DraftReview = RB.Review.extend(RB.DraftResourceModelMixin);
+RB.DraftReview = RB.Review.extend(_.extend({
+    /*
+     * Publishes the review.
+     *
+     * Before publish, the "publishing" event will be triggered.
+     *
+     * After the publish has succeeded, the "publishing" event will be
+     * triggered.
+     */
+    publish: function(options, context) {
+        var error;
+
+        options = options || {};
+
+        error = _.isFunction(options.error)
+                ? _.bind(options.error, context)
+                : undefined;
+
+        this.trigger('publishing');
+
+        this.ready({
+            ready: function() {
+                this.set('public', true);
+                this.save({
+                    success: function() {
+                        this.trigger('published');
+
+                        if (_.isFunction(options.success)) {
+                            options.success.call(context);
+                        }
+                    },
+                    error: error
+                }, this);
+            },
+            error: error
+        }, this);
+    }
+}, RB.DraftResourceModelMixin));
--- reviewboard/static/rb/js/models/tests/draftReviewModelTests.js
+++ reviewboard/static/rb/js/models/tests/draftReviewModelTests.js
@@ -17,65 +17,115 @@ describe('models/DraftReview', function() {
         model.rspNamespace = 'foo';
     });
 
-    describe('ready', function() {
+    describe('Methods', function() {
         var callbacks;
 
         beforeEach(function() {
             callbacks = {
                 ready: function() {},
+                success: function() {},
                 error: function() {}
             };
 
-            spyOn(Backbone.Model.prototype, 'fetch')
-                .andCallFake(function(options) {
-                    if (options && _.isFunction(options.success)) {
-                        options.success();
-                    }
-                });
-            spyOn(parentObject, 'ready')
-                .andCallFake(function(options, context) {
-                    if (options && _.isFunction(options.ready)) {
-                        options.ready.call(context);
-                    }
-                });
-            spyOn(model, '_retrieveDraft')
-                .andCallFake(function(options, context) {
-                    if (options && _.isFunction(options.ready)) {
-                        options.ready.call(context);
-                    }
-                });
             spyOn(callbacks, 'ready');
+            spyOn(callbacks, 'success');
             spyOn(callbacks, 'error');
         });
 
-        describe('With isNew=true', function() {
+        describe('ready', function() {
             beforeEach(function() {
-                expect(model.isNew()).toBe(true);
-                expect(model.get('loaded')).toBe(false);
+                spyOn(Backbone.Model.prototype, 'fetch')
+                    .andCallFake(function(options) {
+                        if (options && _.isFunction(options.success)) {
+                            options.success();
+                        }
+                    });
+                spyOn(parentObject, 'ready')
+                    .andCallFake(function(options, context) {
+                        if (options && _.isFunction(options.ready)) {
+                            options.ready.call(context);
+                        }
+                    });
+                spyOn(model, '_retrieveDraft')
+                    .andCallFake(function(options, context) {
+                        if (options && _.isFunction(options.ready)) {
+                            options.ready.call(context);
+                        }
+                    });
             });
 
-            it('With callbacks', function() {
-                model.ready(callbacks);
+            describe('With isNew=true', function() {
+                beforeEach(function() {
+                    expect(model.isNew()).toBe(true);
+                    expect(model.get('loaded')).toBe(false);
+                });
 
-                expect(parentObject.ready).toHaveBeenCalled();
-                expect(model._retrieveDraft).toHaveBeenCalled();
-                expect(callbacks.ready).toHaveBeenCalled();
-            });;
+                it('With callbacks', function() {
+                    model.ready(callbacks);
+
+                    expect(parentObject.ready).toHaveBeenCalled();
+                    expect(model._retrieveDraft).toHaveBeenCalled();
+                    expect(callbacks.ready).toHaveBeenCalled();
+                });;
+            });
+
+            describe('With isNew=false', function() {
+                beforeEach(function() {
+                    model.set({
+                        id: 123
+                    });
+                });
+
+                it('With callbacks', function() {
+                    model.ready(callbacks);
+
+                    expect(parentObject.ready).toHaveBeenCalled();
+                    expect(model._retrieveDraft).not.toHaveBeenCalled();
+                    expect(callbacks.ready).toHaveBeenCalled();
+                });
+            });
         });
 
-        describe('With isNew=false', function() {
+        describe('publish', function() {
             beforeEach(function() {
-                model.set({
-                    id: 123
+                spyOn(model, 'save').andCallFake(function(options, context) {
+                    options.success.call(context);
                 });
             });
 
-            it('With callbacks', function() {
-                model.ready(callbacks);
+            it('Triggers "publishing" event before publish', function() {
+                spyOn(model, 'trigger');
+                spyOn(model, 'ready');
 
-                expect(parentObject.ready).toHaveBeenCalled();
-                expect(model._retrieveDraft).not.toHaveBeenCalled();
-                expect(callbacks.ready).toHaveBeenCalled();
+                model.publish();
+
+                expect(model.trigger).toHaveBeenCalledWith('publishing');
+            });
+
+            it('Triggers "published" event after publish', function() {
+                spyOn(model, 'trigger');
+
+                spyOn(model, 'ready').andCallFake(function(options, context) {
+                    options.ready.call(context);
+                });
+
+                model.publish(callbacks);
+
+                expect(callbacks.success).toHaveBeenCalled();
+                expect(model.trigger).toHaveBeenCalledWith('published');
+            });
+
+            it('Sets "public" to true', function() {
+                spyOn(model, 'ready').andCallFake(function(options, context) {
+                    options.ready.call(context);
+                });
+
+                expect(model.get('public')).toBe(false);
+
+                model.publish(callbacks);
+
+                expect(callbacks.success).toHaveBeenCalled();
+                expect(model.get('public')).toBe(true);
             });
         });
     });
