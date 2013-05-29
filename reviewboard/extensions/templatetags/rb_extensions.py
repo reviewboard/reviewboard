21d72944dbdb49a7f71477abbb9f7b4f705c58b8
David Trowbridge <trowbrds@gmail.com>
2013-05-28 23:30:11 -0700

    Actually support the url_name parameter in NavigationBarHook.
    The docstring for this hook claimed that you could initialize it with a URL
    name, and have it do the reverse lookup. This was a lie. I've implemented this
    functionality.
    
    Testing done:
    - Used this in an extension I've been working on.
    - Ran unit tests.
    
    Reviewed at http://reviews.reviewboard.org/r/4186/

--- reviewboard/extensions/templatetags/rb_extensions.py
+++ reviewboard/extensions/templatetags/rb_extensions.py
@@ -6,6 +6,7 @@ from reviewboard.extensions.hooks import DiffViewerActionHook, \
                                          NavigationBarHook, \
                                          ReviewRequestActionHook, \
                                          ReviewRequestDropdownActionHook
+from reviewboard.site.urlresolvers import local_site_reverse
 
 
 register = template.Library()
@@ -62,6 +63,11 @@ def navigation_bar_hooks(context):
     for hook in NavigationBarHook.hooks:
         for nav_info in hook.get_entries(context):
             if nav_info:
+                url_name = nav_info.get('url_name', None)
+                if url_name:
+                    nav_info['url'] = local_site_reverse(
+                        url_name, request=context.get('request'))
+
                 context.push()
                 context['entry'] = nav_info
                 s += render_to_string("extensions/navbar_entry.html", context)
