6c5f3465da5ed03dca8128bb3dd03121bd2cddb2
David Trowbridge <trowbrds@gmail.com>
2013-06-25 15:40:30 -0700

    Add periods after API error strings.
    Some of the error strings had periods, and some didn't. I haven't yet checked
    djblets' errors, but this makes the reviewboard errors consistent.

--- reviewboard/webapi/errors.py
+++ reviewboard/webapi/errors.py
@@ -4,74 +4,74 @@ from djblets.webapi.errors import WebAPIError
 #
 # Standard error messages
 #
-UNSPECIFIED_DIFF_REVISION = WebAPIError(200, "Diff revision not specified",
+UNSPECIFIED_DIFF_REVISION = WebAPIError(200, "Diff revision not specified.",
                                         http_status=400) # 400 Bad Request)
-INVALID_DIFF_REVISION     = WebAPIError(201, "Invalid diff revision",
+INVALID_DIFF_REVISION     = WebAPIError(201, "Invalid diff revision.",
                                         http_status=404) # 404 Not Found
-INVALID_ACTION            = WebAPIError(202, "Invalid action specified",
+INVALID_ACTION            = WebAPIError(202, "Invalid action specified.",
                                         http_status=400) # 400 Bad Request
 INVALID_CHANGE_NUMBER     = WebAPIError(203, "The change number specified "
-                                             "could not be found",
+                                             "could not be found.",
                                         http_status=404) # 404 Not Found
 CHANGE_NUMBER_IN_USE      = WebAPIError(204, "The change number specified "
-                                             "has already been used",
+                                             "has already been used.",
                                         http_status=409) # 409 Conflict
 MISSING_REPOSITORY        = WebAPIError(205, "There was no repository found "
-                                             "at the specified path",
+                                             "at the specified path.",
                                         http_status=400) # 400 Bad Request
 INVALID_REPOSITORY        = WebAPIError(206, "The repository path specified "
                                              "is not in the list of known "
-                                             "repositories",
+                                             "repositories.",
                                         http_status=400) # 400 Bad Request
 REPO_FILE_NOT_FOUND       = WebAPIError(207, "The file was not found in the "
-                                             "repository",
+                                             "repository.",
                                         http_status=400) # 400 Bad Request
-INVALID_USER              = WebAPIError(208, "User does not exist",
+INVALID_USER              = WebAPIError(208, "User does not exist.",
                                         http_status=400) # 400 Bad Request
 REPO_NOT_IMPLEMENTED      = WebAPIError(209, "The specified repository is "
-                                             "not able to perform this action",
+                                             "not able to perform this action.",
                                         http_status=501) # 501 Not Implemented
 REPO_INFO_ERROR           = WebAPIError(210, "There was an error fetching "
                                              "extended information for this "
-                                             "repository",
+                                             "repository.",
                                         http_status=500) # 500 Internal Server
                                                          #     Error
 NOTHING_TO_PUBLISH        = WebAPIError(211, "You attempted to publish a "
                                              "review request that doesn't "
-                                             "have an associated draft",
+                                             "have an associated draft.",
                                         http_status=400) # 400 Bad Request
 EMPTY_CHANGESET           = WebAPIError(212, "The change number specified "
-                                             "represents an empty changeset",
+                                             "represents an empty changeset.",
                                         http_status=400) # 400 Bad Request
 SERVER_CONFIG_ERROR       = WebAPIError(213, "There was an error storing "
-                                             "configuration on the server",
+                                             "configuration on the server.",
                                         http_status=500) # 500 Internal Server
                                                          #     Error
 BAD_HOST_KEY              = WebAPIError(214, "The SSH key on the host does "
-                                             "not match the stored key",
+                                             "not match the stored key.",
                                         http_status=403) # 403 Forbidden
 UNVERIFIED_HOST_KEY       = WebAPIError(215, "The SSH key on the host is "
-                                             "unverified",
+                                             "unverified.",
                                         http_status=403) # 403 Forbidden
 UNVERIFIED_HOST_CERT      = WebAPIError(216, "The HTTPS certificate on the "
-                                             "host is unverified",
+                                             "host is unverified.",
                                         http_status=403) # 403 Forbidden
 MISSING_USER_KEY          = WebAPIError(217, "A public SSH key was requested, "
                                              "but no SSH key was available "
-                                             "to send",
+                                             "to send.",
                                         http_status=403) # 403 Forbidden
 REPO_AUTHENTICATION_ERROR = WebAPIError(218, "Unable to authenticate with "
                                              "the repository using the "
-                                             "provided credentials",
+                                             "provided credentials.",
                                         http_status=403) # 403 Forbidden
 DIFF_EMPTY                = WebAPIError(219, "The specified diff file is "
-                                             "empty",
+                                             "empty.",
                                         http_status=400) # 400 Bad Request
 DIFF_TOO_BIG              = WebAPIError(220, "The specified diff file is "
-                                             "too large",
+                                             "too large.",
                                         http_status=400) # 400 Bad Request
 FILE_RETRIEVAL_ERROR      = WebAPIError(221, "There was an error fetching "
-                                             "a source file",
+                                             "a source file.",
                                         http_status=500) # 500 Internal Server
                                                          #     Error
 HOSTINGSVC_AUTH_ERROR     = WebAPIError(222, "There was an error authorizing "
