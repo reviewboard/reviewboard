bf0d51691999c51c00f048c8db1b60795d0ec8d1
David Trowbridge <trowbrds@gmail.com>
2013-06-28 01:59:23 -0700

    Add a generic CollectionView for handling live-updating collections.
    This adds a utility view for rendering collections of things. To use this, one
    extends this view with a new one that defines the itemViewType attribute. Items
    can be added to the collection before or after the view is rendered, and the
    results in the DOM will always match the current state of the collection.
    
    Testing done: Ran js-tests, used this in my post-commit view.
    Reviewed at http://reviews.reviewboard.org/r/4269/

---
+++ reviewboard/static/rb/js/views/collectionView.js
@@ -0,0 +1,84 @@
+/*
+ * An abstract view for rendering a collection.
+ *
+ * This provides core, reusable functionality for any view that wants to render
+ * a collection and respond to add/remove events. Types that extend this should
+ * make sure to define the 'itemViewType' attribute, which will be the view
+ * instantiated for each model in the collection.
+ */
+RB.CollectionView = Backbone.View.extend({
+    /*
+     * The view that will be instantiated for rendering items in the collection.
+     */
+    itemViewType: null,
+
+    /*
+     * Initializes CollectionView.
+     */
+    initialize: function(options) {
+        var collection = options.collection;
+
+        this.collection = collection;
+        this.views = [];
+
+        collection.each(this._add, this);
+        collection.on({
+            add: this._add,
+            remove: this._remove
+        }, this)
+    },
+
+    /*
+     * Renders the view.
+     *
+     * This will iterate over all the child views and render them as well.
+     */
+    render: function() {
+        this._rendered = true;
+
+        this.$el.empty();
+        _.each(this.views, function(view) {
+            this.$el.append(view.render().el);
+        }, this);
+
+        return this;
+    },
+
+    /*
+     * Add a view for an item in the collection.
+     *
+     * This will instantiate the itemViewType, and if the CollectionView has
+     * been rendered, render and append it as well.
+     */
+    _add: function(item) {
+        var view;
+
+        console.assert(this.itemViewType,
+                       'itemViewType must be defined by the subclass');
+        view = new this.itemViewType({
+            model: item
+        });
+        this.views.push(view);
+
+        if (this._rendered) {
+            this.$el.append(view.render().el);
+        }
+    },
+
+    /*
+     * Remove a view for an item in the collection.
+     */
+    _remove: function(item) {
+        var grouped = _.groupBy(this.views, function(view) {
+            return view.model === item ? 'toRemove' : 'toKeep';
+        });
+
+        this.views = grouped.toKeep;
+
+        if (this._rendered) {
+            _.each(grouped.toRemove, function(view) {
+                view.remove();
+            });
+        }
+    }
+});
