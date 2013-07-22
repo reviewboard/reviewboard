627c3a5fc8521a669a102e5fa6e7259ae465f037
David Trowbridge <trowbrds@gmail.com>
2013-07-21 19:39:06 -0700

    Add views and tests for a repository selector.
    This change adds views to show a collection of RB.Repository models in HTML
    suitable for the new review request UI. This is plumbed through so that the view
    will emit a 'selected' event and toggle element classes when the user clicks on
    any individual repository in the list.
    
    Testing done:
    - Ran unit tests.
    - Used this in my new review request UI.
    
    Reviewed at https://reviews.reviewboard.org/r/4353/

---
+++ reviewboard/static/rb/js/newReviewRequest/views/repositorySelectionView.js
@@ -0,0 +1,48 @@
+/*
+ * A view for selecting a repository from a collection.
+ */
+RB.RepositorySelectionView = RB.CollectionView.extend({
+    className: 'repository-selector',
+    itemViewType: RB.RepositoryView,
+
+    /*
+     * Initialize the view.
+     */
+    initialize: function() {
+        _.super(this).initialize.apply(this, arguments);
+
+        this._selected = null;
+
+        this.listenTo(this.collection, 'selected', this._onRepositorySelected);
+    },
+
+    /*
+     * Render the view.
+     */
+    render: function() {
+        _.super(this).render.apply(this, arguments);
+
+        $('<h3>').text('Repositories').prependTo(this.$el);
+        return this;
+    },
+
+    /*
+     * Callback for when an individual repository is selected.
+     *
+     * Ensures that the selected repository has the 'selected' class applied
+     * (and no others do), and triggers the 'selected' event on the view.
+     */
+    _onRepositorySelected: function(item) {
+        this._selected = item;
+
+        _.each(this.views, function(view) {
+            if (view.model === item) {
+                view.$el.addClass('selected');
+            } else {
+                view.$el.removeClass('selected');
+            }
+        });
+
+        this.trigger('selected', item);
+    }
+});
