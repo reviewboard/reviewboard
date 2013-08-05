reviewboard/reviewboard

commit 562a8b4a21a932c590551e3164d0679f46f75445

Don't deal with unused files in a parent diff.
When uploading a diff with a parent diff, it's very common to have files
that are in the parent diff that are not in the actual diff going up for
review. This is especially true for large merge branches, or when some
incorrect parent diffs are put up (wrong parameters passed to
post-review/rbt post).

Each unused file gets checked against the repository and then then
thrown away. This is pointless.

We now ignore every file in a parent diff that doesn't have a matching
diff in the actual diff file.

reviewboard/diffviewer/forms.py
@@ -99,10 +99,13 @@ class UploadDiffForm(forms.Form):
         parent_changeset_id = None
 
         if parent_diff_file:
+            diff_filenames = set([f.origFile for f in files])
+
             # If the user supplied a base diff, we need to parse it and
             # later apply each of the files that are in the main diff
             for f in self._process_files(parent_diff_file, basedir,
-                                         check_existance=True):
+                                         check_existance=True,
+                                         limit_to=diff_filenames):
                 parent_files[f.origFile] = f
 
                 # Store the original changeset ID if we have it; this should
@@ -154,7 +157,8 @@ class UploadDiffForm(forms.Form):
 
         return diffset
 
-    def _process_files(self, file, basedir, check_existance=False):
+    def _process_files(self, file, basedir, check_existance=False,
+                       limit_to=None):
         tool = self.repository.get_scmtool()
 
         for f in tool.get_parser(file.read()).parse():
@@ -165,6 +169,11 @@ class UploadDiffForm(forms.Form):
                 filename = f2
             else:
                 filename = os.path.join(basedir, f2).replace("\\", "/")
+
+            if limit_to is not None and filename not in limit_to:
+                # This file isn't actually needed for the diff, so save
+                # ourselves a remote file existence check and some storage.
+                continue
 
             # FIXME: this would be a good place to find permissions errors
             if (revision != PRE_CREATION and

reviewboard/diffviewer/tests.py
@@ -1,14 +1,16 @@
 import os
 import unittest
 
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from djblets.siteconfig.models import SiteConfiguration
 
+from reviewboard.diffviewer.forms import UploadDiffForm
 from reviewboard.diffviewer.models import DiffSet, FileDiff
 from reviewboard.diffviewer.templatetags.difftags import highlightregion
 import reviewboard.diffviewer.diffutils as diffutils
 import reviewboard.diffviewer.parser as diffparser
-from reviewboard.scmtools.models import Repository
+from reviewboard.scmtools.models import Repository, Tool
 
 
 class MyersDifferTest(TestCase):
@@ -448,3 +450,86 @@ class DbTests(TestCase):
         filediff2.save()
 
         self.assertEquals(filediff1.diff_hash, filediff2.diff_hash)
+
+
+class UploadDiffFormTests(TestCase):
+    """Unit tests for UploadDiffForm."""
+    fixtures = ['test_scmtools']
+
+    def test_parent_diff_filtering(self):
+        """Testing UploadDiffForm and filtering parent diff files"""
+        saw_file_exists = {}
+
+        def get_file_exists(filename, revision):
+            saw_file_exists[(filename, revision)] = True
+            return True
+
+        diff = (
+            'Index: README\n'
+            '==========================================================='
+            '========\n'
+            '--- README  (revision 124)\n'
+            '+++ README  (new)\n'
+            '@ -1,1 +1,1 @@\n'
+            '-blah blah\n'
+            '+blah!\n'
+        )
+        parent_diff_1 = (
+            'Index: README\n'
+            '==========================================================='
+            '========\n'
+            '--- README  (revision 123)\n'
+            '+++ README  (new)\n'
+            '@ -1,1 +1,1 @@\n'
+            '-blah..\n'
+            '+blah blah\n'
+        )
+        parent_diff_2 = (
+            'Index: UNUSED\n'
+            '==========================================================='
+            '========\n'
+            '--- UNUSED  (revision 123)\n'
+            '+++ UNUSED  (new)\n'
+            '@ -1,1 +1,1 @@\n'
+            '-foo\n'
+            '+bar\n'
+        )
+        parent_diff = parent_diff_1 + parent_diff_2
+
+        diff_file = SimpleUploadedFile('diff', diff,
+                                       content_type='text/x-patch')
+        parent_diff_file = SimpleUploadedFile('parent_diff', parent_diff,
+                                              content_type='text/x-patch')
+
+        # Note that we're using SVN here for the test just because it's
+        # a bit easier to test with than Git (easier diff parsing logic
+        # and revision numbers).
+        repository = Repository.objects.create(
+            name='Subversion SVN',
+            path='file://%s' % (os.path.join(os.path.dirname(__file__),
+                                             '..', 'scmtools', 'testdata',
+                                             'svn_repo')),
+            tool=Tool.objects.get(name='Subversion'))
+        repository.get_file_exists = get_file_exists
+
+        form = UploadDiffForm(
+            repository=repository,
+            data={
+                'basedir': '/',
+            },
+            files={
+                'path': diff_file,
+                'parent_diff_path': parent_diff_file,
+            })
+        self.assertTrue(form.is_valid())
+
+        diffset = form.create(diff_file, parent_diff_file)
+        self.assertEqual(diffset.files.count(), 1)
+
+        filediff = diffset.files.get()
+        self.assertEqual(filediff.diff, diff)
+        self.assertEqual(filediff.parent_diff, parent_diff_1)
+
+        self.assertTrue(('/README', '123') in saw_file_exists)
+        self.assertFalse(('/UNUSED', '123') in saw_file_exists)
+        self.assertEqual(len(saw_file_exists), 1)

commit 45faed44d12c752a953d9df79b57989edf5a8739

Correctly store base revision for Mercurial diffs with parent diffs.
Mercurial diffs with parent diffs do not work at all in RB 1.7.7.1 if there is
no overlap between the files in the diff and those in the parent diff.

This change fixes the issue and improves the method of dealing with Mercurial
diffs that was introduced changeset 6b1f537b4cf9. The old method was somewhat
fragile and was broken by changeset 562a8b4a21a9.

Testing done:
Tested using a local repo on which the problem was originally seen.
Added a unit test that tests this scenario.

Fixes bug 2971.
Reviewed at https://reviews.reviewboard.org/r/4121/

reviewboard/diffviewer/forms.py
@@ -85,7 +85,9 @@ class UploadDiffForm(forms.Form):
 
         # Parse the diff
         files = list(self._process_files(
-            diff_file, basedir, check_existance=(not parent_diff_file)))
+            tool.get_parser(diff_file.read()),
+            basedir,
+            check_existance=(not parent_diff_file)))
 
         if len(files) == 0:
             raise EmptyDiffError(_("The diff file is empty"))
@@ -99,22 +101,24 @@ class UploadDiffForm(forms.Form):
         # This is used only for tools like Mercurial that use atomic changeset
         # IDs to identify all file versions but not individual file version
         # IDs.
-        parent_changeset_id = None
+        parent_commit_id = None
 
         if parent_diff_file:
             diff_filenames = set([f.origFile for f in files])
 
+            parent_parser = tool.get_parser(parent_diff_file.read())
+
             # If the user supplied a base diff, we need to parse it and
             # later apply each of the files that are in the main diff
-            for f in self._process_files(parent_diff_file, basedir,
+            for f in self._process_files(parent_parser, basedir,
                                          check_existance=True,
                                          limit_to=diff_filenames):
                 parent_files[f.origFile] = f
 
-                # Store the original changeset ID if we have it; this should
-                # be the same for all files.
-                if f.origChangesetId:
-                    parent_changeset_id = f.origChangesetId
+            # This will return a non-None value only for tools that use
+            # commit IDs to identify file versions as opposed to file revision
+            # IDs.
+            parent_commit_id = parent_parser.get_orig_commit_id()
 
         diffset = DiffSet(name=diff_file.name, revision=0,
                           basedir=basedir,
@@ -131,10 +135,8 @@ class UploadDiffForm(forms.Form):
             else:
                 parent_content = ""
 
-                if (tool.diff_uses_changeset_ids and
-                    parent_changeset_id and
-                    f.origInfo != PRE_CREATION):
-                    source_rev = parent_changeset_id
+                if parent_commit_id and f.origInfo != PRE_CREATION:
+                    source_rev = parent_commit_id
                 else:
                     source_rev = f.origInfo
 
@@ -160,11 +162,11 @@ class UploadDiffForm(forms.Form):
 
         return diffset
 
-    def _process_files(self, file, basedir, check_existance=False,
+    def _process_files(self, parser, basedir, check_existance=False,
                        limit_to=None):
         tool = self.repository.get_scmtool()
 
-        for f in tool.get_parser(file.read()).parse():
+        for f in parser.parse():
             f2, revision = tool.parse_diff_revision(f.origFile, f.origInfo,
                                                     f.moved)
 

reviewboard/diffviewer/parser.py
@@ -199,3 +199,11 @@ class DiffParser(object):
         The returned diff as composed of all FileDiffs in the provided diffset.
         """
         return ''.join([filediff.diff for filediff in diffset.files.all()])
+
+    def get_orig_commit_id(self):
+        """Returns the commit ID of the original revision for the diff.
+
+        This is overridden by tools that only use commit IDs, not file
+        revision IDs.
+        """
+        return None

reviewboard/diffviewer/tests.py
@@ -513,6 +513,58 @@ class UploadDiffFormTests(TestCase):
         self.assertFalse(('/UNUSED', '123') in saw_file_exists)
         self.assertEqual(len(saw_file_exists), 1)
 
+    def test_mercurial_parent_diff_base_rev(self):
+        """Testing that the correct base revision is used for Mercurial diffs"""
+        diff = (
+            '# Node ID a6fc203fee9091ff9739c9c00cd4a6694e023f48\n'
+            '# Parent  7c4735ef51a7c665b5654f1a111ae430ce84ebbd\n'
+            'diff --git a/doc/readme b/doc/readme\n'
+            '--- a/doc/readme\n'
+            '+++ b/doc/readme\n'
+            '@@ -1,3 +1,3 @@\n'
+            ' Hello\n'
+            '-\n'
+            '+...\n'
+            ' goodbye\n'
+        )
+
+        parent_diff = (
+            '# Node ID 7c4735ef51a7c665b5654f1a111ae430ce84ebbd\n'
+            '# Parent  661e5dd3c4938ecbe8f77e2fdfa905d70485f94c\n'
+            'diff --git a/doc/newfile b/doc/newfile\n'
+            'new file mode 100644\n'
+            '--- /dev/null\n'
+            '+++ b/doc/newfile\n'
+            '@@ -0,0 +1,1 @@\n'
+            '+Lorem ipsum\n'
+        )
+
+        diff_file = SimpleUploadedFile('diff', diff,
+                                       content_type='text/x-patch')
+        parent_diff_file = SimpleUploadedFile('parent_diff', parent_diff,
+                                              content_type='text/x-patch')
+
+        repository = Repository.objects.create(
+            name='Test HG',
+            path='scmtools/testdata/hg_repo.bundle',
+            tool=Tool.objects.get(name='Mercurial'))
+
+        form = UploadDiffForm(
+            repository=repository,
+            files={
+                'path': diff_file,
+                'parent_diff_path': parent_diff_file,
+            })
+        self.assertTrue(form.is_valid())
+
+        diffset = form.create(diff_file, parent_diff_file)
+        self.assertEqual(diffset.files.count(), 1)
+
+        filediff = diffset.files.get()
+
+        self.assertEqual(filediff.source_revision,
+                         '661e5dd3c4938ecbe8f77e2fdfa905d70485f94c')
+
 
 class DiffChunkGeneratorTests(TestCase):
     """Unit tests for DiffChunkGenerator."""

reviewboard/scmtools/core.py
@@ -52,7 +52,6 @@ PRE_CREATION = Revision("PRE-CREATION")
 class SCMTool(object):
     name = None
     uses_atomic_revisions = False
-    diff_uses_changeset_ids = False
     supports_authentication = False
     supports_raw_file_urls = False
     supports_ticket_auth = False

reviewboard/scmtools/hg.py
@@ -32,7 +32,6 @@ class HgTool(SCMTool):
             self.client = HgClient(repository.path, repository.local_site)
 
         self.uses_atomic_revisions = True
-        self.diff_uses_changeset_ids = True
 
     def get_file(self, path, revision=HEAD):
         return self.client.cat_file(path, str(revision))
@@ -170,6 +169,9 @@ class HgDiffParser(DiffParser):
                     break
 
         return linenum
+
+    def get_orig_commit_id(self):
+        return self.origChangesetId
 
     def _check_file_diff_start(self, linenum, info):
         if linenum + 1 < len(self.lines) and \