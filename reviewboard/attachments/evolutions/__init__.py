a7c85620c570e0f344fdac098052f6bda424fc6e
Christian Hammond <chipx86@chipx86.com>
2013-08-28 13:46:24 -0700

    Remove the evolution to delete FileAttachment.repo_path's index.
    Turns out this evolution doesn't work everywhere, so delete it.
    
    Anyone who updated their database when repo_path was added will likely
    need to run evolve --hint --execute. This will only affect developers
    who updated in the past several days.

--- reviewboard/attachments/evolutions/__init__.py
+++ reviewboard/attachments/evolutions/__init__.py
@@ -2,5 +2,4 @@ SEQUENCE = [
     'file_attachment_orig_filename',
     'file_attachment_file_max_length_512',
     'file_attachment_repo_info',
-    'file_attachment_repo_path_no_index',
 ]
--- reviewboard/attachments/evolutions/file_attachment_repo_path_no_index.py
+++
@@ -1,6 +0,0 @@
-from django_evolution.mutations import ChangeField
-
-
-MUTATIONS = [
-    ChangeField('FileAttachment', 'repo_path', initial=None, db_index=False)
-]
