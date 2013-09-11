b327bd9c90505956dc81f30f51c0eb266e988bb4
Christian Hammond <chipx86@chipx86.com>
2013-09-11 16:15:57 -0700

    Include sample extension entries in the docs database.
    Sample extension entries were missing from the docs database, causing a
    failure to build on the server. This wasn't noticed locally, due to them
    being imported during the docs run.

--- docs/manual/scripts/dumpdata.sh
+++ docs/manual/scripts/dumpdata.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ./docsmanage.py dumpdata --indent=4 \
-	auth accounts attachments changedescs diffviewer hostingsvcs \
+	auth accounts attachments changedescs diffviewer extensions hostingsvcs \
 	reviews scmtools site \
 	> fixtures/initial_data.json
