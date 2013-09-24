31ea6294f14a90d5fc20bb705905e19e0be5589f
Christian Hammond <chipx86@chipx86.com>
2013-09-24 15:19:25 -0700

    Fix docs generation properly.
    My previous fix for docs generation didn't actually work. It just
    appeared to, due to my test conditions.
    
    We now set RUNNING_TEST = True in settings, which is where it should
    go. The Makefile no longer has to set it.
    
    make clean also now removes the database, and this is called when
    fixtures change.

--- docs/manual/settings_local.py
+++ docs/manual/settings_local.py
@@ -13,3 +13,7 @@ LOCAL_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
 ADMINS = [
     ('Example Admin', 'admin@example.com'),
 ]
+
+# Set this in order to bypass code that auto-fills the database with
+# SCMTool data.
+RUNNING_TEST = True
