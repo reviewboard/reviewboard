670a3b230edcc70a6b5909ae5e194512953f7382
Christian Hammond <chipx86@chipx86.com>
2013-09-26 19:59:08 -0700

    Fix the webapi decorators to properly preserve decorator state.
    This makes use of the new fixes in Djblets for webapi decorators to
    preserve the response errors and not pollute the original functions.
    
    It also removes decorators that haven't been used in a very long time.

--- reviewboard/webapi/decorators.py
+++ reviewboard/webapi/decorators.py
@@ -1,25 +1,24 @@
 from django.http import HttpRequest
 from djblets.siteconfig.models import SiteConfiguration
-from djblets.util.decorators import simple_decorator
 from djblets.webapi.core import WebAPIResponse, WebAPIResponseError
-from djblets.webapi.decorators import (webapi_login_required,
+from djblets.webapi.decorators import (webapi_decorator,
+                                       webapi_login_required,
                                        webapi_response_errors,
                                        _find_httprequest)
-from djblets.webapi.encoders import BasicAPIEncoder
 from djblets.webapi.errors import (DOES_NOT_EXIST, NOT_LOGGED_IN,
                                    PERMISSION_DENIED)
 
 from reviewboard.site.models import LocalSite
 
 
-@webapi_response_errors(NOT_LOGGED_IN)
-@simple_decorator
+@webapi_decorator
 def webapi_check_login_required(view_func):
     """
     A decorator that checks whether login is required on this installation
     and, if so, checks if the user is logged in. If login is required and
     the user is not logged in, they'll get a NOT_LOGGED_IN error.
     """
+    @webapi_response_errors(NOT_LOGGED_IN)
     def _check(*args, **kwargs):
         siteconfig = SiteConfiguration.objects.get_current()
         request = _find_httprequest(args)
@@ -31,7 +30,7 @@ def webapi_check_login_required(view_func):
         else:
             return view_func(*args, **kwargs)
 
-    view_func.checks_login_required = True
+    _check.checks_login_required = True
 
     return _check
 
@@ -82,37 +81,18 @@ def webapi_deprecated(deprecated_in, force_error_http_status=None,
     return _dec
 
 
-_deprecated_api_encoders = []
-
-
-def webapi_deprecated_in_1_5(view_func):
-    from reviewboard.webapi.encoder import DeprecatedReviewBoardAPIEncoder
-    global _deprecated_api_encoders
-
-    if not _deprecated_api_encoders:
-        _deprecated_api_encoders = [
-            DeprecatedReviewBoardAPIEncoder(),
-            BasicAPIEncoder(),
-        ]
-
-    return webapi_deprecated(
-        deprecated_in='1.5',
-        force_error_http_status=200,
-        default_api_format='json',
-        encoders=_deprecated_api_encoders)(view_func)
-
-
-@webapi_response_errors(DOES_NOT_EXIST, NOT_LOGGED_IN, PERMISSION_DENIED)
-@simple_decorator
+@webapi_decorator
 def webapi_check_local_site(view_func):
     """Checks whether a user has access to a local site given in the URL.
 
     This decorator can be added to get/get_list methods to check whether or
     not a user should be able to view them given the local site name in the URL.
     """
+    @webapi_response_errors(DOES_NOT_EXIST, NOT_LOGGED_IN, PERMISSION_DENIED)
     def _check(*args, **kwargs):
         request = _find_httprequest(args)
         local_site_name = kwargs.get('local_site_name', None)
+
         if local_site_name:
             try:
                 local_site = LocalSite.objects.get(name=local_site_name)
@@ -126,5 +106,7 @@ def webapi_check_local_site(view_func):
                 return DOES_NOT_EXIST
 
         return view_func(*args, **kwargs)
+
+    _check.checks_local_site = True
 
     return _check
