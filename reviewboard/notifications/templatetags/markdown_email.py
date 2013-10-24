0bc5880d092123c33e4e27d5310ee7bacaa7917c
David Trowbridge <trowbrds@gmail.com>
2013-10-23 17:44:02 -0700

    Render markdown fields for HTML emails.
    This change adds some slightly questionable code to render markdown to HTML for
    the HTML emails. Annoyingly, python markdown doesn't interpret the input quite
    the same as marked.js, so some things don't render the same (this is mostly
    edge cases, particularly with the treatment of things like whether lists
    require a blank line before them or not).
    
    This includes a simple extension for python-markdown that will go through the
    resulting element tree and add inline styles. These styles are similar to what
    we get from reviews.less, but somewhat simplified.
    
    Testing done:
    Viewed the html email previews for review requests, reviews, and replies with
    markdown in the review request fields, review body_top/bottom, and comments.
    Saw all of the fields rendered to markdown, instead of seeing all the control
    and escape characters.
    
    Reviewed at https://reviews.reviewboard.org/r/4844/

---
+++ reviewboard/notifications/templatetags/markdown_email.py
@@ -0,0 +1,93 @@
+import markdown
+from django import template
+from django.utils.safestring import mark_safe
+from markdown.extensions import Extension
+from markdown.treeprocessors import Treeprocessor
+from markdown.util import etree
+
+
+register = template.Library()
+
+
+class InlineStyleProcessor(Treeprocessor):
+    def process_element(self, context, el):
+        # This adds a handful of inline styles to the resulting document which
+        # mimic the .rich-text rules in reviews.less. This does not do quite
+        # everything that the reviews.less rules do, because implementing CSS
+        # selectors in python is pretty gross to begin with, and even then
+        # we're at the mercy of whatever the email client is going to do. The
+        # end result is that the email will look similar but not identical to
+        # the page.
+        textarea_font_size = '13px'
+        textarea_line_height = '15px'
+
+        style = {
+            'margin': 0,
+            'padding': 0,
+            'line-height': 'inherit',
+            'text-rendering': 'inherit',
+            'white-space': 'normal',
+        }
+        tag = el.tag
+
+        if tag in ('h1', 'h2', 'h3', 'h4', 'h5', 'h6'):
+            style['font-size'] = '100%'
+
+        if tag == 'ul':
+            style['margin'] = '0 0 0 1em'
+        elif tag == 'ol':
+            style['margin'] = '0 0 0 2em'
+        elif tag == 'code':
+            style['color'] = '#4444cc'
+        elif tag == 'blockquote':
+            style.update({
+                'border-left': '1px solid #bbb',
+                'padding': '0 0 0 1em',
+                'margin': '0 0 0 0.5em',
+            })
+        elif tag == 'hr':
+            style['border'] = '1px solid #ddd'
+        elif tag in ('th', 'td'):
+            style.update({
+                'border-bottom': '1px solid #bbb',
+                'padding': '0.2em 1em',
+            })
+
+        # Create a valid CSS string and set it as the style attribute
+        el.set('style', ''.join([
+            '%s: %s;' % (k, v)
+            for k, v in style.iteritems()
+        ]))
+
+        # Recurse into children
+        context.append(tag)
+        for child in el:
+            self.process_element(context, child)
+        context.pop()
+
+    def run(self, root):
+        # We don't just process the root element, because if we add any style
+        # characteristics to it, markdown can't strip off the top-level <div>
+        # tag. Why it doesn't use the element tree to do the stripping is
+        # beyond me.
+        for child in root:
+            self.process_element([], child)
+
+
+class InlineStyleExtension(Extension):
+    def extendMarkdown(self, md, md_globals):
+        md.treeprocessors.add('inlinestyle', InlineStyleProcessor(), '_end')
+
+
+@register.filter
+def markdown_email_html(text, is_rich_text):
+    if not is_rich_text:
+        return text
+
+    marked = markdown.markdown(
+        text,
+        extensions=['fenced_code', 'codehilite(noclasses=True)', 'tables', 'nl2br',
+                    InlineStyleExtension()],
+        output_format='xhtml1',
+        safe_mode='escape')
+    return mark_safe(marked)
