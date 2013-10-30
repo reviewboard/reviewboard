f1ce4a5a6fd336ea9e079429775f422c41f40c36
Christian Hammond <chipx86@chipx86.com>
2013-10-30 02:23:16 -0700

    Update the draft filediff test suite to use the new mixins.
    The draft filediff support was written before all the new test mixins,
    but committed after merging those mixins. It was never updated to use
    them. This change fixes that.

--- reviewboard/webapi/tests/test_draft_filediff.py
+++ reviewboard/webapi/tests/test_draft_filediff.py
@@ -1,85 +1,53 @@
-from djblets.testing.decorators import add_fixtures
-
+from reviewboard.webapi.resources import resources
 from reviewboard.webapi.tests.base import BaseWebAPITestCase
 from reviewboard.webapi.tests.mimetypes import (filediff_item_mimetype,
                                                 filediff_list_mimetype)
+from reviewboard.webapi.tests.mixins import BasicTestsMetaclass
 from reviewboard.webapi.tests.urls import (get_draft_filediff_item_url,
                                            get_draft_filediff_list_url)
 
 
 class ResourceListTests(BaseWebAPITestCase):
     """Testing the DraftFileDiffResource list APIs."""
+    __metaclass__ = BasicTestsMetaclass
+
     fixtures = ['test_users', 'test_scmtools']
+    sample_api_url = 'review-requests/<id>/draft/diffs/<revision>/files/'
+    resource = resources.draft_filediff
 
-    def test_post_method_not_allowed(self):
-        """Testing the
-        POST review-requests/<id>/draft/diffs/<revision>/files/ API
-        gives Method Not Allowed
-        """
-        review_request = self.create_review_request(create_repository=True,
-                                                    submitter=self.user)
+    def compare_item(self, item_rsp, filediff):
+        self.assertEqual(item_rsp['id'], filediff.pk)
+        self.assertEqual(item_rsp['source_file'], filediff.source_file)
+
+    def setup_http_not_allowed_list_test(self, user):
+        review_request = self.create_review_request(
+            create_repository=True,
+            submitter=user)
         diffset = self.create_diffset(review_request, draft=True)
 
-        self.apiPost(
-            get_draft_filediff_list_url(diffset, review_request),
-            expected_status=405)
+        return get_draft_filediff_list_url(diffset, review_request)
 
-    def test_get(self):
-        """Testing the
-        GET review-requests/<id>/draft/diffs/<revision>/files/ API
-        """
-        review_request = self.create_review_request(create_repository=True,
-                                                    submitter=self.user)
+    #
+    # HTTP GET tests
+    #
+
+    def setup_basic_get_test(self, user, with_local_site, local_site_name,
+                             populate_items):
+        review_request = self.create_review_request(
+            create_repository=True,
+            with_local_site=with_local_site,
+            submitter=user)
         diffset = self.create_diffset(review_request, draft=True)
-        filediff = self.create_filediff(diffset)
 
-        rsp = self.apiGet(
-            get_draft_filediff_list_url(diffset, review_request),
-            expected_mimetype=filediff_list_mimetype)
+        if populate_items:
+            items = [self.create_filediff(diffset)]
+        else:
+            items = []
 
-        self.assertEqual(rsp['stat'], 'ok')
-        self.assertEqual(rsp['files'][0]['id'], filediff.pk)
-        self.assertEqual(rsp['files'][0]['source_file'], filediff.source_file)
-
-    @add_fixtures(['test_site'])
-    def test_get_with_site(self):
-        """Testing the
-        GET review-requests/<id>/draft/diffs/<revision>/files/ API
-        with a local site
-        """
-        user = self._login_user(local_site=True)
-
-        review_request = self.create_review_request(create_repository=True,
-                                                    with_local_site=True,
-                                                    submitter=user)
-        diffset = self.create_diffset(review_request, draft=True)
-        filediff = self.create_filediff(diffset)
-
-        rsp = self.apiGet(
-            get_draft_filediff_list_url(diffset, review_request,
-                                        self.local_site_name),
-            expected_mimetype=filediff_list_mimetype)
-
-        self.assertEqual(rsp['stat'], 'ok')
-        self.assertEqual(rsp['files'][0]['id'], filediff.pk)
-        self.assertEqual(rsp['files'][0]['source_file'], filediff.source_file)
-
-    @add_fixtures(['test_site'])
-    def test_get_with_site_no_access(self):
-        """Testing the
-        GET review-requests/<id>/draft/diffs/<revision>/files/ API
-        with a local site and user not on the site
-        """
-        review_request = self.create_review_request(create_repository=True,
-                                                    with_local_site=True)
-        self.assertNotEqual(review_request.submitter, self.user)
-        diffset = self.create_diffset(review_request, draft=True)
-        self.create_filediff(diffset)
-
-        self.apiGet(
-            get_draft_filediff_list_url(diffset, review_request,
-                                        self.local_site_name),
-            expected_status=403)
+        return (get_draft_filediff_list_url(diffset, review_request,
+                                            local_site_name),
+                filediff_list_mimetype,
+                items)
 
     def test_get_not_owner(self):
         """Testing the
@@ -97,81 +65,42 @@ class ResourceListTests(BaseWebAPITestCase):
 
 class ResourceItemTests(BaseWebAPITestCase):
     """Testing the DraftFileDiffResource item APIs."""
+    __metaclass__ = BasicTestsMetaclass
+
     fixtures = ['test_users', 'test_scmtools']
+    sample_api_url = 'review-requests/<id>/draft/diffs/<revision>/files/<id>/'
+    resource = resources.draft_filediff
+    test_http_methods = ('DELETE', 'GET')
 
-    def test_delete_method_not_allowed(self):
-        """Testing the
-        DELETE review-requests/<id>/draft/diffs/<revision>/files/<id>/ API
-        gives Method Not Allowed"""
-        review_request = self.create_review_request(create_repository=True,
-                                                    submitter=self.user)
+    def setup_http_not_allowed_item_test(self, user):
+        review_request = self.create_review_request(
+            create_repository=True,
+            submitter=user)
         diffset = self.create_diffset(review_request, draft=True)
         filediff = self.create_filediff(diffset)
 
-        self.apiDelete(
-            get_draft_filediff_item_url(filediff, review_request),
-            expected_status=405)
+        return get_draft_filediff_item_url(filediff, review_request)
 
-    def test_get(self):
-        """Testing the
-        GET review-requests/<id>/draft/diffs/<revision>/files/<id>/ API
-        """
-        review_request = self.create_review_request(create_repository=True,
-                                                    submitter=self.user)
+    def compare_item(self, item_rsp, filediff):
+        self.assertEqual(item_rsp['id'], filediff.pk)
+        self.assertEqual(item_rsp['source_file'], filediff.source_file)
+
+    #
+    # HTTP GET tests
+    #
+
+    def setup_basic_get_test(self, user, with_local_site, local_site_name):
+        review_request = self.create_review_request(
+            create_repository=True,
+            with_local_site=with_local_site,
+            submitter=user)
         diffset = self.create_diffset(review_request, draft=True)
         filediff = self.create_filediff(diffset)
 
-        rsp = self.apiGet(
-            get_draft_filediff_item_url(filediff, review_request),
-            expected_mimetype=filediff_item_mimetype)
-
-        self.assertEqual(rsp['stat'], 'ok')
-        self.assertEqual(rsp['file']['id'], filediff.pk)
-        self.assertEqual(rsp['file']['source_file'], filediff.source_file)
-
-    @add_fixtures(['test_site'])
-    def test_get_with_site(self):
-        """Testing the
-        GET review-requests/<id>/draft/diffs/<revision>/files/<id>/ API
-        with a local site
-        """
-        user = self._login_user(local_site=True)
-
-        review_request = self.create_review_request(create_repository=True,
-                                                    with_local_site=True,
-                                                    submitter=user)
-        diffset = self.create_diffset(review_request, draft=True)
-        filediff = self.create_filediff(diffset)
-
-        rsp = self.apiGet(
-            get_draft_filediff_item_url(filediff, review_request,
-                                        self.local_site_name),
-            expected_mimetype=filediff_item_mimetype)
-
-        self.assertEqual(rsp['stat'], 'ok')
-        self.assertEqual(rsp['file']['id'], filediff.pk)
-        self.assertEqual(rsp['file']['source_file'], filediff.source_file)
-
-    @add_fixtures(['test_site'])
-    def test_get_with_site_no_access(self):
-        """Testing the
-        GET review-requests/<id>/draft/diffs/<revision>/files/<id>/ API
-        with a local site and user not on the site
-        """
-        user = self._login_user(local_site=True)
-
-        review_request = self.create_review_request(submitter=user,
-                                                    create_repository=True,
-                                                    with_local_site=True)
-        diffset = self.create_diffset(review_request, draft=True)
-        filediff = self.create_filediff(diffset)
-
-        user = self._login_user()
-
-        self.apiGet(
-            get_draft_filediff_item_url(filediff, review_request,
-                                        self.local_site_name),
-            expected_status=403)
+        return (get_draft_filediff_item_url(filediff, review_request,
+                                            local_site_name),
+                filediff_item_mimetype,
+                filediff)
 
     def test_get_not_owner(self):
         """Testing the
@@ -186,17 +115,3 @@ class ResourceItemTests(BaseWebAPITestCase):
         self.apiGet(
             get_draft_filediff_item_url(filediff, review_request),
             expected_status=403)
-
-    def test_put_method_not_allowed(self):
-        """Testing the
-        PUT review-requests/<id>/draft/diffs/<revision>/files/<id>/ API
-        gives Method Not Allowed
-        """
-        review_request = self.create_review_request(create_repository=True,
-                                                    submitter=self.user)
-        diffset = self.create_diffset(review_request, draft=True)
-        filediff = self.create_filediff(diffset)
-
-        self.apiPut(
-            get_draft_filediff_item_url(filediff, review_request),
-            expected_status=405)
