reviewboard/reviewboard

commit c316be6c4558bb3577478909881ee65f5430742f

Add the field mirror_path to RepositoryResource.
This adds the mirror_path field to RepositoryResource. This field
was previously not exposed in the API.

Reviewed at https://reviews.reviewboard.org/r/4897/

reviewboard/webapi/resources/repository.py
@@ -57,6 +57,11 @@ class RepositoryResource(WebAPIResource):
                            'for communicating with the repository and '
                            'accessing files.',
         },
+        'mirror_path': {
+            'type': str,
+            'description': 'An alternate path to the repository, for ',
+                           'lookup purposes.',
+        },
         'visible': {
             'type': bool,
             'description': 'Whether or not this repository is visible (admin '

commit 0f6de4fd3341ccfc6cb170f39f7d9382cde6f7bc

Fix a syntax error.
Change c316be6c4558bb3577478909881ee65f5430742f introduced a syntax error with
an extra comma. Trivial fix.

reviewboard/webapi/resources/repository.py
@@ -59,7 +59,7 @@ class RepositoryResource(WebAPIResource):
         },
         'mirror_path': {
             'type': str,
-            'description': 'An alternate path to the repository, for ',
+            'description': 'An alternate path to the repository, for '
                            'lookup purposes.',
         },
         'visible': {