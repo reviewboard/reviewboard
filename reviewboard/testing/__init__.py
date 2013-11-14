6522cd37e74324cbd7888135aec0ff4bb747af84
David Trowbridge <trowbrds@gmail.com>
2013-11-14 00:02:52 -0800

    Perform unicode migration for reviewboard/testing
    This change represents an audit of all of our string literals to make sure that
    bytestrings are annotated with a leading 'b', and then adds
    "from __future__ import unicode_literals" to the beginning of every non-empty
    file. This also includes some changes to to use six.text_type for python 2/3
    compatibility.
    
    Testing done: Ran unit tests
    
    Reviewed at https://reviews.reviewboard.org/r/4974/

--- reviewboard/testing/__init__.py
+++ reviewboard/testing/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import nose
 from djblets.util.decorators import simple_decorator
 
--- reviewboard/testing/scmtool.py
+++ reviewboard/testing/scmtool.py
@@ -1,3 +1,6 @@
+from __future__ import unicode_literals
+
+from djblets.util.compat import six
 from djblets.util.compat.six.moves import range
 
 from reviewboard.scmtools.core import Branch, Commit, HEAD, SCMTool
@@ -31,8 +34,10 @@ class TestTool(GitTool):
 
     def get_commits(self, start):
         return [
-            Commit('user%d' % i, str(i), '2013-01-01T%02d:00:00.0000000' % i,
-                   'Commit %d' % i, str(i - 1))
+            Commit('user%d' % i, six.text_type(i),
+                   '2013-01-01T%02d:00:00.0000000' % i,
+                   'Commit %d' % i,
+                   six.text_type(i - 1))
             for i in range(int(start), 0, -1)
         ]
 
