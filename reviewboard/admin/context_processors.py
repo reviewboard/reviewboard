f59730aa579be1a0f647456cdfef146b0897d634
Christian Hammond <chipx86@chipx86.com>
2013-11-14 16:23:11 -0800

    Provide pre-defined variables for .less files.
    We now supply development and packaged .less files with a set of global
    variables they can use when packagin. These provide a base path for
    static files (to allow for importing defs and other files), debug
    information, and version information, which can be used for LessCSS
    guards to supply different rules for different versions of Review Board.
    
    There's also a fix for building .less files. We didn't have
    FORCE_BUILD_MEDIA set, causing issues building .less files, depending on
    the development setup.

--- reviewboard/admin/context_processors.py
+++ reviewboard/admin/context_processors.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
-from reviewboard import get_version_string, get_package_version, is_release
+from reviewboard import (get_version_string, get_package_version, is_release,
+                         VERSION)
 
 
 def version(request):
@@ -8,4 +9,5 @@ def version(request):
         'version': get_version_string(),
         'package_version': get_package_version(),
         'is_release': is_release(),
+        'version_raw': VERSION,
     }
--- reviewboard/extensions/packaging.py
+++ reviewboard/extensions/packaging.py
@@ -1,17 +1,37 @@
 from __future__ import unicode_literals
 
+import os
+
 from djblets.extensions.packaging import (
     BuildStaticFiles as DjbletsBuildStaticFiles,
     build_extension_cmdclass)
 from setuptools import setup as setuptools_setup
+
+from reviewboard import VERSION
 
 
 class BuildStaticFiles(DjbletsBuildStaticFiles):
     extension_entrypoint_group = 'reviewboard.extensions'
     django_settings_module = 'reviewboard.settings'
 
+    def get_lessc_global_vars(self):
+        # NOTE: Command (the base class) is not a new-style object, so
+        #       we can't use super().
+        global_vars = DjbletsBuildStaticFiles.get_lessc_global_vars(self)
+        global_vars.update({
+            'RB_MAJOR_VERSION': VERSION[0],
+            'RB_MINOR_VERSION': VERSION[1],
+            'RB_MICRO_VERSION': VERSION[2],
+            'RB_PATCH_VERSION': VERSION[3],
+            'RB_IS_RELEASED': VERSION[5],
+        })
+
+        return global_vars
+
 
 def setup(**setup_kwargs):
+    os.environ['FORCE_BUILD_MEDIA'] = '1'
+
     setup_kwargs.update({
         'zip_safe': False,
         'include_package_data': True,
