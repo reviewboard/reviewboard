365d45c89346b9ea6c32c7b4e720140aa2fe468b
Christian Hammond <chipx86@chipx86.com>
2013-12-18 13:06:15 -0800

    Remove ancient SQL index creation files.
    We had these files from way back when Django didn't auto-create indexes
    for us for Many-to-Many through tables. Now, MySQL's warning about these
    whenever Django tries to apply them, so it's time to get rid of them.

--- reviewboard/accounts/sql/profile.sql
+++
@@ -1,5 +0,0 @@
-CREATE
-	UNIQUE INDEX profile_starred_review_requests__reviewrequest_profile
-	ON accounts_profile_starred_review_requests
-	(reviewrequest_id, profile_id);
-
--- reviewboard/reviews/sql/group.sql
+++
@@ -1,4 +0,0 @@
-CREATE
-	INDEX group_users__group_user
-	ON reviews_group_users
-	(group_id, user_id);
