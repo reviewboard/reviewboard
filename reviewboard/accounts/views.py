13fad21ee66122df58be7ac2b7d3b41c5dbbe9b3
Christian Hammond <chipx86@chipx86.com>
2014-03-04 20:36:54 -0800

    Switch the My Account page to use the new Djblets Config Forms code.
    This places the My Account page classes on top of the Config Forms
    codebase. Config Forms takes care of all the actual rendering and
    management of pages. The My Account classes handle the registration and
    additional work needed by the page. No functionality or presentation
    has changed.

--- reviewboard/accounts/views.py
+++ reviewboard/accounts/views.py
@@ -2,15 +2,17 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import Http404, HttpResponseBadRequest, HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_protect
 from djblets.auth.views import register
+from djblets.configforms.views import ConfigPagesView
 from djblets.siteconfig.models import SiteConfiguration
+from djblets.util.decorators import augment_method_from
 
 from reviewboard.accounts.backends import get_enabled_auth_backends
 from reviewboard.accounts.forms.registration import RegistrationForm
-from reviewboard.accounts.models import Profile
 from reviewboard.accounts.pages import get_page_classes
 from reviewboard.accounts.signals import user_registered
 
@@ -39,57 +41,26 @@ def account_register(request, next_url='dashboard'):
     return HttpResponseRedirect(reverse("login"))
 
 
-@csrf_protect
-@login_required
-def user_preferences(request, template_name='accounts/prefs.html'):
+class MyAccountView(ConfigPagesView):
     """Displays the My Account page containing user preferences.
 
     The page will be built based on registered pages and forms. This makes
     it easy to plug in new bits of UI for the page, which is handy for
     extensions that want to offer customization for users.
     """
-    profile, is_new = Profile.objects.get_or_create(user=request.user)
+    title = _('My Account')
 
-    pages = [
-        page_cls(request, request.user)
-        for page_cls in get_page_classes()
-    ]
+    js_bundle_names = ['account-page']
 
-    forms = {}
+    @method_decorator(login_required)
+    @augment_method_from(ConfigPagesView)
+    def dispatch(self, *args, **kwargs):
+        pass
 
-    # Store a mapping of form IDs to form instances, and check for duplicates.
-    for page in pages:
-        for form in page.forms:
-            # This should already be handled during form registration.
-            assert form.form_id not in forms, \
-                'Duplicate form ID %s (on page %s)' % (
-                    form.form_id, page.page_id)
+    @property
+    def nav_title(self):
+        return self.request.user.username
 
-            forms[form.form_id] = form
-
-    if request.POST:
-        form_id = request.POST.get('form_target')
-
-        if form_id is None:
-            return HttpResponseBadRequest()
-
-        if form_id not in forms:
-            return Http404
-
-        # Replace the form in the list with a new instantiation containing
-        # the form data. If we fail to save, this will ensure the error is
-        # shown on the page.
-        old_form = forms[form_id]
-        form_cls = old_form.__class__
-        form = form_cls(old_form.page, request, request.user, request.POST)
-        forms[form_id] = form
-
-        if form.is_valid():
-            form.save()
-
-            return HttpResponseRedirect(request.path)
-
-    return render(request, template_name, {
-        'pages': pages,
-        'forms': forms.values(),
-    })
+    @property
+    def page_classes(self):
+        return get_page_classes()
