e884a436ef68144765f1d77da63877288347848f
Christian Hammond <chipx86@chipx86.com>
2014-04-08 19:02:08 -0700

    Added a management command for resetting GitHub auth tokens.
    With the new Heartbleed vulnerability in the wild, GitHub recommends
    resetting all authentication tokens, just to be sure. This change
    introduces a management command that makes that somewhat easy.
    
    This command will loop through all GitHub accounts that are linked to
    Review Board, resetting their tokens and replacing them new, valid
    tokens.
    
    If using a registered GitHub OAuth app for Review Board, then the resets
    won't require any credentials. However, for personal tokens (which is
    the standard case), it will require a password, and possibly a
    two-factor auth token.

--- reviewboard/hostingsvcs/github.py
+++ reviewboard/hostingsvcs/github.py
@@ -10,6 +10,7 @@ from django.utils.translation import ugettext_lazy as _
 from djblets.siteconfig.models import SiteConfiguration
 
 from reviewboard.hostingsvcs.errors import (AuthorizationError,
+                                            HostingServiceError,
                                             InvalidPlanError,
                                             RepositoryError,
                                             TwoFactorAuthCodeRequiredError)
@@ -282,12 +283,85 @@ class GitHub(HostingService):
             else:
                 raise AuthorizationError(str(e))
 
-        self.account.data['authorization'] = rsp
-        self.account.save()
+        self._save_auth_data(rsp)
 
     def is_authorized(self):
         return ('authorization' in self.account.data and
                 'token' in self.account.data['authorization'])
+
+    def get_reset_auth_token_requires_password(self):
+        """Returns whether or not resetting the auth token requires a password.
+
+        A password will be required if not using a GitHub client ID or
+        secret.
+        """
+        if not self.is_authorized():
+            return True
+
+        app_info = self.account.data['authorization']['app']
+        client_id = app_info.get('client_id', '')
+        has_client = (client_id.strip('0') != '')
+
+        return (not has_client or
+                (not (hasattr(settings, 'GITHUB_CLIENT_ID') and
+                      hasattr(settings, 'GITHUB_CLIENT_SECRET'))))
+
+    def reset_auth_token(self, password=None, two_factor_auth_code=None):
+        """Resets the authorization token for the linked account.
+
+        This will attempt to reset the token in a few different ways,
+        depending on how the token was granted.
+
+        Tokens linked to a registered GitHub OAuth app can be reset without
+        requiring any additional credentials.
+
+        Tokens linked to a personal account (which is the case on most
+        installations) require a password and possibly a two-factor auth
+        code. Callers should call get_reset_auth_token_requires_password()
+        before determining whether to pass a password, and should pass
+        a two-factor auth code if this raises TwoFactorAuthCodeRequiredError.
+        """
+        if self.is_authorized():
+            token = self.account.data['authorization']['token']
+        else:
+            token = None
+
+        if self.get_reset_auth_token_requires_password():
+            assert password
+
+            if self.account.local_site:
+                local_site_name = self.account.local_site.name
+            else:
+                local_site_name = None
+
+            if token:
+                try:
+                    self._delete_auth_token(
+                        self.account.data['authorization']['id'],
+                        password=password,
+                        two_factor_auth_code=two_factor_auth_code)
+                except HostingServiceError, e:
+                    # If we get a Not Found, then the authorization was
+                    # probably already deleted.
+                    if str(e) != 'Not Found':
+                        raise
+
+                self.account.data['authorization'] = ''
+                self.account.save()
+
+            # This may produce errors, which we want to bubble up.
+            self.authorize(self.account.username, password,
+                           self.account.hosting_url,
+                           two_factor_auth_code=two_factor_auth_code,
+                           local_site_name=local_site_name)
+        else:
+            # We can use the new API for resetting the token without
+            # re-authenticating.
+            auth_data = self._reset_authorization(
+                settings.GITHUB_CLIENT_ID,
+                settings.GITHUB_CLIENT_SECRET,
+                token)
+            self._save_auth_data(auth_data)
 
     def get_file(self, repository, path, revision, *args, **kwargs):
         url = self._build_api_url(self._get_repo_api_url(repository),
@@ -312,6 +386,49 @@ class GitHub(HostingService):
             return True
         except (urllib2.URLError, urllib2.HTTPError):
             return False
+
+    def _reset_authorization(self, client_id, client_secret, token):
+        """Resets the authorization info for an OAuth app-linked token.
+
+        If the token is associated with a registered OAuth application,
+        its token will be reset, without any authentication details required.
+        """
+        url = '%sapplications/%s/tokens/%s' % (
+            self.get_api_url(self.account.hosting_url),
+            client_id,
+            token)
+
+        # Allow any errors to bubble up
+        return self._api_post(url=url,
+                              username=client_id,
+                              password=client_secret)
+
+    def _delete_auth_token(self, auth_id, password, two_factor_auth_code=None):
+        """Requests that an authorization token be deleted.
+
+        This will delete the authorization token with the given ID. It
+        requires a password and, depending on the settings, a two-factor
+        authentication code to perform the deletion.
+        """
+        headers = {}
+
+        if two_factor_auth_code:
+            headers['X-GitHub-OTP'] = two_factor_auth_code
+
+        url = self._build_api_url(
+            '%sauthorizations/%s' % (
+                self.get_api_url(self.account.hosting_url),
+                auth_id))
+
+        self._api_delete(url=url,
+                         headers=headers,
+                         username=self.account.username,
+                         password=password)
+
+    def _save_auth_data(self, auth_data):
+        """Saves authorization data sent from GitHub."""
+        self.account.data['authorization'] = auth_data
+        self.account.save()
 
     def _get_api_error_message(self, rsp, status_code):
         """Return the error(s) reported by the GitHub API, as a string
@@ -380,19 +497,44 @@ class GitHub(HostingService):
         return self._api_get(self._build_api_url(
             self._get_repo_api_url_raw(owner, repo_name)))
 
-    def _api_get(self, url):
+    def _api_get(self, url, *args, **kwargs):
         try:
-            data, headers = self._http_get(url)
-            return simplejson.loads(data)
+            data, headers = self._json_get(url, *args, **kwargs)
+            return data
         except (urllib2.URLError, urllib2.HTTPError), e:
-            data = e.read()
+            self._check_api_error(e)
 
-            try:
-                rsp = simplejson.loads(data)
-            except:
-                rsp = None
+    def _api_post(self, url, *args, **kwargs):
+        try:
+            data, headers = self._json_post(url, *args, **kwargs)
+            return data
+        except (urllib2.URLError, urllib2.HTTPError), e:
+            self._check_api_error(e)
 
-            if rsp and 'message' in rsp:
-                raise Exception(rsp['message'])
-            else:
-                raise Exception(str(e))
+    def _api_delete(self, url, *args, **kwargs):
+        try:
+            data, headers = self._json_delete(url, *args, **kwargs)
+            return data
+        except (urllib2.URLError, urllib2.HTTPError), e:
+            self._check_api_error(e)
+
+    def _check_api_error(self, e):
+        data = e.read()
+
+        try:
+            rsp = simplejson.loads(data)
+        except:
+            rsp = None
+
+        if rsp and 'message' in rsp:
+            response_info = e.info()
+            x_github_otp = response_info.get('X-GitHub-OTP', '')
+
+            if x_github_otp.startswith('required;'):
+                raise TwoFactorAuthCodeRequiredError(
+                    _('Enter your two-factor authentication code. '
+                      'This code will be sent to you by GitHub.'))
+
+            raise HostingServiceError(rsp['message'])
+        else:
+            raise HostingServiceError(str(e))
---
+++ reviewboard/hostingsvcs/management/commands/reset-github-tokens.py
@@ -0,0 +1,89 @@
+import getpass
+import sys
+from optparse import make_option
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext_lazy as _
+from djblets.siteconfig.models import SiteConfiguration
+
+from reviewboard.hostingsvcs.errors import (AuthorizationError,
+                                            TwoFactorAuthCodeRequiredError)
+from reviewboard.hostingsvcs.models import HostingServiceAccount
+
+
+class Command(BaseCommand):
+    help = _('Resets associated GitHub tokens')
+
+    option_list = BaseCommand.option_list + (
+        make_option('--yes',
+                    action='store_true',
+                    default=False,
+                    dest='force_yes',
+                    help=_('Answer yes to all questions')),
+        make_option('--local-sites',
+                    action='store',
+                    dest='local_sites',
+                    help=_('Comma-separated list of Local Sites to '
+                           'filter by')),
+    )
+
+    def handle(self, *usernames, **options):
+        force_yes = options['force_yes']
+        local_sites = options['local_sites']
+
+        accounts = HostingServiceAccount.objects.filter(service_name='github')
+
+        if usernames:
+            accounts = accounts.filter(username__in=usernames)
+
+        if local_sites:
+            local_site_names = local_sites.split(',')
+
+            if local_site_names:
+                accounts = accounts.filter(
+                    local_site__name__in=local_site_names)
+
+        for account in accounts:
+            if force_yes:
+                reset = 'y'
+            else:
+                if account.local_site:
+                    reset_msg = _('Reset token for %s (%s) [Y/n] ') % (
+                        account.local_site.name,
+                        account.username)
+                else:
+                    reset_msg = _('Reset token for %s [Y/n] ') % (
+                        account.username)
+
+                reset = raw_input(reset_msg)
+
+            if reset != 'n':
+                self._reset_token(account)
+
+    def _reset_token(self, account):
+        service = account.service
+        password = None
+        auth_token = None
+
+        while True:
+            if (not password and
+                service.get_reset_auth_token_requires_password()):
+                password = getpass.getpass(_('Password for %s: ')
+                                           % account.username)
+                auth_token = None
+
+            try:
+                service.reset_auth_token(password, auth_token)
+
+                self.stdout.write(_('Successfully reset token for %s\n')
+                                  % account.username)
+                break
+            except TwoFactorAuthCodeRequiredError:
+                auth_token = raw_input('Enter your two-factor auth token: ')
+            except AuthorizationError, e:
+                self.stderr.write('%s\n' % e)
+                password = None
+            except Exception, e:
+                self.stderr.write(_('Unexpected error: %s\n') % e)
+                break
