20173d846dd04f71d50f8da09a202ab1d91efc30
Christian Hammond <chipx86@chipx86.com>
2014-05-12 20:22:03 -0700

    Condense some unnecessarily verbose code in reviewboard.admin.
    Due to changes made over time to the admin widget code, we ended up
    having some code in reviewboard.admin.signals that didn't really belong
    there. A signals.py file should be fairly minimal and contain actual
    signals, but this one had just signal handlers that were called from
    reviewboard.admin itself.
    
    This change just removes the file and moves that code directly into
    reviewboard.admin, to keep things simple and consistent.
    
    Reviewed at https://reviews.reviewboard.org/r/5812/

--- reviewboard/admin/__init__.py
+++ reviewboard/admin/__init__.py
@@ -2,9 +2,14 @@ from __future__ import unicode_literals
 
 from django.db.models.signals import post_save, post_delete
 
-from reviewboard.admin.signals import _delete_widget_cache
+from reviewboard.admin.widgets import increment_sync_num
 from reviewboard.reviews.models import Group
 from reviewboard.scmtools.models import Repository
+
+
+def _delete_widget_cache(*args, **kwargs):
+    """Clear the cache to keep the admin dashboard up to date."""
+    increment_sync_num()
 
 
 post_save.connect(_delete_widget_cache, sender=Group)
