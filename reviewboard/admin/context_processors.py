9ad221613ce6fa1636133148600d6081174d0479
Christian Hammond <chipx86@chipx86.com>
2014-05-19 17:15:18 -0700

    Always link to the current documentation URL.
    We've always had to manually update the various URLs to the user manual,
    and we've never been very consistent about doing that. However, this is
    pretty easy to compute, so there's not much reason for us to have to
    change anything.
    
    There's now a new get_manual_url() function that's used to populate the
    URL for the web UI and for rb-site. This will use a versioned manual if
    it's a released build tagged 'final', or the 'dev' manual otherwise.
    
    Reviewed at https://reviews.reviewboard.org/r/5854/

--- reviewboard/admin/context_processors.py
+++ reviewboard/admin/context_processors.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
-from reviewboard import (get_version_string, get_package_version, is_release,
-                         VERSION)
+from reviewboard import (get_manual_url, get_package_version,
+                         get_version_string, is_release, VERSION)
 
 
 def version(request):
@@ -10,4 +10,5 @@ def version(request):
         'package_version': get_package_version(),
         'is_release': is_release(),
         'version_raw': VERSION,
+        'RB_MANUAL_URL': get_manual_url(),
     }
