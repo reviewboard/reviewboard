d9130539638d49dffbe36543c1fc3bfbae51a0ee
Christian Hammond <chipx86@chipx86.com>
2014-05-26 18:19:27 -0700

    Bump to 2.0.2 dev.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (2, 0, 1, 0, 'final', 0, True)
+VERSION = (2, 0, 2, 0, 'final', 0, False)
 
 
 # Required version of Django
