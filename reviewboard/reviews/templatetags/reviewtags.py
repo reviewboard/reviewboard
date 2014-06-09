260619e2530f6ca7d1ff7f4eb38e5849b61a4087
David Trowbridge <trowbrds@gmail.com>
2014-06-08 21:27:31 -0700

    Do a better job of escaping text in commentcounts tag.
    If a user set their full name to include </script>, they could cause some
    problems. Most of this was fixed in Djblets 0.7.30 and 0.8.3, but the
    commentcounts tag in Review Board 1.7.x didn't use json_dumps, and therefore
    still had the problem.
    
    This change adds some more escaping for different parts of the commentcounts
    structure that gets serialized.
    
    Testing done:
    * Tested the view_diff.html template.
    * Ran unit tests
    
    Reviewed at https://reviews.reviewboard.org/r/5955/

--- reviewboard/reviews/templatetags/reviewtags.py
+++ reviewboard/reviews/templatetags/reviewtags.py
@@ -108,17 +108,18 @@ def commentcounts(context, filediff, interfilediff=None):
                 'line': comment.first_line,
                 'num_lines': comment.num_lines,
                 'user': {
-                    'username': review.user.username,
-                    'name': review.user.get_full_name() or review.user.username,
+                    'username': escape(review.user.username),
+                    'name': escape(review.user.get_full_name() or
+                                   review.user.username),
                 },
                 'url': comment.get_review_url(),
-                'localdraft': review.user == user and \
-                              not review.public,
+                'localdraft': (review.user == user and
+                               not review.public),
                 'review_id': review.id,
                 'review_request_id': review.review_request.id,
                 'issue_opened': comment.issue_opened,
-                'issue_status': BaseComment
-                                .issue_status_to_string(comment.issue_status),
+                'issue_status': escape(
+                    BaseComment.issue_status_to_string(comment.issue_status)),
             })
 
     comments_array = []
