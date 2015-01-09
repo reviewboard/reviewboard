d095085898804a877fad3aea41e835950c4f077e
Stephen Gallagher <sgallagh@fedoraproject.org>
2015-01-08 16:48:11 -0800

    Create new sites in a default path
    This lays the groundwork for better support of SELinux on platforms that
    support it. SELinux rules are based in large part on filesystem location. By
    installing sites into a default path, we can create the SELinux rules on those
    standard paths.
    
    This patch also moves a few global variables into a new platform.py file. This
    is so that packagers for Linux distributions have an easy place to identify
    things like filesystem paths that commonly need to be modified to meet the
    system's packaging guidelines.
    
    NOTE: This patch changes the current behavior in one significant place: rb-site
    can no longer reference relative paths for the site directory. Any site input
    that is not absolute will be assumed to be relative to the default site path
    instead of the current directory.
    
    Testing Done:
    I created a site using 'rb-site install rb.example.com', which succeeded. I then also tested 'rb-site upgrade rb.example.com', which also succeeded.
    
    Reviewed at https://reviews.reviewboard.org/r/6682/

---
+++ reviewboard/platform.py
@@ -0,0 +1,26 @@
+# platform.py
+# This file contains global values that may need to be
+# modified for deployment on different operating system
+# platforms.
+# Packagers should review this file and patch it
+# appropriately for their systems.
+from __future__ import unicode_literals
+
+
+# Location of the sitelist file. This file maintains
+# a list of the installed Review Board sites on this
+# machine and is used when performing a site upgrade
+# to ensure all sites are upgraded together.
+SITELIST_FILE_UNIX = "/etc/reviewboard/sites"
+
+
+# Default location of the cache directory. This path
+# is used in 'rb-site install' if using a file-based
+# cache instead of a memcached-based cache.
+DEFAULT_FS_CACHE_PATH = "/tmp/reviewboard_cache"
+
+
+# Preferred location of the reviewboard sites.
+# If the rb-site tool is passed a site name instead of
+# a full path, it will be prepended with this path.
+INSTALLED_SITE_PATH = "/var/www"
