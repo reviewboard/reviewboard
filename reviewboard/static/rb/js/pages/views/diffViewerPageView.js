937e17275cbab40122aaa1d362a352c7ad47c37c
David Trowbridge <trowbrds@gmail.com>
2015-03-20 13:21:48 -0700

    Fix file links in multi-page diff views.
    When using the file links in diffs that had multiple pages, the scroll position
    would jump to the file, but then the diff viewer would immediately reload on
    page 1. The issue here is that we were setting location.hash in order to update
    the displayed URL to include the page number, and the backbone router was
    interpreting that as an indication that it should reroute.
    
    I've changed it to use `router.navigate()` with `trigger: false`. This updates
    the displayed hash without actually triggering any routing.
    
    Testing done:
    Created a giant multi-page diff and loaded page 2. Clicked on a filename in the
    file list. Saw that it jumped to the file and updated the displayed URL
    correctly, and did not navigate back to page 1.
    
    Reviewed at https://reviews.reviewboard.org/r/7096/

--- reviewboard/static/rb/js/pages/views/diffViewerPageView.js
+++ reviewboard/static/rb/js/pages/views/diffViewerPageView.js
@@ -375,7 +375,8 @@ RB.DiffViewerPageView = RB.ReviewablePageView.extend({
      */
     selectAnchor: function($anchor, scroll) {
         var scrollAmount,
-            i;
+            i,
+            url;
 
         if (!$anchor || $anchor.length === 0 ||
             $anchor.parent().is(':hidden')) {
@@ -383,7 +384,17 @@ RB.DiffViewerPageView = RB.ReviewablePageView.extend({
         }
 
         if (scroll !== false) {
-            location.hash = "#" + $anchor.attr("name");
+            url = [
+                this._getCurrentURL(),
+                location.search,
+                '#',
+                $anchor.attr('name')
+            ].join('');
+
+            this.router.navigate(url, {
+                replace: true,
+                trigger: false
+            });
 
             scrollAmount = this.DIFF_SCROLLDOWN_AMOUNT;
 
@@ -604,17 +615,29 @@ RB.DiffViewerPageView = RB.ReviewablePageView.extend({
     },
 
     /*
-     * Callback for when a new page is selected.
+     * Get the current URL.
      *
-     * Navigates to the same revision with a different page number.
+     * This will compute and return the current page's URL (relative to the
+     * router root), not including query parameters or hash locations.
      */
-    _onPageSelected: function(scroll, page) {
+    _getCurrentURL: function() {
         var revision = this.model.get('revision'),
             url = revision.get('revision');
 
         if (revision.get('interdiffRevision') !== null) {
             url += '-' + revision.get('interdiffRevision');
         }
+
+        return url;
+    },
+
+    /*
+     * Callback for when a new page is selected.
+     *
+     * Navigates to the same revision with a different page number.
+     */
+    _onPageSelected: function(scroll, page) {
+        var url = this._getCurrentURL();
 
         if (scroll) {
             this.selectAnchorByName('index_header', true);
