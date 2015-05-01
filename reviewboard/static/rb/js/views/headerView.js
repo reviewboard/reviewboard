98205e5b175f4daddaf3ce02fdd9e2bef4dd270c
Christian Hammond <christian@beanbaginc.com>
2015-04-30 22:10:47 -0700

    Refine the responsive UI support.
    In 2.5, we gained the beginnings of response UI support, featuring a
    mobile-friendly page header, a pop-down search, and a slide-out sidebar
    for basic navigation.
    
    This builds upon that work to provide a more standardized UI for mobile
    devices and to make it easier to integrate with the slide-out sidebar
    for navigation.
    
    The page header now has a standard "hamburger menu" icon (the three
    stacked lines), which pulls out the sidebar. The sidebar appears
    recessed from the page's point of view, with a small shadow covering
    part of it. The page itself is "pushed" to the right, rather than the
    sidebar overlapping the page. While visible, the page cannot be
    scrolled. This uses CSS3 transitions to animate, improving the
    performance on mobile devices over jQuery animation.
    
    This sidebar contains links at the top for logging in/out, registering,
    and viewing account settings. Below that is the general page navigation
    (New review request, dashboard, etc.). Below that is any page-specific
    navigation (such as the dashboard sidebar). Docked to the bottom of the
    menu are a set of links for Docs, Support, and the Administration UI (if
    logged in as an administrator).
    
    The page header now always shows the search bar, instead of requiring
    another tap to show search. That makes it easier to start a search
    (useful on mobile devices) and frees up room in the top-right for future
    expansion.
    
    Search results appear much like they do on other mobile apps, taking up
    the full page content and giving enough room to easily tap to navigate.
    This is an improvement over the default implementation, which simply
    drops down a small list of results.
    
    The result of the change is a friendlier UI that we'll be able to build
    upon for all future responsive UI support.
    
    Testing Done:
    Tested on Chrome, Firefox, iPhone 6, and Nexus 7 (Chrome) in both mobile
    and non-mobile modes.
    
    I tested:
    
    * Pages with and without custom content in the sidebar.
    * Opening the sidebar and navigating to all the different links.
    * Closing the sidebar by tapping on the page content (and ensuring that doesn't trigger links there)
    * Searching and tapping results.
    * Going in and out of responsive mode on browsers.
    * Landscape mode on the iPhone and Nexus 7.
    * Sidebar content when logged in/out.
    
    Reviewed at https://reviews.reviewboard.org/r/7257/

--- reviewboard/static/rb/js/views/headerView.js
+++ reviewboard/static/rb/js/views/headerView.js
@@ -2,11 +2,13 @@
  * Site Header
  */
 RB.HeaderView = Backbone.View.extend({
-    MOBILE_MENU_MAX_WIDTH: 720,
+    SEARCH_SUMMARY_TRIM_LEN: 28,
+    DESKTOP_SEARCH_RESULTS_WIDTH: 350,
 
     events: {
-        'click #logo': '_handleLogoClick',
-        'click #search-icon': '_toggleSearchBar'
+        'click #nav_toggle': '_handleNavClick',
+        'touchstart #nav_toggle': '_handleNavClick',
+        'focus #search_field': '_closeMobileMenu'
     },
 
     /*
@@ -15,77 +17,157 @@ RB.HeaderView = Backbone.View.extend({
     initialize: function() {
         this._mobileMenuOpened = false;
         this._$window = $(window);
+        this._$body = $(document.body);
+        this._$navToggle = $('#nav_toggle');
+
+        this._$mobileMenuMask = $('<div id="mobile_menu_mask"/>')
+            .on('click touchstart', _.bind(this._closeMobileMenu, this))
+            .insertAfter($('#mobile_navbar_container'));
+
+        this._setupSearch();
 
         this._$window.on('resize', _.throttle(_.bind(function() {
-            if (this._$window.width() > this.MOBILE_MENU_MAX_WIDTH) {
-                this._closeMobileMenu();
-            }
+            this._setMobileMode(this._$navToggle.is(':visible'));
         }, this), 100));
-
-        this._$mobileMenuMask = $('#mobile-menu-mask')
-            .click(_.bind(this._closeMobileMenu, this));
-        this._$navbarContainer = $('#navbar-container');
-        this._$body = $('body');
     },
 
-    _handleLogoClick: function() {
-        if (this._$window.width() <= this.MOBILE_MENU_MAX_WIDTH) {
-            this._mobileMenuToggle(
-                !$('#navbar-container').hasClass('menu-active'));
-        } else {
-            window.location.href = SITE_ROOT;
-        }
+    _handleNavClick: function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        this._setMobileMenuOpened(!this._mobileMenuOpened);
     },
 
-    _toggleSearchBar: function() {
-        this.$('#search').toggle();
-    },
-
-    _closeMobileMenu: function() {
-        this._mobileMenuToggle(false);
-    },
-
-    /*
-     * Mobile Menu toggle for Navbar.
-     * This will control the open and close of mobile navbar menu and menu mask
-     *
-     * @param {bool} toggle   To open or close the menu
-     */
-    _mobileMenuToggle: function(toggle) {
-        if (toggle === this._mobileMenuOpened) {
+    _setMobileMode: function(inMobileMode) {
+        if (inMobileMode === this._inMobileMode) {
             return;
         }
 
-        if (toggle) {
-            this._$navbarContainer.animate({
-                left: '0px'
-            }, function() {
-                $(this).addClass('menu-active');
-            });
-
-            this._$mobileMenuMask
-                .css('opacity', 0)
-                .show()
-                .animate({
-                    opacity: 0.5
-                });
-            this._$body.css('overflow', 'hidden');
-        } else {
-            this._$body.css('overflow', '');
-            this._$mobileMenuMask
-                .animate({
-                    opacity: 0
-                }, function() {
-                    $(this).hide();
-                });
-
-            this._$navbarContainer.animate({
-                left: '-160px'
-            }, function() {
-                $(this).removeClass('menu-active');
-            });
+        if (!inMobileMode) {
+            this._setMobileMenuOpened(false);
         }
 
-        this._mobileMenuOpened = toggle;
+        this._inMobileMode = inMobileMode;
+    },
+
+    /*
+     * Closes the mobile menu.
+     *
+     * This is used as an event handler, to make it easy to close the
+     * mobile menu and prevent any bubbling or default actions from the
+     * event.
+     */
+    _closeMobileMenu: function() {
+        this._setMobileMenuOpened(false);
+
+        return false;
+    },
+
+    /*
+     * Sets up the search auto-complete field.
+     */
+    _setupSearch: function() {
+        this._$search = $('#search_field').rbautocomplete({
+            formatItem: _.bind(function(data) {
+                var s;
+
+                if (data.username) {
+                    // For the format of users
+                    s = data.username;
+
+                    if (data.fullname) {
+                        s += " <span>(" + _.escape(data.fullname) + ")</span>";
+                    }
+
+                } else if (data.name) {
+                    // For the format of groups
+                    s = data.name;
+                    s += " <span>(" + _.escape(data.display_name) + ")</span>";
+                } else if (data.summary) {
+                    // For the format of review requests
+                    if (data.summary.length < this.SEARCH_SUMMARY_TRIM_LEN) {
+                        s = data.summary;
+                    } else {
+                        s = data.summary.substring(
+                            0, this.SEARCH_SUMMARY_TRIM_LEN);
+                    }
+
+                    s += " <span>(" + _.escape(data.id) + ")</span>";
+                }
+
+                return s;
+            }, this),
+            matchCase: false,
+            multiple: false,
+            clickToURL: true,
+            selectFirst: false,
+            width: this.DESKTOP_SEARCH_RESULTS_WIDTH,
+            enterToURL: true,
+            resultsParentEl: $('#page-container'),
+            parse: function(data) {
+                var jsonData = data,
+                    jsonDataSearch = jsonData.search,
+                    parsed = [],
+                    objects = ["users", "groups", "review_requests"],
+                    values = ["username", "name", "summary"],
+                    value,
+                    items,
+                    i,
+                    j;
+
+                for (j = 0; j < objects.length; j++) {
+                    items = jsonDataSearch[objects[j]];
+
+                    for (i = 0; i < items.length; i++) {
+                        value = items[i];
+
+                        if (j !== 2) {
+                            parsed.push({
+                                data: value,
+                                value: value[values[j]],
+                                result: value[values[j]]
+                            });
+                        } else if (value['public']) {
+                            // Only show review requests that are public
+                            value.url = SITE_ROOT + "r/" + value.id;
+                            parsed.push({
+                                data: value,
+                                value: value[values[j]],
+                                result: value[values[j]]
+                            });
+                        }
+                    }
+                }
+
+                return parsed;
+            },
+            url: SITE_ROOT + "api/" + "search/"
+        });
+    },
+
+
+    /*
+     * Set whether the mobile menu is opened.
+     */
+    _setMobileMenuOpened: function(opened) {
+        if (opened === this._mobileMenuOpened) {
+            return;
+        }
+
+        if (opened) {
+            this._$mobileMenuMask.show();
+
+            _.defer(_.bind(function() {
+                this._$body.addClass('mobile-menu-open');
+            }, this));
+        } else {
+            this._$body.removeClass('mobile-menu-open');
+
+            _.delay(_.bind(function() {
+                this._$mobileMenuMask.hide();
+            }, this), 300);
+        }
+
+        this._mobileMenuOpened = opened;
     }
 });
