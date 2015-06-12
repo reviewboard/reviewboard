ce13ecc2cc42fea4243c9b9a48a2a87287386ce6
Christian Hammond <christian@beanbaginc.com>
2015-06-12 01:12:16 -0700

    Bump to 2.0.17 dev.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (2, 0, 16, 0, 'final', 0, True)
+VERSION = (2, 0, 17, 0, 'alpha', 0, False)
 
 
 # Required version of Django
