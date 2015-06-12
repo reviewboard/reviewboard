d754f673f6a9befb2e21e2981268b8e7d1902784
Christian Hammond <christian@beanbaginc.com>
2015-06-11 20:47:28 -0700

    Release Review Board 2.0.16.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -7,11 +7,11 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (2, 0, 16, 0, 'alpha', 0, False)
+VERSION = (2, 0, 16, 0, 'final', 0, True)
 
 
 # Required version of Django
-django_version = 'Django>=1.6.10,<1.7'
+django_version = 'Django>=1.6.11,<1.7'
 
 
 def get_version_string():
