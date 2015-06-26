4ede7110112955731dc8790afa96aed77845517f
Christian Hammond <christian@beanbaginc.com>
2015-06-26 00:04:09 -0700

    Improve the diff chunk selector.
    The diff chunk selector is now cleaner, both visually and code-wise.
    
    The big borders are gone. Instead, we have highlighting on the side,
    which no longer has a "jumpy" effect like the borders have. They also
    don't have the offset problems, nor do they cover up content.
    
    The code is a bit cleaner. This class is no longer a singleton, and
    instead is owned by the page. Recalculating the positions and sizes is
    less expensive, and done as infrequently as possible.
    
    The result is a lighter-weight, less noisy chunk selector.
    
    Testing Done:
    Played around with the selector on different browsers. The results were
    consistent.
    
    Jumped all around the page, back-and-forth, with arrow keys and with clicking.
    
    Tried zooming the page in/out, and resizing, while watching console.log
    output. We calculated infrequently.
    
    Reviewed at https://reviews.reviewboard.org/r/7454/

--- reviewboard/static/rb/js/diffviewer/views/chunkHighlighterView.js
+++ reviewboard/static/rb/js/diffviewer/views/chunkHighlighterView.js
@@ -15,18 +15,11 @@ RB.ChunkHighlighterView = Backbone.View.extend({
      * Initializes the highlighter.
      */
     initialize: function() {
-        this._$top = null;
-        this._$bottom = null;
-        this._$left = null;
-        this._$right = null;
-        this._borderWidth = null;
-        this._borderHeight = null;
-        this._borderOffsetX = null;
-        this._borderOffsetY = null;
-
         this._resetState();
+        this._$pageContainer = null;
+        this._width = null;
 
-        _.bindAll(this, '_queueUpdatePosition', '_updatePosition');
+        _.bindAll(this, '_updatePosition');
     },
 
     /*
@@ -36,40 +29,20 @@ RB.ChunkHighlighterView = Backbone.View.extend({
      * we want to keep around for further rendering.
      */
     render: function() {
-        var $border = $('<div class="diff-highlight-border"/>');
+        $(window).on('resize.' + this.cid, _.throttle(_.bind(function() {
+            this._recalcGlobalSizes();
+            this._resetChunkSizes();
 
-        this._$top = $border.clone().appendTo(this.$el);
-        this._$bottom = $border.clone().appendTo(this.$el);
-        this._$left = $border.clone().appendTo(this.$el);
-        this._$right = $border.clone().appendTo(this.$el);
-
-        this._borderWidth = this._$left.width();
-        this._borderHeight = this._$top.height();
-        this._borderOffsetX = this._borderWidth / 2;
-        this._borderOffsetY = this._borderHeight / 2;
-
-        if ($.browser.msie) {
-            if ($.browser.version <= 9) {
-                /* On IE <= 9, the black rectangle is too far to the top. */
-                this._borderOffsetY = 0;
-            } else {
-                /* On IE >= 10, the black rectangle is too far down. */
-                this._borderOffsetY = this._borderHeight;
-            }
-
-            if ($.browser.version === 8) {
-                /* And on IE8, it's also too far to the left. */
-                this._borderOffsetX = -this._borderOffsetX;
-            }
-        }
-
-        $(window).on('resize.' + this.cid, _.bind(function() {
             /*
              * Other operations may impact the size of the page, so do this
              * after all resize handlers have been called.
              */
             _.defer(this._updatePosition);
-        }, this));
+        }, this)));
+
+        this._$pageContainer = $('#page-container');
+
+        this._recalcGlobalSizes();
 
         return this;
     },
@@ -93,7 +66,9 @@ RB.ChunkHighlighterView = Backbone.View.extend({
         this._resetState();
 
         this._$chunk = $chunk;
-        this._queueUpdatePosition();
+        this._$chunkContainer = $chunk.parents('.diff-container');
+
+        this._updatePosition();
     },
 
     /*
@@ -101,24 +76,32 @@ RB.ChunkHighlighterView = Backbone.View.extend({
      */
     _resetState: function() {
         this._$chunk = null;
-        this._oldTop = null;
-        this._oldWidth = null;
-        this._oldHeight = null;
+        this._$chunkContainer = null;
+
+        this._resetChunkSizes();
+    },
+
+    /*
+     * Resets the calculated offsets and sizes for a chunk.
+     */
+    _resetChunkSizes: function() {
+        this._top = null;
+        this._height = null;
+    },
+
+    /*
+     * Re-calculates the common sizes for the page container.
+     */
+    _recalcGlobalSizes: function() {
+        this._width = this._$pageContainer.outerWidth();
+
+        this.$el.css('left', -this._$pageContainer.getExtents('p', 'l'));
     },
 
     /*
      * Updates the position of the borders, based on the chunk dimensions.
      */
     _updatePosition: function(e) {
-        var $container,
-            offset,
-            top,
-            width,
-            height,
-            outerHeight,
-            outerWidth,
-            outerTop;
-
         if (e && e.target && e.target !== window &&
             !e.target.getElementsByTagName) {
             /*
@@ -128,101 +111,34 @@ RB.ChunkHighlighterView = Backbone.View.extend({
             return;
         }
 
-        offset = this._$chunk.position();
-
-        if (!offset) {
+        if (this._$chunk === null) {
             return;
         }
 
-        $container = this._$chunk.parents('.diff-container');
+        if (this._top === null) {
+            this._top = Math.floor(this._$chunk.position().top +
+                                   this._$chunkContainer.position().top);
+        }
 
-        /*
-         * Attach the element to the container, so it'll move along with
-         * the container if anything above moves.
-         */
-        this.$el
-            .remove()
-            .appendTo($container);
+        if (this._height === null) {
+            this._height = this._$chunk.outerHeight();
+        }
 
-        top = Math.floor(offset.top);
-        width = $container.outerWidth();
-        height = this._$chunk.outerHeight();
-
-        if (top === this._oldTop &&
-            width === this._oldWidth &&
-            height === this._oldHeight) {
+        if (this._top === this._oldTop &&
+            this._width === this._oldWidth &&
+            this._height === this._oldHeight) {
             /* The position and size haven't actually changed. */
             return;
         }
 
-        outerHeight = height + this._borderOffsetX;
-        outerWidth  = width - this._borderWidth;
-        outerTop    = top - this._borderOffsetY;
-
-        this._$left.css({
-            left: 0,
-            top: outerTop,
-            height: outerHeight
+        this.$el.css({
+            top: this._top,
+            height: this._height + 1,  // Compensate for the tbody border below.
+            width: this._width
         });
 
-        this._$top.css({
-            left: 0,
-            top: outerTop,
-            width: outerWidth
-        });
-
-        this._$right.css({
-            left: outerWidth,
-            top: outerTop,
-            height: outerHeight
-        });
-
-        this._$bottom.css({
-            left: 0,
-            top: outerTop + outerHeight,
-            width: outerWidth + this._borderWidth
-        });
-
-        this._oldTop = top;
-        this._oldWidth = width;
-        this._oldHeight = height;
-    },
-
-    /*
-     * Queues an update of the border positions.
-     *
-     * Since this may be called many times in quick succession, it will
-     * only ever actually trigger updating the position when the events
-     * have calmed down. It has a 50ms throttle.
-     */
-    _queueUpdatePosition: _.debounce(function(e) {
-        this._updatePosition(e);
-    }, 50)
-}, {
-    _instance: null,
-
-    /*
-     * Highlights a chunk on the page.
-     *
-     * If a highlighter isn't already created, this will create one.
-     */
-    highlight: function($chunk) {
-        if (!$chunk || $chunk.length === 0) {
-            return;
-        }
-
-        if (!this._instance) {
-            this._instance = new RB.ChunkHighlighterView();
-
-            /*
-             * This placement is temporary, in order to calculate the
-             * sizes and offsets. It will be moved in the highlight call
-             * below.
-             */
-            this._instance.$el.appendTo($('#diffs'));
-            this._instance.render();
-        }
-
-        this._instance.highlight($chunk);
+        this._oldTop = this._top;
+        this._oldWidth = this._width;
+        this._oldHeight = this._height;
     }
 });
