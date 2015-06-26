d2de9296f3da2a743280c02c04839291c9f7c3eb
Christian Hammond <christian@beanbaginc.com>
2015-06-25 23:53:43 -0700

    Reset rich text of review request fields when importing from commits.
    In our original version of our Markdown support, we had a global
    rich_text flag for an entire review request. Because of that, we needed
    to handle escaping text coming from commits or change descriptions (when
    updating from those), in order to prevent the imported text from
    rendering as Markdown if the review request was set to rich text mode.
    
    We don't have this limitation anymore, and have had reports of
    frustration where a commit message expected to be plain text ended up
    being escaped.
    
    We now reset the flags instead for any field we import, triggering the
    expected behavior. The text will still appear escaped for edit if the
    user has that option enabled, but won't appear escaped otherwise.
    
    Testing Done:
    Unit tests were updated to check for the different code paths for this.
    They pass.
    
    Bugs closed: 3898
    
    Reviewed at https://reviews.reviewboard.org/r/7452/

--- reviewboard/reviews/models/base_review_request_details.py
+++ reviewboard/reviews/models/base_review_request_details.py
@@ -9,7 +9,6 @@ from django.utils.translation import ugettext_lazy as _
 from djblets.db.fields import JSONField
 
 from reviewboard.diffviewer.models import DiffSet
-from reviewboard.reviews.markdown_utils import markdown_escape
 from reviewboard.reviews.models.default_reviewer import DefaultReviewer
 from reviewboard.scmtools.errors import InvalidChangeNumberError
 
@@ -216,17 +215,13 @@ class BaseReviewRequestDetails(models.Model):
         description = changeset.description
         testing_done = changeset.testing_done
 
-        if self.description_rich_text:
-            description = markdown_escape(description)
-
-        if self.testing_done_rich_text:
-            testing_done = markdown_escape(testing_done)
-
         self.summary = changeset.summary
         self.description = description
+        self.description_rich_text = False
 
         if testing_done:
             self.testing_done = testing_done
+            self.testing_done_rich_text = False
 
         if changeset.branch:
             self.branch = changeset.branch
@@ -247,10 +242,8 @@ class BaseReviewRequestDetails(models.Model):
         self.commit = commit_id
         self.summary = summary.strip()
 
-        if self.description_rich_text:
-            self.description = markdown_escape(message)
-        else:
-            self.description = message
+        self.description = message
+        self.description_rich_text = False
 
         DiffSet.objects.create_from_data(
             repository=self.repository,
