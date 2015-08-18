030b3008079fc3d366e4bb7737145ad0fb235228
Christian Hammond <christian@beanbaginc.com>
2015-08-17 18:04:44 -0700

    Bump to 2.5 RC 2 dev.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -13,7 +13,7 @@ from __future__ import unicode_literals
 #:
 #: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #:
-VERSION = (2, 5, 0, 0, 'rc', 1, True)
+VERSION = (2, 5, 0, 0, 'rc', 2, False)
 
 
 #: The major version of Django we're using.
