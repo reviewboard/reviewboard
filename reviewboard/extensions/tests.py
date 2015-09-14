3255fa739f68cc53a9236f8ef6de6bffb8536fa1
Christian Hammond <christian@beanbaginc.com>
2015-09-13 18:52:33 -0700

    Add LocalSite compatibility for NavigationBarHook.is_enabled_for.
    `NavigationBarHook.is_enabled_for` only took a user as a parameter, which
    meant that the hook couldn't easily differentiate between LocalSites
    without subclassing.
    
    Now, `is_enabled_for` is meant to take keyword arguments, and will be
    passed the user, request, and the LocalSite name. Older implementations
    that only take a user are still supported, but a warning will be shown.
    
    Testing Done:
    Unit tests pass.
    
    Used this in an extension that needed to know the LocalSite, and checked
    that it worked both with and without the new function signatures.
    
    Reviewed at https://reviews.reviewboard.org/r/7626/

--- reviewboard/extensions/tests.py
+++ reviewboard/extensions/tests.py
@@ -176,14 +176,123 @@ class HookTests(TestCase):
 
         hook = NavigationBarHook(extension=self.extension, entries=[entry])
 
-        context = Context({})
+        request = self.client.request()
+        request.user = User(username='text')
+
+        context = Context({
+            'request': request,
+            'local_site_name': 'test-site',
+        })
         entries = hook.get_entries(context)
         self.assertEqual(len(entries), 1)
         self.assertEqual(entries[0], entry)
 
         t = Template(
-            "{% load rb_extensions %}"
-            "{% navigation_bar_hooks %}")
+            '{% load rb_extensions %}'
+            '{% navigation_bar_hooks %}')
+
+        self.assertEqual(t.render(context).strip(),
+                         '<li><a href="%(url)s">%(label)s</a></li>' % entry)
+
+    def test_navigation_bar_hooks_with_is_enabled_for_true(self):
+        """Testing NavigationBarHook.is_enabled_for and returns true"""
+        def is_enabled_for(**kwargs):
+            self.assertEqual(kwargs['user'], request.user)
+            self.assertEqual(kwargs['request'], request)
+            self.assertEqual(kwargs['local_site_name'], 'test-site')
+
+            return True
+
+        entry = {
+            'label': 'Test Nav Entry',
+            'url': 'foo-url',
+        }
+
+        hook = NavigationBarHook(extension=self.extension, entries=[entry],
+                                 is_enabled_for=is_enabled_for)
+
+        request = self.client.request()
+        request.user = User(username='text')
+
+        context = Context({
+            'request': request,
+            'local_site_name': 'test-site',
+        })
+        entries = hook.get_entries(context)
+        self.assertEqual(len(entries), 1)
+        self.assertEqual(entries[0], entry)
+
+        t = Template(
+            '{% load rb_extensions %}'
+            '{% navigation_bar_hooks %}')
+
+        self.assertEqual(t.render(context).strip(),
+                         '<li><a href="%(url)s">%(label)s</a></li>' % entry)
+
+    def test_navigation_bar_hooks_with_is_enabled_for_false(self):
+        """Testing NavigationBarHook.is_enabled_for and returns false"""
+        def is_enabled_for(**kwargs):
+            self.assertEqual(kwargs['user'], request.user)
+            self.assertEqual(kwargs['request'], request)
+            self.assertEqual(kwargs['local_site_name'], 'test-site')
+
+            return False
+
+        entry = {
+            'label': 'Test Nav Entry',
+            'url': 'foo-url',
+        }
+
+        hook = NavigationBarHook(extension=self.extension, entries=[entry],
+                                 is_enabled_for=is_enabled_for)
+
+        request = self.client.request()
+        request.user = User(username='text')
+
+        context = Context({
+            'request': request,
+            'local_site_name': 'test-site',
+        })
+        entries = hook.get_entries(context)
+        self.assertEqual(len(entries), 0)
+
+        t = Template(
+            '{% load rb_extensions %}'
+            '{% navigation_bar_hooks %}')
+
+        self.assertEqual(t.render(context).strip(), '')
+
+    def test_navigation_bar_hooks_with_is_enabled_for_legacy(self):
+        """Testing NavigationBarHook.is_enabled_for and legacy argument
+        format
+        """
+        def is_enabled_for(user):
+            self.assertEqual(user, request.user)
+
+            return True
+
+        entry = {
+            'label': 'Test Nav Entry',
+            'url': 'foo-url',
+        }
+
+        hook = NavigationBarHook(extension=self.extension, entries=[entry],
+                                 is_enabled_for=is_enabled_for)
+
+        request = self.client.request()
+        request.user = User(username='text')
+
+        context = Context({
+            'request': request,
+            'local_site_name': 'test-site',
+        })
+        entries = hook.get_entries(context)
+        self.assertEqual(len(entries), 1)
+        self.assertEqual(entries[0], entry)
+
+        t = Template(
+            '{% load rb_extensions %}'
+            '{% navigation_bar_hooks %}')
 
         self.assertEqual(t.render(context).strip(),
                          '<li><a href="%(url)s">%(label)s</a></li>' % entry)
@@ -197,14 +306,20 @@ class HookTests(TestCase):
 
         hook = NavigationBarHook(extension=self.extension, entries=[entry])
 
-        context = Context({})
+        request = self.client.request()
+        request.user = User(username='text')
+
+        context = Context({
+            'request': request,
+            'local_site_name': 'test-site',
+        })
         entries = hook.get_entries(context)
         self.assertEqual(len(entries), 1)
         self.assertEqual(entries[0], entry)
 
         t = Template(
-            "{% load rb_extensions %}"
-            "{% navigation_bar_hooks %}")
+            '{% load rb_extensions %}'
+            '{% navigation_bar_hooks %}')
 
         self.assertEqual(t.render(context).strip(),
                          '<li><a href="%(url)s">%(label)s</a></li>' % {
