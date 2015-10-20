8fb4714aa7de285c826c82cda91b5a5b927f7833
Christian Hammond <christian@beanbaginc.com>
2015-10-20 12:45:56 -0700

    Simplify the process of building a development tree.
    This introduces a dev-requirements.txt, which lists the pip-compatible
    Python dependencies we need to use Review Board. It also installs them
    when using prepare-dev.py.
    
    Testing Done:
    Installed a new environment, and got all the development packages
    specified.
    
    Reviewed at https://reviews.reviewboard.org/r/7629/

--- contrib/internal/prepare-dev.py
+++ contrib/internal/prepare-dev.py
@@ -72,8 +72,10 @@ def install_media(site):
     site.mkdir(ext_media_path)
 
 
-def build_egg_info():
-    os.system("%s setup.py egg_info" % sys.executable)
+def install_dependencies():
+    os.system('%s setup.py egg_info' % sys.executable)
+    os.system('pip%s.%s install -f dev-requirements.txt'
+              % sys.version_info[:2])
 
 
 def parse_options(args):
@@ -125,7 +127,7 @@ def main():
     site = Site(site_path, SiteOptions)
 
     create_settings()
-    build_egg_info()
+    install_dependencies()
 
     if options.install_media:
         install_media(site)
