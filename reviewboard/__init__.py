99eed687b76508760eb0e299f3174af67e361441
Christian Hammond <christian@beanbaginc.com>
2015-11-03 16:25:20 -0800

    Bump to 2.5.2 dev.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -13,7 +13,7 @@ from __future__ import unicode_literals
 #:
 #: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #:
-VERSION = (2, 5, 1, 0, 'final', 0, True)
+VERSION = (2, 5, 2, 0, 'alpha', 0, False)
 
 
 #: The major version of Django we're using.
