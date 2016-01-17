9111263f2b00a3afeb221a97d00f6bf3b18a1b1a
David Trowbridge <trowbrds@gmail.com>
2016-01-17 10:21:00 -0500

    Add absolute URL to the review API payload.
    Now that we can link directly to a review in the UI, we should provide that
    link in the API. This change adds a new `absolute_url` field to the Review
    resource that includes this, modeled after how we do it for the ReviewRequest
    resource.
    
    Testing done:
    - Manually verified that the URL was serialized correctly.
    - Ran unit tests.
    
    Reviewed at https://reviews.reviewboard.org/r/7888/

--- reviewboard/webapi/resources/base_review.py
+++ reviewboard/webapi/resources/base_review.py
@@ -27,6 +27,12 @@ class BaseReviewResource(MarkdownFieldsMixin, WebAPIResource):
     """
     model = Review
     fields = {
+        'absolute_url': {
+            'type': six.text_type,
+            'description': "The absolute URL to the review request's page on "
+                           "the site.",
+            'added_in': '2.6',
+        },
         'body_bottom': {
             'type': six.text_type,
             'description': 'The review content below the comments.',
@@ -174,6 +180,9 @@ class BaseReviewResource(MarkdownFieldsMixin, WebAPIResource):
 
     def has_delete_permissions(self, request, review, *args, **kwargs):
         return review.is_mutable_by(request.user)
+
+    def serialize_absolute_url_field(self, obj, request, **kwargs):
+        return request.build_absolute_uri(obj.get_absolute_url())
 
     def serialize_body_top_text_type_field(self, obj, **kwargs):
         # This will be overridden by MarkdownFieldsMixin.
