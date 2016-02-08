8d9acd9447400608ddc2ab46948c7d05430b3e0b
Christian Hammond <christian@beanbaginc.com>
2016-02-08 01:20:34 -0800

    Add a backwards-compatibility import for ServiceTests.
    We had a class, ServiceTests, which was a base testcase class for
    hosting service unit tests. This was moved recently, which broke
    extensions that provided hosting service unit tests based on this.
    
    This restores the import for now. We may want to create something more
    formal down the road.
    
    Testing Done:
    Review Board unit tests pass.
    
    Power Pack unit tests pass.
    
    Reviewed at https://reviews.reviewboard.org/r/7944/

---
+++ reviewboard/hostingsvcs/tests/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from reviewboard.hostingsvcs.tests.testcases import ServiceTests
+
+
+__all__ = [
+    # Backwards-compatibility for third-party modules that used this import.
+    'ServiceTests',
+]
