e31099e964f809a8a6ebcb071c7c2b57e17248c2
Barret Rennie <barret@beanbaginc.com>
2016-04-04 15:17:37 -0600

    Fix evolution for change description users
    Trivial change

--- reviewboard/changedescs/evolutions/changedesc_user.py
+++ reviewboard/changedescs/evolutions/changedesc_user.py
@@ -5,6 +5,6 @@ from django.db import models
 
 
 MUTATIONS = [
-    AddField('ChangeDescription', 'user', models.ForeignKey, blank=True,
+    AddField('ChangeDescription', 'user', models.ForeignKey, null=True,
              related_model='auth.User'),
 ]
