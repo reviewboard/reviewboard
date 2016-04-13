318e6cbf1486b96b6da5494627bb5f06e94e7178
Christian Hammond <christian@beanbaginc.com>
2016-04-13 14:33:51 -0700

    Release Review Board 2.5.4.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -13,7 +13,7 @@ from __future__ import unicode_literals
 #:
 #: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #:
-VERSION = (2, 5, 3, 0, 'final', 0, True)
+VERSION = (2, 5, 4, 0, 'final', 0, True)
 
 
 #: The major version of Django we're using.
