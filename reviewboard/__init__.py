12b8d2eaaabcf6773253f1819b86cf24019fdfe6
Christian Hammond <christian@beanbaginc.com>
2016-04-25 15:57:08 -0700

    Fix a typo in the version info for Review Board 2.5.5.
    A stray backtick snuck in there. This removes the pest.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -13,7 +13,7 @@ from __future__ import unicode_literals
 #:
 #: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #:
-VERSION = (2, 5, 5, 0, 'alpha', 0, False`)
+VERSION = (2, 5, 5, 0, 'alpha', 0, False)
 
 
 #: The major version of Django we're using.
