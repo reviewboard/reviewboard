15668a36a39c124d63686f4e36969929b4722710
Christian Hammond <christian@beanbaginc.com>
2016-05-17 16:45:42 -0700

    Add a lazy, cacheable request.local_site attribute.
    The way our Local Site storage has worked in the past is that the URL
    patterns would capture the Local Site's name, and the decorators for
    views would turn that into a LocalSite object, passing it to the view.
    This worked pretty well, but if other code needed access to the
    LocalSite and only had the request, they'd have to refetch it based on a
    private attribute on the request.
    
    This change adds a public attribute on the request containing the
    LocalSite itself. This attribute is only loaded on demand, the first
    time it's accessed, and is None if not working with a LocalSite. The
    decorators and other code can access this without worrying about
    redundant fetches, and without accessing private attributes.
    
    There's minimal impact to the Local Site code here. It's really just a
    new convenience for code that needs a Local Site from a request (which
    will be used in the upcoming integrations changes).
    
    Testing Done:
    Tested various pages with and without a Local Site.
    
    Tested this new attribute through the decorator and also in some new code
    that only has access to the request.
    
    Unit tests pass.
    
    Reviewed at https://reviews.reviewboard.org/r/8166/

--- reviewboard/site/middleware.py
+++ reviewboard/site/middleware.py
@@ -1,9 +1,49 @@
+"""Middleware for Local Sites."""
+
 from __future__ import unicode_literals
+
+from django.utils.functional import SimpleLazyObject
+from djblets.db.query import get_object_or_none
+
+from reviewboard.site.models import LocalSite
 
 
 class LocalSiteMiddleware(object):
-    """Middleware that handles storing information on the LocalSite in use."""
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        request._local_site_name = view_kwargs.get('local_site_name', None)
+    """Middleware that handles storing information on the Local Site in use.
 
-        return None
+    This adds a new ``local_site`` attribute to the
+    :py:class:`~django.http.HttpRequest` that, when first accessed, will fetch
+    and cache the matching :py:class:`~reviewboard.site.models.LocalSite`. If
+    there's no Local Site for this given request, this will store ``None``
+    instead.
+    """
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        """Process the request before calling the view.
+
+        This sets up a ``local_site`` attribute on the request to fetch the
+        :py:class:`~reviewboard.site.models.LocalSite` used for this view,
+        if any. This is based on the ``local_site_name`` key in
+        ``view_kwargs``.
+
+        Args:
+            request (django.http.HttpRequest):
+                The HTTP request from the client.
+
+            view_func (callable):
+                The view being called. This is unused.
+
+            view_args (tuple):
+                The positional arguments passed to the view. This is unused.
+
+            view_kwargs (dict):
+                The keyword arguments passed to the view.
+        """
+        local_site_name = view_kwargs.get('local_site_name')
+        request._local_site_name = local_site_name
+
+        if request._local_site_name:
+            request.local_site = SimpleLazyObject(
+                lambda: get_object_or_none(LocalSite, name=local_site_name))
+        else:
+            request.local_site = None
