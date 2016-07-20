894c2d52a197d04eb2c2ed3eec0f205b98542373
Barret Rennie <barret@beanbaginc.com>
2016-07-19 18:29:38 -0600

    Revert "Remove dead code from review_detail view on dvcs"
    This reverts commit a1888e2458cccbf26c629648086a2f9bf4b4eb25.
    
    This code was not actually dead; it is used in the
    `BuiltinLocalsFieldMixin`.

--- reviewboard/reviews/views.py
+++ reviewboard/reviews/views.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 import logging
 import time
+from collections import defaultdict
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
@@ -42,7 +43,7 @@ from reviewboard.diffviewer.diffutils import (convert_to_unicode,
                                               get_last_line_number_in_diff,
                                               get_original_file,
                                               get_patched_file)
-from reviewboard.diffviewer.models import DiffSet
+from reviewboard.diffviewer.models import DiffCommit, DiffSet
 from reviewboard.diffviewer.views import (DiffFragmentView, DiffViewerView,
                                           exception_traceback_string)
 from reviewboard.hostingsvcs.bugtracker import BugTracker
@@ -461,6 +462,15 @@ def review_detail(request,
 
     for diffset in diffsets:
         diffsets_by_id[diffset.pk] = diffset
+
+    diff_commits = DiffCommit.objects.filter(diffset__in=diffsets)
+    diff_commits_by_id = defaultdict(list)
+
+    for diff_commit in diff_commits:
+        diff_commits_by_id[diff_commit.diffset_id].append(diff_commit)
+
+    if draft and draft.diffset:
+        diff_commits_by_id[draft.diffset.pk] = draft.diffset.diff_commits.all()
 
     # Find out if we can bail early. Generate an ETag for this.
     last_activity_time, updated_object = \
