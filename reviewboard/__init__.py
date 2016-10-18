4f2db61aa1869c92e36a2481399f881ab225e674
Christian Hammond <christian@beanbaginc.com>
2016-10-17 19:03:36 -0700

    Release Review Board 2.0.25.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (2, 0, 24, 0, 'final', 0, True)
+VERSION = (2, 0, 25, 0, 'final', 0, True)
 
 
 # Required version of Django
