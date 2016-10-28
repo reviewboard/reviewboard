498b32c05cae215fa952a0d53658a7c623ccc1b0
Christian Hammond <christian@beanbaginc.com>
2016-10-27 17:09:28 -0700

    Make Review Board package develop mode install wheels/npm packages.
    This simplifies getting going with a Review Board source tree. Before,
    Review Board would require a few steps to get set up, which included
    installing running `setup.py develop` (and having everything in place to
    compile Pillow and other modules), installing the packages in
    dev-requirements.txt, and installing the required node modules. That was
    a lot of steps, and caused problems for those getting started
    (particularly as some complex dependencies are no longer shipped as
    eggs).
    
    Now, `setup.py develop` takes care of installing dependencies as wheels,
    installing development packages, and installing modules from npm. It's a
    far simpler process than we've had in the past.
    
    Like in the accompanying Djblets change, the package dependencies now
    live in a `reviewboard.dependencies` module. This will help simplify
    things for us in some ways (such as with RBCommons packaging and
    extension packaging).
    
    The `prepare-dev.py` script has been updated to no longer install
    modules itself, and to allow `develop` to do more work.
    
    Testing Done:
    Used a new virtualenv consisting of just the Djblets source tree and
    dependencies. Used a clean copy of the Review Board source tree. Ran
    `./setup.py develop` and saw it install wheels and node modules. Ran
    unit tests and they passed.
    
    Created a new environment and installed Djblets into it. Then I set up
    the Review Board tree using `./contrib/internal/prepare-dev.py` and
    repeated tests. Everything worked.
    
    Reviewed at https://reviews.reviewboard.org/r/8494/

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@
 # A big thanks to Django project for some of the fixes used in here for
 # MacOS X and data files installation.
 
+import json
 import os
 import subprocess
 import sys
+from distutils.command.install import INSTALL_SCHEMES
+from distutils.command.install_data import install_data
+from distutils.core import Command
 
 try:
     from setuptools import setup, find_packages
@@ -16,15 +20,12 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-
+from setuptools.command.develop import develop
 from setuptools.command.egg_info import egg_info
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
-from distutils.core import Command
 
-from reviewboard import (get_package_version,
-                         VERSION,
-                         django_version)
+from reviewboard import get_package_version, VERSION
+from reviewboard.dependencies import (build_dependency_list,
+                                      package_dependencies)
 
 
 # Make sure this is a version of Python we are compatible with. This should
@@ -78,11 +79,101 @@ class BuildEggInfo(egg_info):
     def run(self):
         if ('sdist' in sys.argv or
             'bdist_egg' in sys.argv or
+            'bdist_wheel' in sys.argv or
             'install' in sys.argv):
             self.run_command('build_media')
             self.run_command('build_i18n')
 
         egg_info.run(self)
+
+
+class DevelopCommand(develop):
+    """Installs Review Board in developer mode.
+
+    This will install all standard and development dependencies (using Python
+    wheels and node.js packages from npm) and add the source tree to the
+    Python module search path. That includes updating the versions of pip
+    and setuptools on the system.
+
+    To speed up subsequent runs, callers can pass ``--no-npm`` to prevent
+    installing node.js packages.
+    """
+
+    user_options = develop.user_options + [
+        ('no-npm', None, "Don't install packages from npm"),
+        ('use-npm-cache', None, "Use npm-cache to install packages"),
+    ]
+
+    boolean_options = develop.boolean_options + ['no-npm', 'use-npm-cache']
+
+    def initialize_options(self):
+        """Initialize options for the command."""
+        develop.initialize_options(self)
+
+        self.no_npm = None
+        self.use_npm_cache = None
+
+    def install_for_development(self):
+        """Install the package for development.
+
+        This takes care of the work of installing all dependencies.
+        """
+        if self.no_deps:
+            # In this case, we don't want to install any of the dependencies
+            # below. However, it's really unlikely that a user is going to
+            # want to pass --no-deps.
+            #
+            # Instead, what this really does is give us a way to know we've
+            # been called by `pip install -e .`. That will call us with
+            # --no-deps, as it's going to actually handle all dependency
+            # installation, rather than having easy_install do it.
+            develop.install_for_development(self)
+            return
+
+        # Install the latest pip and setuptools. Note that the order here
+        # matters, as otherwise a stale setuptools can be left behind,
+        # causing installation errors.
+        self._run_pip(['install', '-U', 'setuptools'])
+        self._run_pip(['install', '-U', 'pip'])
+
+        # Install the dependencies using pip instead of easy_install. This
+        # will use wheels instead of eggs, which are ideal for our users.
+        if sys.platform == 'darwin':
+            # We're building on macOS, and some of our dependencies
+            # (hi there, mercurial!) won't compile using gcc (their default
+            # in some cases), so we want to force the proper compiler.
+            os.putenv(b'CC', b'clang')
+
+        self._run_pip(['install', '-e', '.'])
+        self._run_pip(['install', '-r', 'dev-requirements.txt'])
+
+        if not self.no_npm:
+            if self.use_npm_cache:
+                self.distribution.command_options['install_node_deps'] = {
+                    'use_npm_cache': ('install_node_deps', 1),
+                }
+
+            self.run_command('install_node_deps')
+
+    def _run_pip(self, args):
+        """Run pip.
+
+        Args:
+            args (list):
+                Arguments to pass to :command:`pip`.
+
+        Raises:
+            RuntimeError:
+                The :command:`pip` command returned a non-zero exit code.
+        """
+        # NOTE: We need to do pip.__main__ to support Python 2.6. This is not
+        #       required (but does work) for Python 2.7+.
+        cmd = subprocess.list2cmdline([sys.executable, '-m', 'pip.__main__'] +
+                                      args)
+        ret = os.system(cmd)
+
+        if ret != 0:
+            raise RuntimeError('Failed to run `%s`' % cmd)
 
 
 class BuildMedia(Command):
@@ -120,11 +211,86 @@ class BuildI18n(Command):
             raise RuntimeError('Failed to build i18n files')
 
 
+class InstallNodeDependenciesCommand(Command):
+    """Install all node.js dependencies from npm.
+
+    If ``--use-npm-cache`` is passed, this will use :command:`npm-cache`
+    to install the packages, which is best for Continuous Integration setups.
+    Otherwise, :command:`npm` is used.
+    """
+
+    description = \
+        'Install the node packages required for building static media.'
+
+    user_options = [
+        ('use-npm-cache', None, "Use npm-cache to install packages"),
+    ]
+
+    boolean_options = ['use-npm-cache']
+
+    def initialize_options(self):
+        """Initialize options for the command."""
+        self.use_npm_cache = None
+
+    def finalize_options(self):
+        """Finalize options for the command.
+
+        This is required, but does not actually do anything.
+        """
+        pass
+
+    def run(self):
+        """Run the commands to install packages from npm.
+
+        Raises:
+            RuntimeError:
+                There was an error finding or invoking the package manager.
+        """
+        if self.use_npm_cache:
+            npm_command = 'npm-cache'
+        else:
+            npm_command = 'npm'
+
+        try:
+            subprocess.check_call([npm_command, '--version'])
+        except subprocess.CalledProcessError:
+            raise RuntimeError(
+                'Unable to locate %s in the path, which is needed to '
+                'install dependencies required to build this package.'
+                % npm_command)
+
+        # By this point, dependencies should be installed for us. We're also
+        # using the same exact dependencies as Djblets, so no need to
+        # duplicate that list.
+        from djblets.dependencies import npm_dependencies
+
+        with open('package.json', 'w') as fp:
+            fp.write(json.dumps(
+                {
+                    'name': 'reviewboard',
+                    'private': 'true',
+                    'devDependencies': {},
+                    'dependencies': npm_dependencies,
+                },
+                indent=2))
+
+        print 'Installing node.js modules...'
+        result = os.system('%s install' % npm_command)
+
+        os.unlink('package.json')
+
+        if result != 0:
+            raise RuntimeError(
+                'One or more node.js modules could not be installed.')
+
+
 cmdclasses = {
-    'install_data': install_data,
+    'develop': DevelopCommand,
     'egg_info': BuildEggInfo,
     'build_media': BuildMedia,
     'build_i18n': BuildI18n,
+    'install_data': install_data,
+    'install_node_deps': InstallNodeDependenciesCommand,
 }
 
 
@@ -197,29 +363,7 @@ setup(name=PACKAGE_NAME,
           ],
       },
       cmdclass=cmdclasses,
-      install_requires=[
-          django_version,
-          'django_evolution>=0.7.6,<=0.7.999',
-          'django-haystack>=2.3.1,<=2.4.999',
-          'django-multiselectfield',
-          'Djblets>=0.9.4,<=0.9.999',
-          'docutils',
-          'markdown>=2.4.0,<2.4.999',
-          'mimeparse>=0.1.3',
-          'paramiko>=1.12',
-          'pycrypto>=2.6',
-          'Pygments>=2.1',
-          'python-dateutil==1.5',
-          'python-memcached',
-          'pytz',
-          'recaptcha-client',
-          'Whoosh>=2.6',
-      ],
-      dependency_links=[
-          'http://downloads.reviewboard.org/mirror/',
-          'http://downloads.reviewboard.org/releases/Djblets/0.9/',
-          'http://downloads.reviewboard.org/releases/django-evolution/0.7/',
-      ],
+      install_requires=build_dependency_list(package_dependencies),
       include_package_data=True,
       zip_safe=False,
       classifiers=[
