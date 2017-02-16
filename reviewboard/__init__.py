3671a5949d102f69a36884334aadf852e5563760
Christian Hammond <christian@beanbaginc.com>
2017-02-15 18:24:18 -0800

    Bump to 2.0.28 dev.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (2, 0, 27, 0, 'final', 0, True)
+VERSION = (2, 0, 28, 0, 'alpha', 0, False)
 
 
 def get_version_string():
