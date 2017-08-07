f62c4b5733be443bde5574efa433bcedef64b8c9
Christian Hammond <christian@beanbaginc.com>
2017-08-07 15:34:34 -0700

    Release Review Board 2.5.15.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -13,7 +13,7 @@ from __future__ import unicode_literals
 #:
 #: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #:
-VERSION = (2, 5, 14, 0, 'final', 0, True)
+VERSION = (2, 5, 15, 0, 'final', 0, True)
 
 
 def get_version_string():
