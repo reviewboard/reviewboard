0e29a8de9e2197b814245e421313c046af4a81e2
Christian Hammond <christian@beanbaginc.com>
2017-09-04 16:19:39 -0700

    Convert the view and model for the diff viewer page to ES6.
    This updates the diff viewer page's view and model to ES6 and fixes up
    the docs for our modern standards. The logic for these classes remains
    the same, but the code has been streamlined by the usage of template
    literals, consts/lets, and fat arrow functions.
    
    Upcoming work will build upon this to simplify the view further.
    
    Testing Done:
    Unit tests pass.
    
    Tested all functionality of the diff viewer (revision selection,
    interdiffs, pagination, anchor navigation, commenting) to ensure
    nothing broke.
    
    Reviewed at https://reviews.reviewboard.org/r/9152/

--- reviewboard/static/rb/js/pages/models/diffViewerPageModel.es6.js
+++ reviewboard/static/rb/js/pages/models/diffViewerPageModel.es6.js
@@ -1,41 +1,75 @@
-/*
- * A model with all the data for the diff viewer page.
+/**
+ * The model for the diff viewer page.
+ *
+ * This handles all attribute storage and diff context parsing needed to
+ * display and update the diff viewer.
+ *
+ * Model Attributes:
+ *     commentsHint (RB.DiffCommentsHint):
+ *         The comments hint model.
+ *
+ *     files (RB.DiffFileCollection):
+ *         The collection of files on the page.
+ *
+ *     numDiffs (number):
+ *         The number of total diffs.
+ *
+ *     pagination (RB.Pagination):
+ *         The pagination handler for the diff viewer.
+ *
+ *     revision (RB.DiffRevision):
+ *         The current diff revision on the page.
  */
-RB.DiffViewerPageModel = Backbone.Model.extend({
-    defaults: {
+RB.DiffViewerPageModel = RB.Page.extend({
+    defaults: _.defaults({
         commentsHint: null,
         files: null,
         numDiffs: 1,
         pagination: null,
-        revision: null
-    },
+        revision: null,
+    }, RB.Page.prototype.defaults),
 
-    /*
+    /**
      * Parse the data given to us by the server.
+     *
+     * Args:
+     *     rsp (object):
+     *         The payload to parse.
+     *
+     * Returns:
+     *     object:
+     *     The resulting attributes for the model.
      */
-    parse: function(rsp) {
+    parse(rsp) {
         return {
             commentsHint: new RB.DiffCommentsHint(rsp.comments_hint,
                                                   {parse: true}),
             files: new RB.DiffFileCollection(rsp.files, {parse: true}),
             numDiffs: rsp.num_diffs,
             pagination: new RB.Pagination(rsp.pagination, {parse: true}),
-            revision: new RB.DiffRevision(rsp.revision, {parse: true})
+            revision: new RB.DiffRevision(rsp.revision, {parse: true}),
         };
     },
 
-    /*
+    /**
      * Override for Model.set that properly updates the child objects.
      *
      * Because several of this model's properties are other backbone models, we
-     * can't just call set(parse(...)) because it would replace the models
+     * can't just call ``set(parse(...))`` because it would replace the models
      * entirely. If this is called as part of the initial construction, it will
-     * just use the models created by parse(). Otherwise, it will copy the new
-     * data into the child models' attributes.
+     * just use the models created by :js:func:`parse`. Otherwise, it will
+     * copy the new data into the child models' attributes.
+     *
+     * Args:
+     *     attrs (object):
+     *         The new attributes being set.
+     *
+     *     options (object):
+     *         The options for the set.
      */
-    set: function(attrs, options) {
-        var toSet = {
-            numDiffs: attrs.numDiffs
+    set(attrs, options) {
+        const toSet = {
+            numDiffs: attrs.numDiffs,
         };
 
         if (this.attributes.commentsHint) {
@@ -64,6 +98,6 @@ RB.DiffViewerPageModel = Backbone.Model.extend({
             toSet.revision = attrs.revision;
         }
 
-        Backbone.Model.prototype.set.call(this, toSet, options);
-    }
+        RB.Page.prototype.set.call(this, toSet, options);
+    },
 });
