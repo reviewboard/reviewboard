ae0e24f5d2b1c8fe99cc82a98984979086bfc958
Christian Hammond <christian@beanbaginc.com>
2017-10-20 16:09:43 -0700

    Improve documentation for datagrid sidebars.
    This fixes up the documentation for `reviewboard.datagrids.sidebar` to
    better document the various methods, attributes, and parameters for all
    functionality used for providing custom sections and items for the
    Dashboard, User Page, and custom datagrids containing sidebars. The new
    documentation better corresponds to our modern guidelines.
    
    Testing Done:
    Built the docs and looked through them. There were no build errors and
    the generated docs seemed correct.
    
    Reviewed at https://reviews.reviewboard.org/r/9286/

--- reviewboard/datagrids/sidebar.py
+++ reviewboard/datagrids/sidebar.py
@@ -1,3 +1,5 @@
+"""Sidebar item management for datagrids."""
+
 from __future__ import unicode_literals
 
 from django.template.context import RequestContext
@@ -16,19 +18,51 @@ class BaseSidebarItem(object):
 
     They may also have custom templates, for more advanced rendering.
 
-    See SidebarItem and BaseSidebarSection for the common types of sidebar
-    items.
+    See :py:class:`SidebarNavItem` and :py:class:`BaseSidebarSection` for the
+    common types of sidebar items.
+
+    Attributes:
+        datagrid (djblets.datagrid.grids.DataGrid):
+            The datagrid containing this item.
+
+        sidebar (Sidebar):
+            The sidebar containing this item.
     """
 
+    #: The template to use for rendering this item in the sidebar.
     template_name = None
+
+    #: The displayed label for the item.
     label = None
+
+    #: The name of the optional CSS icon to use beside the label.
     icon_name = None
+
+    #: The datagrid "view" to link to when clicking this item.
+    #:
+    #: This corresponds to the ``?view=`` parameter passed to the datagrid
+    #: page.
     view_id = None
+
+    #: Additional key/values to pass to the URL when clicking this item.
+    #:
+    #: If provided, this must be a dictionary of keys and values for the
+    #: URL. The keys and values will be automatically URL-encoded.
     view_args = None
+
+    #: Additional CSS classes to include for the item.
     css_classes = None
 
     def __init__(self, sidebar, datagrid):
-        """Initialize the sidebar item."""
+        """Initialize the sidebar item.
+
+        Args:
+            sidebar (Sidebar):
+                The sidebar containing this item.
+
+            datagrid (djblets.datagrid.grids.DataGrid):
+                The datagrid containing this item.
+        """
         self.sidebar = sidebar
         self.datagrid = datagrid
 
@@ -38,6 +72,10 @@ class BaseSidebarItem(object):
         By default, this builds a URL to the parent datagrid using
         the ``view_id`` and ``view_args`` attributes. If they are not
         set, then the item won't be clickable.
+
+        Returns:
+            unicode:
+            The URL to the dashboard view represented by this item.
         """
         if not self.view_id and not self.view_args:
             return None
@@ -57,6 +95,11 @@ class BaseSidebarItem(object):
 
         By default, this shows nothing. Subclasses can override to display
         a count.
+
+        Returns:
+            int:
+            The count to display beside the item, or ``None`` if no count
+            should be displayed.
         """
         return None
 
@@ -65,6 +108,10 @@ class BaseSidebarItem(object):
 
         By default, an item is visible. Subclasses can override this to
         control visibility.
+
+        Returns:
+            bool:
+            ``True`` if the item is visible. ``False`` if it's hidden.
         """
         return True
 
@@ -73,6 +120,11 @@ class BaseSidebarItem(object):
 
         The item will be active if the current page matches the URL
         associated with the item.
+
+        Returns:
+            bool:
+            ``True`` if the item represents the active page. ``False`` if it
+            does not.
         """
         if self.view_id is None:
             return False
@@ -91,7 +143,12 @@ class BaseSidebarItem(object):
         return True
 
     def render(self):
-        """Render the item."""
+        """Render the item.
+
+        Returns:
+            django.utils.safestring.SafeText:
+            The rendered HTML for the item.
+        """
         count = self.get_count()
         context = {
             'datagrid': self.datagrid,
@@ -111,7 +168,13 @@ class BaseSidebarItem(object):
                                 RequestContext(self.datagrid.request, context))
 
     def get_extra_context(self):
-        """Return extra context for the render."""
+        """Return extra context for the render.
+
+        Returns:
+            dict:
+            A dictionary of additional template context. By default, this is
+            empty.
+        """
         return {}
 
 
@@ -127,7 +190,15 @@ class BaseSidebarSection(BaseSidebarItem):
     template_name = 'datagrids/sidebar_section.html'
 
     def __init__(self, *args, **kwargs):
-        """Initialize the section."""
+        """Initialize the section.
+
+        Args:
+            *args (tuple):
+                Positional arguments to pass to the parent class.
+
+            **kwargs (dict):
+                Keyword arguments to pass to the parent class.
+        """
         super(BaseSidebarSection, self).__init__(*args, **kwargs)
 
         self.items = list(self.get_items())
@@ -137,6 +208,10 @@ class BaseSidebarSection(BaseSidebarItem):
 
         Subclasses must override this and return or yield the items
         to be displayed.
+
+        Returns:
+            list of BaseSidebarItem:
+            The list of items to display in the section.
         """
         raise NotImplementedError
 
@@ -144,12 +219,24 @@ class BaseSidebarSection(BaseSidebarItem):
         """Return whether the section is visible.
 
         By default, a section is visible if it has any item classes
-        registered.
+        registered. Subclasses can override this to provide more specific
+        logic.
+
+        Returns:
+            bool:
+            ``True`` if the section is visible. ``False`` if it's hidden.
         """
         return len(self.items) > 0
 
     def get_extra_context(self):
-        """Return extra context for the section."""
+        """Return extra context for the section.
+
+        Subclasses that override this method must call the parent method.
+
+        Returns:
+            dict:
+            Additional template context for the rendering of the section.
+        """
         return {
             'items': self.items,
         }
@@ -158,9 +245,10 @@ class BaseSidebarSection(BaseSidebarItem):
 class SidebarNavItem(BaseSidebarItem):
     """A typical navigation link item on the sidebar.
 
-    This is the standard type of item added to sections on a sidebar.
-    It will automatically generate a link to the dashboard view matching
-    ``view_id`` and ``view_args``, and display the provided count.
+    This is the standard type of item added to sections on a sidebar. An item
+    can contain an explicit URL or a resolvable URL name to link to. If not
+    provided, the current datagrid page's URL will be used along with query
+    arguments built from :py:attr:`view_id` and :py:attr:`view_args`.
     """
 
     template_name = 'datagrids/sidebar_nav_item.html'
@@ -168,7 +256,39 @@ class SidebarNavItem(BaseSidebarItem):
     def __init__(self, section, label, icon_name=None, view_id=None,
                  view_args=None, count=None, url=None, url_name=None,
                  css_classes=None):
-        """Initialize the item."""
+        """Initialize the item.
+
+        Args:
+            section (BaseSidebarSection):
+                The section that should contain this item.
+
+            label (unicode):
+                The displayed label for this item.
+
+            icon_name (unicode, optional):
+                The name of the optional CSS icon to display beside the label.
+
+            view_id (unicode, optional):
+                The ID of the optional datagrid view to display when clicking
+                the item. See :py:attr:`BaseSidebarItem.view_id` for more
+                information.
+
+            view_args (unicode, optional)
+                Keys/values to display in the URL when clicking the item. See
+                :py:attr:`BaseSidebarItem.view_args` for more information.
+
+            count (int, optional):
+                The count to display beside the label.
+
+            url (unicode, optional):
+                The optional URL to navigate to when clicked.
+
+            url_name (unicode, optional):
+                The optional URL name to resolve and navigate to when clicked.
+
+            css_classes (list of unicode, optional):
+                Additional CSS classes to apply to the item.
+        """
         super(SidebarNavItem, self).__init__(section.sidebar, section.datagrid)
 
         self.label = label
@@ -181,7 +301,22 @@ class SidebarNavItem(BaseSidebarItem):
         self.url_name = url_name
 
     def get_url(self):
-        """Return the URL for the item."""
+        """Return the URL for the item.
+
+        If :py:attr:`url` is set, that URL will be returned directly.
+
+        If :py:attr:`url_name` is set instead, it will be resolved relative
+        to any Local Site that might be accessed and used as the URL. Note that
+        the URL can't require any parameters.
+
+        If not explicit URL or name is provided, the current page is used along
+        with query parameters built from :py:attr:`view_id` and
+        :py:attr:`view_args`.
+
+        Returns:
+            unicode:
+            The URL to navigate to when clicked.
+        """
         if self.url:
             return self.url
         elif self.url_name:
@@ -191,7 +326,16 @@ class SidebarNavItem(BaseSidebarItem):
             return super(SidebarNavItem, self).get_url()
 
     def get_count(self):
-        """Return the count provided in the constructor."""
+        """Return the count provided in the constructor.
+
+        Subclasses can override this if they need additional logic to compute
+        a count.
+
+        Returns:
+            int:
+            The count to display beside the label, or ``None`` if no count
+            should be shown.
+        """
         return self.count
 
 
@@ -203,7 +347,20 @@ class Sidebar(object):
     """
 
     def __init__(self, item_classes, default_view_id=None, css_classes=[]):
-        """Initialize the sidebar."""
+        """Initialize the sidebar.
+
+        Args:
+            item_classes (list of type):
+                The list of :py:class:`BaseSidebarItem` subclasses to include
+                by default in the sidebar.
+
+            default_view_id (unicode, optional):
+                The default "view" of the datagrid to display. This corresponds
+                to a registered :py:attr:`BaseSidebarItem.view_id`.
+
+            css_classes (list of unicode):
+                The list of additional CSS classes to apply to the sidebar.
+        """
         self._item_classes = []
         self.css_classes = css_classes
         self.default_view_id = default_view_id
@@ -212,15 +369,36 @@ class Sidebar(object):
             self.add_item(item_cls)
 
     def add_item(self, item_cls):
-        """Add an item class to the sidebar."""
+        """Add an item class to the sidebar.
+
+        Args:
+            item_cls (type):
+                The item to add to the sidebar. This must be a subclass of
+                :py:class:`BaseSidebarItem`.
+        """
         self._item_classes.append(item_cls)
 
     def remove_item(self, item_cls):
-        """Remove an item class from the sidebar."""
+        """Remove an item class from the sidebar.
+
+        Args:
+            item_cls (type):
+                The item to remove from the sidebar. This must be a subclass
+                of :py:class:`BaseSidebarItem`.
+        """
         self._item_classes.remove(item_cls)
 
     def get_items(self, datagrid):
-        """Instantiate and returns all items on the sidebar."""
+        """Instantiate and return all items on the sidebar.
+
+        Args:
+            datagrid (djblets.datagrid.grids.DataGrid):
+                The datagrid instance to associate with each item.
+
+        Returns:
+            list of DataGridSidebarItem:
+            The list of instantiated items.
+        """
         return [
             item_cls(self, datagrid)
             for item_cls in self._item_classes
@@ -230,12 +408,27 @@ class Sidebar(object):
 class DataGridSidebarMixin(object):
     """A mixin for datagrids using a sidebar.
 
-    This is meant to be used along with Sidebar. It will initialize the
-    sidebar, providing instances of all the items for the template.
+    This is meant to be used along with :py:class:`Sidebar`. It will initialize
+    the sidebar, providing instances of all the items for the template.
     """
 
     def load_extra_state(self, *args, **kwargs):
-        """Compute any extra state for the sidebar."""
+        """Compute any extra state for the sidebar.
+
+        This will set :py:attr:`sidebar_items` on the datagrid to a list of
+        instantiated items.
+
+        Args:
+            *args (tuple):
+                Additional positional arguments passed to the method.
+
+            **kwargs (dict):
+                Additional keyword arguments passed to the method.
+
+        Returns:
+            object:
+            The result from the parent method on the datagrid.
+        """
         result = super(DataGridSidebarMixin, self).load_extra_state(
             *args, **kwargs)
 
