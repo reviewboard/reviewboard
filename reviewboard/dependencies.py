ef95fa2dd28e9f13706301a0e3d2ace69e17ef32
Christian Hammond <christian@beanbaginc.com>
2018-01-09 00:45:58 -0800

    Release Review Board 3.0.2.

--- reviewboard/dependencies.py
+++ reviewboard/dependencies.py
@@ -23,7 +23,7 @@ djblets_doc_major_version = '1.0'
 django_version = '>=1.6.11,<1.6.999'
 
 #: The version range required for Djblets.
-djblets_version = '>=1.0.1,<=1.0.999'
+djblets_version = '>=1.0.2,<=1.0.999'
 
 #: All dependencies required to install Review Board.
 package_dependencies = {
