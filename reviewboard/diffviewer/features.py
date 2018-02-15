ebc8665dfcfc79a02f261970c82e41667767103a
Barret Rennie <barret@beanbaginc.com>
2018-02-15 16:51:18 -0600

    Add a feature for DVCS support
    This patch adds a feature to enable the DVCS API and UI coming in future
    patches.
    
    Reviewed at https://reviews.reviewboard.org/r/9634/

---
+++ reviewboard/diffviewer/features.py
@@ -0,0 +1,22 @@
+"""Diffviewer features."""
+
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _
+from djblets.features import Feature, FeatureLevel
+
+
+class DVCSFeature(Feature):
+    """A feature for DVCS support.
+
+    With this enabled, the DVCS API is enabled and review requests can be
+    created with multiple commits.
+    """
+
+    feature_id = 'diffviewer.dvcs'
+    name = _('DVCS Support')
+    level = FeatureLevel.EXPERIMENTAL
+    summary = _('Support for reviewing multiple commits.')
+
+
+dvcs_feature = DVCSFeature()
