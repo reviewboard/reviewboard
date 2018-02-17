978c5081ddc648d90ffa74f73f82786a6c2f0b51
Christian Hammond <christian@beanbaginc.com>
2018-02-17 01:25:57 -0800

    Move review request status/timestamp info out of the template.
    We've previously been generating the human-readable information on the
    review request's status (created time, updated time) in the review
    request box template. This was a bit messy, and covered up issues like
    the created timestamp never showing up when there's a draft.
    
    This logic has been moved server-side to a reusable function that allows
    additional information (like the diffset information in the diff viewer)
    to be tacked on. Other views could potentially add their own information
    as well. This handles creating a consistent representation for each of
    the bits of status, reducing the complexity and improving the
    performance of the template in the process.
    
    Since this is more consolidated, it also allows for new unit tests that
    ensure the correct output.
    
    Testing Done:
    Unit tests pass.
    
    Tested various states manually in the review request page and the diff
    viewer.
    
    Reviewed at https://reviews.reviewboard.org/r/9628/

---
+++ reviewboard/reviews/tests/test_review_request_view_mixin.py
@@ -0,0 +1,133 @@
+"""Unit tests for reviewboard.reviews.views.ReviewRequestViewMixin."""
+
+from __future__ import unicode_literals
+
+from datetime import datetime
+
+import pytz
+from django.utils import timezone
+
+from reviewboard.reviews.models import ReviewRequest
+from reviewboard.reviews.views import ReviewRequestViewMixin
+from reviewboard.testing import TestCase
+
+
+_local_timezone = pytz.timezone('US/Pacific')
+
+
+class ReviewRequestViewMixinTests(TestCase):
+    """Unit tests for reviewboard.reviews.views.ReviewRequestViewMixin."""
+
+    fixtures = ['test_users']
+
+    def test_get_review_request_status_html_with_submitted(self):
+        """Testing ReviewRequestViewMixin.get_review_request_status_html
+        with status=SUBMITTED
+        """
+        review_request = self.create_review_request(
+            status=ReviewRequest.SUBMITTED,
+            time_added=datetime(2018, 2, 10, 9, 23, 12, tzinfo=timezone.utc))
+
+        mixin = ReviewRequestViewMixin()
+        mixin.review_request = review_request
+
+        with timezone.override(_local_timezone):
+            html = mixin.get_review_request_status_html(
+                review_request_details=review_request,
+                close_info={
+                    'timestamp': datetime(2018, 2, 12, 14, 56, 00,
+                                          tzinfo=timezone.utc),
+                })
+
+        self.assertHTMLEqual(
+            html,
+            'Created Feb. 10, 2018 and submitted '
+            '<time class="timesince" datetime="2018-02-12T06:56:00-08:00">'
+            'Feb. 12, 2018, 6:56 a.m.</time>')
+
+    def test_get_review_request_status_html_with_discarded(self):
+        """Testing ReviewRequestViewMixin.get_review_request_status_html
+        with status=DISCARDED
+        """
+        review_request = self.create_review_request(
+            status=ReviewRequest.DISCARDED,
+            time_added=datetime(2018, 2, 10, 9, 23, 12, tzinfo=timezone.utc))
+
+        mixin = ReviewRequestViewMixin()
+        mixin.review_request = review_request
+
+        with timezone.override(_local_timezone):
+            html = mixin.get_review_request_status_html(
+                review_request_details=review_request,
+                close_info={
+                    'timestamp': datetime(2018, 2, 12, 14, 56, 00,
+                                          tzinfo=timezone.utc),
+                })
+
+        self.assertHTMLEqual(
+            html,
+            'Created Feb. 10, 2018 and discarded '
+            '<time class="timesince" datetime="2018-02-12T06:56:00-08:00">'
+            'Feb. 12, 2018, 6:56 a.m.</time>')
+
+    def test_get_review_request_status_html_with_pending_review(self):
+        """Testing ReviewRequestViewMixin.get_review_request_status_html
+        with status=PENDING_REVIEW
+        """
+        review_request = self.create_review_request(
+            status=ReviewRequest.PENDING_REVIEW,
+            time_added=datetime(2018, 2, 10, 9, 23, 12, tzinfo=timezone.utc),
+            last_updated=datetime(2018, 2, 10, 15, 19, 23,
+                                  tzinfo=timezone.utc))
+
+        mixin = ReviewRequestViewMixin()
+        mixin.review_request = review_request
+
+        with timezone.override(_local_timezone):
+            html = mixin.get_review_request_status_html(
+                review_request_details=review_request,
+                close_info={})
+
+        self.assertHTMLEqual(
+            html,
+            'Created Feb. 10, 2018 and updated '
+            '<time class="timesince" datetime="2018-02-10T07:19:23-08:00">'
+            'Feb. 10, 2018, 7:19 a.m.</time>')
+
+    def test_get_review_request_status_html_with_extra_info(self):
+        """Testing ReviewRequestViewMixin.get_review_request_status_html
+        with extra_info
+        """
+        review_request = self.create_review_request(
+            status=ReviewRequest.PENDING_REVIEW,
+            time_added=datetime(2018, 2, 10, 9, 23, 12, tzinfo=timezone.utc),
+            last_updated=datetime(2018, 2, 10, 15, 19, 23,
+                                  tzinfo=timezone.utc))
+
+        mixin = ReviewRequestViewMixin()
+        mixin.review_request = review_request
+
+        with timezone.override(_local_timezone):
+            html = mixin.get_review_request_status_html(
+                review_request_details=review_request,
+                close_info={},
+                extra_info=[
+                    {
+                        'text': '{var} updated at {timestamp}',
+                        'timestamp': datetime(2018, 2, 11, 23, 32, 00,
+                                              tzinfo=timezone.utc),
+                        'extra_vars': {
+                            'var': 'Thingie',
+                        },
+                    },
+                ])
+
+        self.assertHTMLEqual(
+            html,
+            'Created Feb. 10, 2018 and updated '
+            '<time class="timesince" datetime="2018-02-10T07:19:23-08:00">'
+            'Feb. 10, 2018, 7:19 a.m.</time>'
+            ' &mdash; '
+            'Thingie updated at '
+            '<time class="timesince" datetime="2018-02-11T15:32:00-08:00">'
+            'Feb. 11, 2018, 3:32 p.m.</time>')
