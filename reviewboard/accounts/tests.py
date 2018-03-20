37a8986d6d94918a1599cffeaeedf74dee9035f3
Barret Rennie <barret@beanbaginc.com>
2018-03-19 21:30:59 -0600

    Don't update syntax highlighting prefs when it is disabled globally
    When syntax highlighting was disabled globally, we were deleting the
    form field from the `AccountSettingsForm`. However, later on we were
    trying to read from this field, causing an exception and the my account
    page to not load.
    
    Now when syntax highlighting is disabled, we mark the field as disabled
    and refuse updates to it. It will also always render as if the user had
    it disabled so it does not appear in a disabled but always on state for
    the user.
    
    Testing Done:
    Ran unit tests.
    
    The my account page no longer raises an exception.
    
    Reviewed at https://reviews.reviewboard.org/r/9796/

--- reviewboard/accounts/tests.py
+++ reviewboard/accounts/tests.py
@@ -6,6 +6,8 @@ import nose
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.messages.middleware import MessageMiddleware
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.test.client import RequestFactory
@@ -28,6 +30,7 @@ from reviewboard.accounts.backends import (AuthBackend, auth_backends,
                                            StandardAuthBackend,
                                            unregister_auth_backend)
 from reviewboard.accounts.forms.pages import (AccountPageForm,
+                                              AccountSettingsForm,
                                               ChangePasswordForm,
                                               ProfileForm)
 from reviewboard.accounts.mixins import (CheckLoginRequiredViewMixin,
@@ -37,9 +40,12 @@ from reviewboard.accounts.models import (LocalSiteProfile,
                                          Profile,
                                          ReviewRequestVisit,
                                          Trophy)
-from reviewboard.accounts.pages import (AccountPage, get_page_classes,
+from reviewboard.accounts.pages import (AccountPage,
+                                        AccountSettingsPage,
+                                        get_page_classes,
                                         register_account_page_class,
                                         unregister_account_page_class)
+from reviewboard.extensions.tests import set_siteconfig_settings
 from reviewboard.site.urlresolvers import local_site_reverse
 from reviewboard.testing import TestCase
 
@@ -936,6 +942,76 @@ class LoginRequiredViewMixinTests(TestCase):
         response = view(request)
 
         self.assertIsInstance(response, HttpResponseRedirect)
+
+
+class AccountSettingsFormTests(TestCase):
+    """Unit tests for AccountSettingsForm."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up the test class."""
+        super(AccountSettingsFormTests, cls).setUpClass()
+
+        cls._request_factory = RequestFactory()
+        cls._middleware = [
+            SessionMiddleware(),
+            MessageMiddleware(),
+        ]
+
+    @add_fixtures(['test_users'])
+    def test_save_syntax_highlighting_disabled(self):
+        """Testing AccountSettingsForm.save() with
+        diffviewer_syntax_highlighting disabled
+        """
+        view = View()
+        user = User.objects.get(username='doc')
+
+        profile = user.get_profile()
+        profile.syntax_highlighting = True
+        profile.save()
+
+        request = self._build_request(user)
+        page = AccountSettingsPage(view, request, user)
+
+        settings = {'diffviewer_syntax_highlighting': False}
+
+        with set_siteconfig_settings(settings):
+            form = AccountSettingsForm(page, request, user, data={
+                'syntax_highlighting': False,
+                'timezone': profile.timezone,
+            })
+
+            self.assertTrue(form.is_valid())
+            form.save()
+
+        profile = Profile.objects.get(pk=profile.pk)
+        self.assertTrue(profile.syntax_highlighting)
+
+    def _build_request(self, user=None):
+        """Make a request and process it through middleware.
+
+        Args:
+            user (django.contrib.auth.models.User, optional):
+                The user for the request to be authenticated for.
+
+                If not provided, an
+                :py:class:`~django.contrib.auth.models.AnonymousUser`
+                will be assigned instead.
+
+        Returns:
+            django.http.HttpRequest:
+            The created request.
+        """
+        if user is None:
+            user = AnonymousUser()
+
+        request = self._request_factory.request()
+        request.user = user
+
+        for middleware in self._middleware:
+            middleware.process_request(request)
+
+        return request
 
 
 class UserProfileRequiredViewMixinTests(TestCase):
