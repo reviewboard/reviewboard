b49ba54e5931055dd2099f42d66d568aeffa7f85
Christian Hammond <christian@beanbaginc.com>
2018-10-13 02:00:51 -0700

    Change the string types for encrypted passwords from bytes to Unicode.
    `encrypt_password` and `decrypt_password` were previously expecting to
    work with byte strings, but given the nature of Python 2, those were
    implicitly being converted elsewhere to Unicode strings. In Python 3,
    they're not so interchangeable, and requiring/returning byte strings
    makes for too much hassle, breaking all sorts of code.
    
    These functions now expect to be dealing with Unicode strings. Both
    return Unicode strings and accept either byte strings or Unicode strings
    as values. This is actually safer than what we had before, since they
    were always turning into Unicode at some point, and now it's at a level
    that we control.
    
    Testing Done:
    All unit tests pass on Python 2.7.
    
    Unit tests that can be run on Python 3.x that used to break due to
    password string types now pass.
    
    Manually tested saving and loading passwords for repositories.
    
    Reviewed at https://reviews.reviewboard.org/r/10204/

--- reviewboard/scmtools/crypto_utils.py
+++ reviewboard/scmtools/crypto_utils.py
@@ -53,7 +53,7 @@ def get_default_aes_encryption_key():
         bytes:
         The default encryption key.
     """
-    return settings.SECRET_KEY[:16].encode('utf8')
+    return settings.SECRET_KEY[:16].encode('utf-8')
 
 
 def aes_encrypt(data, key=None):
@@ -82,7 +82,7 @@ def aes_encrypt(data, key=None):
             The encryption key was not in the right format.
     """
     if isinstance(data, six.text_type):
-        data = data.encode('utf8')
+        data = data.encode('utf-8')
 
     iv = os.urandom(AES_BLOCK_SIZE)
     cipher = _create_cipher(iv, key or get_default_aes_encryption_key())
@@ -130,8 +130,13 @@ def encrypt_password(password, key=None):
     The password will be encrypted using AES encryption in CFB mode (using an
     8-bit shift register), and serialized into Base64.
 
+    Version Changed:
+        4.0:
+        The return type has been changed to :py:class:`unicode`, in order to
+        improve the expected behavior on Python 3.
+
     Args:
-        password (bytes):
+        password (unicode or bytes):
             The password to encrypt. If a unicode string is passed in, it will
             be encoded to UTF-8 first.
 
@@ -141,14 +146,14 @@ def encrypt_password(password, key=None):
             :py:func:`get_default_aes_encryption_key)` will be used.
 
     Returns:
-        bytes:
+        unicode:
         The encrypted password encoded in Base64.
 
     Raises:
         ValueError:
             The encryption key was not in the right format.
     """
-    return base64.b64encode(aes_encrypt(password, key=key))
+    return base64.b64encode(aes_encrypt(password, key=key)).decode('utf-8')
 
 
 def decrypt_password(encrypted_password, key=None):
@@ -157,8 +162,13 @@ def decrypt_password(encrypted_password, key=None):
     This will decrypt a Base64-encoded encrypted password (from
     :py:func:`encrypt_password`) into a usable password string.
 
+    Version Changed:
+        4.0:
+        The return type has been changed to :py:class:`unicode`, in order to
+        improve the expected behavior on Python 3.
+
     Args:
-        encrypted_password (bytes):
+        encrypted_password (unicode or bytes):
             The Base64-encoded encrypted password to decrypt.
 
         key (bytes, optional):
@@ -167,14 +177,17 @@ def decrypt_password(encrypted_password, key=None):
             (from :py:func:`get_default_aes_encryption_key)` will be used.
 
     Returns:
-        bytes:
+        unicode:
         The resulting password.
 
     Raises:
         ValueError:
             The encryption key was not in the right format.
     """
-    return aes_decrypt(base64.b64decode(encrypted_password), key=key)
+    return (
+        aes_decrypt(base64.b64decode(encrypted_password), key=key)
+        .decode('utf-8')
+    )
 
 
 # The following are deprecated. They're likely not used anywhere, but we
