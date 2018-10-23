88448fc3495d23188bb86f5af5e0087f88807e4c
Storm Kaefer <stormkaefer@gmail.com>
2018-10-23 11:45:44 -0700

    Fix rendering new widgets in admin dashboard
    The dashboard on /admin/ crashed when a brand new widget was added to the page.
    The expected output is that the new widget, whether it's a primary or secondary
    widget, should appear on the dashboard with the other widgets.
    
    This was because there was a check to find the new widgets position, before it
    actually had been given one. This raised a `KeyError`, which crashed the page.
    
    I added some .get() checks, so that if the widget position is not in the
    dictionary, a None value is returned.
    
    I also refactored code on how new widgets are rendered in the dashboard, so
    that the `dashboard()` function in `admin/views.py` is much easier to read.
    
    Testing Done:
    I ran all tests using  `./tests/runtests.py` and no errors resulted.
    
    I also used the JS-tests on /js-tests/, and there were no issues.
    
    I created a Unit Test class named WidgetTests, that can be run with
    `./tests/runtests.py reviewboard.admin.tests:WidgetTests`. This test checks
    that adding a new primary widget and a new secondary widget on the dashboard
    does not raise a KeyError, and gives the two widgets a position.
    
    Bugs closed: 4747
    
    Reviewed at https://reviews.reviewboard.org/r/10165/

--- reviewboard/admin/tests.py
+++ reviewboard/admin/tests.py
@@ -11,6 +11,11 @@ from djblets.siteconfig.models import SiteConfiguration
 from reviewboard.admin import checks
 from reviewboard.admin.forms import SearchSettingsForm
 from reviewboard.admin.validation import validate_bug_tracker
+from reviewboard.admin.widgets import (Widget,
+                                       primary_widgets,
+                                       register_admin_widget,
+                                       secondary_widgets,
+                                       unregister_admin_widget)
 from reviewboard.search import search_backend_registry
 from reviewboard.search.search_backends.base import (SearchBackend,
                                                      SearchBackendForm)
@@ -233,3 +238,95 @@ class SSHSettingsFormTestCase(TestCase):
 
         # Check whether the key has been deleted.
         self.assertEqual(self.ssh_client.get_user_key(), None)
+
+
+class WidgetTests(TestCase):
+    """Tests for administrator dashboard widgets."""
+
+    fixtures = ['test_users']
+
+    def setUp(self):
+        """Set up this test case."""
+        super(WidgetTests, self).setUp()
+
+    def tearDown(self):
+        """Tear down this test case."""
+        super(WidgetTests, self).tearDown()
+
+    def test_new_widget_render(self):
+        """Testing that a new widget renders in the admin dashboard"""
+        self.client.login(username='admin', password='admin')
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 200)
+        total_inital_widgets = (
+            len(response.context['selected_secondary_widgets']) +
+            len(response.context['selected_primary_widgets']))
+
+        # Since admin/views.py widget_select() does not get run in testing,
+        # we must do this instead to set up the data.
+        profile = response.context['user'].get_profile()
+        profile.extra_data.update({
+            'primary_widget_selections': {
+                widget.widget_id: '1'
+                for widget in primary_widgets
+            },
+            'secondary_widget_selections': {
+                widget.widget_id: '1'
+                for widget in secondary_widgets
+            },
+            'primary_widget_positions': {
+                widget.widget_id: i
+                for i, widget in enumerate(primary_widgets)
+            },
+            'secondary_widget_positions': {
+                widget.widget_id: i
+                for i, widget in enumerate(secondary_widgets)
+            },
+        })
+        profile.save(update_fields=('extra_data',))
+
+        class TestPrimaryWidget(Widget):
+            widget_id = 'test-primary-widget'
+
+        class TestSecondaryWidget(Widget):
+            widget_id = 'test-secondary-widget'
+
+        # If either new widget doesn't render correctly, the page will break.
+        try:
+            register_admin_widget(TestPrimaryWidget, True)
+            register_admin_widget(TestSecondaryWidget)
+
+            # We must also add TestPrimaryWidget to primary_widget_selections
+            # and TestSecondaryWidget to secondary_widget_selections, so that
+            # they are selected to display on the page, but have no position.
+            primary_selections = (
+                profile.extra_data['primary_widget_selections'])
+            secondary_selections = (
+                profile.extra_data['secondary_widget_selections'])
+            primary_selections[TestPrimaryWidget.widget_id] = '1'
+            secondary_selections[TestSecondaryWidget.widget_id] = '1'
+            profile.save(update_fields=('extra_data',))
+
+            response = self.client.get('/admin/')
+            self.assertEqual(response.status_code, 200)
+            total_tested_widgets = (
+                len(response.context['selected_secondary_widgets']) +
+                len(response.context['selected_primary_widgets']))
+            self.assertTrue(total_tested_widgets == total_inital_widgets + 2)
+            self.assertIn(TestPrimaryWidget,
+                          response.context['selected_primary_widgets'])
+            self.assertIn(TestSecondaryWidget,
+                          response.context['selected_secondary_widgets'])
+
+        finally:
+            # If an error was encountered above, the widgets will not be
+            # registered. Ignore any errors in that case.
+            try:
+                unregister_admin_widget(TestPrimaryWidget)
+            except KeyError:
+                pass
+
+            try:
+                unregister_admin_widget(TestSecondaryWidget)
+            except KeyError:
+                pass
