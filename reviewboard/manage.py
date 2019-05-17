78958be8563145c25c7aeb4d7d677b7d4214fb07
Christian Hammond <christian@beanbaginc.com>
2019-05-17 02:17:04 -0700

    Stop attempting to load extensions in manage.py.
    `manage.py` attempted to load any extensions in order to factor in their
    models for database upgrades and management commands for execution. This
    hasn't actually been needed for a while, and isn't compatible with
    Django 1.11 at all.
    
    This change removes this logic entirely from `manage.py`.
    
    Testing Done:
    Verified that management commands worked correctly on Django 1.6 and 1.11,
    and that upgrades worked on Django 1.6 (and were attempted on 1.11, though
    that requires more work to complete compatibility).
    
    Reviewed at https://reviews.reviewboard.org/r/10531/

--- reviewboard/manage.py
+++ reviewboard/manage.py
@@ -115,28 +115,6 @@ def check_dependencies(settings):
     fail_if_missing_dependencies()
 
 
-def include_enabled_extensions(settings):
-    """
-    This adds enabled extensions to the INSTALLED_APPS cache
-    so that operations like syncdb and evolve will take extensions
-    into consideration.
-    """
-    from django.db.models.loading import load_app
-    from django.db import DatabaseError
-
-    from reviewboard.extensions.base import get_extension_manager
-
-    try:
-        manager = get_extension_manager()
-    except DatabaseError:
-        # This database is from a time before extensions, so don't attempt to
-        # load any extensions yet.
-        return
-
-    for extension in manager.get_enabled_extensions():
-        load_app(extension.info.app_name)
-
-
 def upgrade_database():
     """Perform an upgrade of the database.
 
@@ -268,8 +246,6 @@ def main(settings, in_subprocess):
             # done.
             upgrade_database()
             return
-
-        include_enabled_extensions(settings)
 
     execute_from_command_line(sys.argv)
 
