2e6450663a8a614253d05e09b88e2a5f3d1e853c
Christian Hammond <christian@beanbaginc.com>
2019-05-17 02:16:10 -0700

    Fix the OAuth app on Django 1.11.
    Our `reviewboard.oauth` app had an app label of `reviewboard_oauth`,
    which didn't match any other references to the app. The custom label
    affected admin URLs, the label for the app in the database UI (though
    this is easily correctable), and some internal machinery of the
    `oauth2_provider` module. This only applied on Django 1.7+, meaning that
    we had a major discrepency between 1.6 and newer versions.
    
    To keep things compatible with what we've had in the past, we're going
    to continue using an app label of `oauth`.
    
    We're also lazily importing the method for enabling OAuth2 scopes, in
    order to avoid breaking the Django app initialization process.
    
    Testing Done:
    Unit tests pass on Django 1.6 and 1.11.
    
    Reviewed at https://reviews.reviewboard.org/r/10523/

--- reviewboard/oauth/__init__.py
+++ reviewboard/oauth/__init__.py
@@ -5,11 +5,22 @@ This module loads the WebAPI scopes when Review Board initializes.
 
 from __future__ import unicode_literals
 
-from djblets.webapi.oauth2_scopes import enable_web_api_scopes
+from django.dispatch import receiver
+
 from reviewboard.signals import initializing
 
 
 default_app_config = 'reviewboard.oauth.apps.OAuthAppConfig'
 
 
-initializing.connect(enable_web_api_scopes)
+@receiver(initializing)
+def _on_initializing(**kwargs):
+    """Enable OAuth scopes for the API when initializing Review Board.
+
+    Args:
+        **kwargs (dict):
+            Keyword arguments from the signal.
+    """
+    from djblets.webapi.oauth2_scopes import enable_web_api_scopes
+
+    enable_web_api_scopes()
