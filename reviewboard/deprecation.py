fbadaea7588cc95cdfb1d434012dfa060b8c0ae6
David Trowbridge <trowbrds@gmail.com>
2020-11-11 15:21:15 -0700

    Backport RemovedInReviewBoard50Warning to 3.0.x
    We'll be making a small API change to allow SCMTool filename
    normalization to make use of the extra_data field. We'll want this to be
    3.0.19 and newer, but won't actually remove the deprecation of the older
    APIs until 5.0. The release-4.0.x branch has the
    `RemovedInReviewBoard50Warning` and the `warn` helper method, but 3.0.x
    didn't. This change backports it.
    
    Testing Done:
    Used in conjunction with other changes.
    
    Reviewed at https://reviews.reviewboard.org/r/11272/

--- reviewboard/deprecation.py
+++ reviewboard/deprecation.py
@@ -7,6 +7,8 @@ stable.
 
 from __future__ import unicode_literals
 
+import warnings
+
 
 class BaseRemovedInReviewBoardVersionWarning(DeprecationWarning):
     """Base class for a Review Board deprecation warning.
@@ -15,6 +17,23 @@ class BaseRemovedInReviewBoardVersionWarning(DeprecationWarning):
     callers to check for Review Board deprecations without being tied to a
     specific version.
     """
+
+    @classmethod
+    def warn(cls, message, stacklevel=2):
+        """Emit the deprecation warning.
+
+        This is a convenience function that emits a deprecation warning using
+        this class, with a suitable default stack level. Callers can provide
+        a useful message and a custom stack level.
+
+        Args:
+            message (unicode):
+                The message to show in the deprecation warning.
+
+            stacklevel (int, optional):
+                The stack level for the warning.
+        """
+        warnings.warn(message, cls, stacklevel=stacklevel + 1)
 
 
 class RemovedInReviewBoard40Warning(BaseRemovedInReviewBoardVersionWarning):
@@ -25,6 +44,15 @@ class RemovedInReviewBoard40Warning(BaseRemovedInReviewBoardVersionWarning):
     :py:class:`BaseRemovedInReviewBoardVersionWarning`. Alternatively, you
     can use the alias for this class,
     :py:data:`RemovedInNextReviewBoardVersionWarning`.
+    """
+
+
+class RemovedInReviewBoard50Warning(BaseRemovedInReviewBoardVersionWarning):
+    """Deprecations for features removed in Review Board 5.0.
+
+    Note that this class will itself be removed in Review Board 5.0. If you
+    need to check against Review Board deprecation warnings, please see
+    :py:class:`BaseRemovedInReviewBoardVersionWarning`.
     """
 
 
