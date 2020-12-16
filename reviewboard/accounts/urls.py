18b7ef8e18c8b3bf71f3162d1a863c4287cae561
Christian Hammond <christian@beanbaginc.com>
2020-12-16 14:22:09 -0800

    Use newer Django auth-related views, and fix a Reset Password e-mail issue.
    Django is getting rid of their legacy auth-related function-based views
    and moving to class-based views. Usage of the legacy views emits a
    deprecation warning (if enabled). This change updated our URLs to use
    the modern views.
    
    This was discovered while investigating an issue with Reset Password
    e-mails, where the text would say "Thank you for using !" instead of
    "Thank you for using Review Board!" That happened because the e-mail
    context is slim, and doesn't have access to Django settings. We now pass
    in the product name using one of the class-based view's attributes for
    setting e-mail context.
    
    Testing Done:
    Tested each auth view, making sure nothing regressed.
    
    Reset my password. Verified that the text was correct in the e-mail.
    
    Reviewed at https://reviews.reviewboard.org/r/11334/

--- reviewboard/accounts/urls.py
+++ reviewboard/accounts/urls.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
@@ -9,11 +10,9 @@ from reviewboard.accounts import views as accounts_views
 
 urlpatterns = [
     url(r'^login/$',
-        auth_views.login,
-        kwargs={
-            'template_name': 'accounts/login.html',
-            'authentication_form': AuthenticationForm,
-        },
+        auth_views.LoginView.as_view(
+            template_name='accounts/login.html',
+            authentication_form=AuthenticationForm),
         name='login'),
     url(r'^logout/$',
         auth_views.logout_then_login,
@@ -32,30 +31,25 @@ urlpatterns = [
         },
         name='register'),
     url(r'^recover/$',
-        auth_views.password_reset,
-        kwargs={
-            'template_name': 'accounts/password_reset.html',
-            'email_template_name': 'accounts/password_reset_email.txt'
-        },
+        auth_views.PasswordResetView.as_view(
+            template_name='accounts/password_reset.html',
+            email_template_name='accounts/password_reset_email.txt',
+            extra_email_context={
+                'PRODUCT_NAME': settings.PRODUCT_NAME,
+            }),
         name='recover'),
     url(r'^recover/done/$',
-        auth_views.password_reset_done,
-        kwargs={
-            'template_name': 'accounts/password_reset_done.html',
-        },
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_done.html'),
         name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/'
         r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm,
-        kwargs={
-            'template_name': 'accounts/password_reset_confirm.html',
-        },
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html'),
         name='password_reset_confirm'),
     url(r'^reset/done/$',
-        auth_views.password_reset_complete,
-        kwargs={
-            'template_name': 'accounts/password_reset_complete.html',
-        },
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_complete.html'),
         name='password_reset_complete'),
     url(r'^preferences/oauth2-application/(?:(?P<app_id>[0-9]+)/)?$',
         accounts_views.edit_oauth_app,
