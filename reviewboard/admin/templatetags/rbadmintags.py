73aeba27ba095057f04bd082ed9c85338950ebed
Christian Hammond <christian@beanbaginc.com>
2020-12-23 13:40:55 -0800

    Fix the defaults around the Save buttons in Settings/Model Change forms.
    The new template for the change forms tried to use defaults if variables
    for controlling the presence of the Save buttons weren't provided.
    However, it did this using `|default:True`, which meant that a value of
    `False` would be turned into `True`. Not what we wanted.
    
    That, however, was put in place (in change 663f50d) in order to fix a
    prior (incorrect) attempt that used `|default_if_none`. That could have
    worked if that filter considered a missing variable to be equivalent to
    `None`, but it didn't.
    
    Even with these attempts, we had two code paths for generating that
    template: Ours, and Django's, depending on whether a change form was
    being rendered.
    
    This change implements a more future-proof, reliable attempt at
    generating these buttons. We now have a dedicated template tag that
    covers the logic in Django's own `submit_row` template tag and our own
    logic, and expands upon that to allow greater customization by callers
    (a Delete button is now possible without the rest of the admin classes).
    
    With this, the Settings forms and Model Change forms all now show the
    correct buttons.
    
    Testing Done:
    Tested all the Settings forms, various model change forms, and the
    integrations/extensions forms (which use their own templates anyway).
    
    Reviewed at https://reviews.reviewboard.org/r/11344/

--- reviewboard/admin/templatetags/rbadmintags.py
+++ reviewboard/admin/templatetags/rbadmintags.py
@@ -4,7 +4,12 @@ import re
 
 from django import template
 from django.contrib import messages
+from django.contrib.admin.templatetags.admin_urls import (
+    add_preserved_filters,
+    admin_urlname,
+    admin_urlquote)
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.template.context import RequestContext
 from django.utils import six
 from django.utils.safestring import mark_safe
@@ -191,6 +196,76 @@ def changelist_js_model_attrs(context):
         ]
 
     return json_dumps_items(model_data)
+
+
+@register.inclusion_tag('admin/submit_line.html', takes_context=True)
+def change_form_submit_buttons(context):
+    """Return HTML for a change form's submit buttons.
+
+    This will compute the correct set of Save/Delete buttons, based on whether
+    this is rendering for a Django admin change form (taking into account
+    the object's state and user's permissions) or for any other type of form.
+
+    Args:
+        context (django.template.Context):
+            The context for the page.
+
+    Returns:
+        django.utils.safestring.SafeText:
+        A string containing the submit buttons.
+    """
+    show_save = context.get('show_save', True)
+
+    if 'change' in context:
+        change = context['change']
+        is_popup = context['is_popup']
+        show_delete = context.get('show_delete', True)
+
+        if is_popup:
+            show_delete = False
+            show_save_as_new = False
+            show_save_and_add_another = False
+            show_save_and_continue = False
+            delete_url = None
+        else:
+            save_as = context['save_as']
+            opts = context['opts']
+            original = context['original']
+
+            show_delete = (
+                change is not None and
+                context.get('show_delete', True) and
+                context['has_delete_permission'])
+            show_save_as_new = (
+                save_as and
+                change is not None)
+            show_save_and_add_another = (
+                (not save_as or context['add']) and
+                context['has_add_permission'])
+            show_save_and_continue = (
+                context.get('show_save_and_continue', True) and
+                context['has_change_permission'])
+
+            delete_url = add_preserved_filters(
+                context,
+                reverse(admin_urlname(opts, 'delete'),
+                        args=[admin_urlquote(original.pk)]))
+    else:
+        delete_url = context.get('delete_url', '#')
+        show_delete = context.get('show_delete', False)
+        show_save_as_new = context.get('show_save_as_new', False)
+        show_save_and_add_another = context.get('show_save_and_add_another',
+                                                False)
+        show_save_and_continue = context.get('show_save_and_continue', False)
+
+    return {
+        'delete_url': delete_url,
+        'show_delete_link': show_delete,
+        'show_save': show_save,
+        'show_save_and_add_another': show_save_and_add_another,
+        'show_save_and_continue': show_save_and_continue,
+        'show_save_as_new': show_save_as_new,
+    }
 
 
 @register.filter
