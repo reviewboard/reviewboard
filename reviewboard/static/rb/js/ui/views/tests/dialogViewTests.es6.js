bceb0b624a4d59afdba7143e67e6b8d508252654
Hannah Lin <hxqlin@gmail.com>
2021-01-27 22:41:16 -0700

    Add event handlers to RB.DialogView
    This patch adds event handlers to `RB.DialogView` for `submit` and
    `keydown`. The form `submit` event handler fixes the bug where pressing
    Enter on the upload file dialog (`RB.UploadAttachmentView`) would cancel
    the file upload instead of actually uploading it. Pressing the return
    key on `RB.DialogView` dialogs that have a completed form will now
    submit the form instead of reloading the page and discarding changes
    unless the form specifies an action explicitly. The `keydown` event
    handler allows the dialog to be closed when the Escape key is pressed.
    
    Since `RB.DialogView` is a child of the `modalBox`, it needs to listen
    to `keydown` events from the main box area and not from its own content
    area so that it can handle events from within it. This means we can't
    use the events map for the `keydown` event and have to bind an explicit
    `keydown` event handler on `.modalbox-inner`.
    
    Testing Done:
    New unit tests:
    - triggering submit with primary button enabled triggers the button
    - triggering submit with primary button disabled on a dialog does
      nothing
    - triggering submit with primary button enabled on a dialog with a
      form does nothing
    - triggering keydown using the Escape key hides the dialog
    
    Manual testing:
    File upload dialog:
    - pressing Enter after opening the dialog and not filling out any
      fields does nothing
    - pressing Enter after uploading a file & editing the caption uploads
      the file with the caption
    - pressing Escape after opening the dialog hides the dialog
    
    New review request dialog:
    - pressing Escape after opening the dialog hides the dialog
    
    Fixes bug 4847.
    
    Reviewed at https://reviews.reviewboard.org/r/10967/

--- reviewboard/static/rb/js/ui/views/tests/dialogViewTests.es6.js
+++ reviewboard/static/rb/js/ui/views/tests/dialogViewTests.es6.js
@@ -140,6 +140,141 @@ suite('rb/ui/views/DialogView', function() {
                     button.click();
                     expect(dialogView._onClicked).toHaveBeenCalled();
                 });
+
+                describe('Keydown', function() {
+                    it('Esc key', function() {
+                        const dialogView = new RB.DialogView({
+                            buttons: [{
+                                id: 'testid',
+                                label: 'Test',
+                            }],
+                        });
+
+                        dialogView.show();
+
+                        const myFunc = jasmine.createSpy('hide');
+                        dialogView.hide = myFunc;
+
+                        const buttons = dialogView.$buttonsMap;
+                        expect(Object.keys(buttons).length).toBe(1);
+                        expect(buttons.testid.val()).toBe('Test');
+
+                        dialogView.$el.trigger(
+                            $.Event('keydown', { which: $.ui.keyCode.ESCAPE }));
+
+                        expect(myFunc).toHaveBeenCalled();
+
+                        dialogView.remove();
+                    });
+                });
+
+                describe('Submit', function() {
+                    it('with primary button enabled', function() {
+                        const myFunc = jasmine.createSpy('cb');
+                        const dialogView = new RB.DialogView({
+                            buttons: [{
+                                id: 'testid',
+                                label: 'Test',
+                                onClick: myFunc,
+                                disabled: false,
+                                primary: true,
+                            }],
+                            body: _.template(dedent`
+                                <form>
+                                 <input value="test">
+                                </form>
+                            `),
+                        });
+
+                        dialogView.show();
+
+                        const buttons = dialogView.$buttonsMap;
+                        const button = buttons.testid;
+                        const form = dialogView.$el.find('form');
+
+                        expect(Object.keys(buttons).length).toBe(1);
+                        expect(button.val()).toBe('Test');
+                        expect(button.prop('disabled')).toBe(false);
+                        expect(button.hasClass('primary')).toBe(true);
+
+                        form.trigger($.Event('submit'));
+                        expect(myFunc).toHaveBeenCalled();
+
+                        dialogView.remove();
+                    });
+
+                    it('with primary button disabled', function() {
+                        const myFunc = jasmine.createSpy('cb');
+                        const dialogView = new RB.DialogView({
+                            buttons: [{
+                                id: 'testid',
+                                label: 'Test',
+                                onClick: myFunc,
+                                disabled: true,
+                                primary: true,
+                            }],
+                            body: _.template(dedent`
+                                <form>
+                                 <input value="test">
+                                </form>
+                            `),
+                        });
+
+                        dialogView.show();
+
+                        const buttons = dialogView.$buttonsMap;
+                        const button = buttons.testid;
+                        const form = dialogView.$el.find('form');
+
+                        expect(Object.keys(buttons).length).toBe(1);
+                        expect(button.val()).toBe('Test')
+                        expect(button.prop('disabled')).toBe(true);
+                        expect(button.hasClass('primary')).toBe(true);
+
+                        form.trigger($.Event('submit'));
+                        expect(myFunc).not.toHaveBeenCalled();
+
+                        dialogView.remove();
+                    });
+
+                    it('with explicit action', function() {
+                        const myFunc1 = jasmine.createSpy('cb1');
+                        const myFunc2 = jasmine.createSpy('cb2')
+                            .and.returnValue(false);
+                        const dialogView = new RB.DialogView({
+                            buttons: [{
+                                id: 'testid',
+                                label: 'Test',
+                                onClick: myFunc1,
+                                disabled: false,
+                                primary: true,
+                            }],
+                            body: _.template(dedent`
+                                <form action=".">
+                                 <input value="test">
+                                </form>
+                            `),
+                        });
+
+                        dialogView.show();
+
+                        const buttons = dialogView.$buttonsMap;
+                        const button = buttons.testid;
+                        const form = dialogView.$el.find('form');
+                        form.submit(myFunc2);
+
+                        expect(Object.keys(buttons).length).toBe(1);
+                        expect(button.val()).toBe('Test');
+                        expect(button.prop('disabled')).toBe(false);
+                        expect(button.hasClass('primary')).toBe(true);
+
+                        form.trigger($.Event('submit'));
+                        expect(myFunc1).not.toHaveBeenCalled();
+                        expect(myFunc2).toHaveBeenCalled();
+
+                        dialogView.remove();
+                    });
+                });
             });
         });
     });
