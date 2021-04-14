6a07b28dde3caae22a1e9db644b2c87e1cbbd56d
Christian Hammond <christian@beanbaginc.com>
2021-04-13 17:28:49 -0700

    Remove all functionality from the dumpdb/loaddb commands.
    Due to changes within Django's serialization components, and issues with
    preserving any compatibility between Django 1.6's output and 1.11's,
    we've decided to remove the `dumpdb` and `loaddb` commands. Native
    database tools are almost always a better option anyway, and we'll be
    providing an enhanced replacement for these tools as part of Power Pack
    pretty soon anyway.
    
    See https://reviews.reviewboard.org/r/11592/ for a summary of the
    reasons we can't continue to maintain these tools.
    
    This change does not get rid of the commands themselves. Rather, it
    strips all functionality and provides an error message that explains why
    we're removing the tools and the alternative approaches available to
    them.
    
    Testing Done:
    Tested both commands, with and without parameters. They produced the
    expected message.
    
    Verified we weren't referencing these anywhere in our docs.
    
    Bugs closed: 4920, 4921
    
    Reviewed at https://reviews.reviewboard.org/r/11593/

--- reviewboard/admin/management/commands/loaddb.py
+++ reviewboard/admin/management/commands/loaddb.py
@@ -2,131 +2,58 @@
 
 from __future__ import unicode_literals
 
-import importlib
-import os
-import re
+import textwrap
+import sys
 
-from django import db
-from django.core import serializers
-from django.core.management.base import CommandError
-from django.db import transaction
-from django.utils.six.moves import input
-from django.utils.translation import ugettext as _
-from django_evolution.compat.apps import get_apps
 from djblets.util.compat.django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     """Management command to load data into the database."""
 
-    help = _('Loads data formatted by dumpdb, for migration across types '
-             'of databases.')
+    help = (
+        "[No longer supported] Loads data formatted by dumpdb.\n"
+        "\n"
+        "This functionality has been removed. Please use your database's "
+        "native tools instead, or contact support@beanbaginc.com for "
+        "alternative solutions."
+    )
 
-    def handle(self, *args, **options):
+    def add_arguments(self, parser):
+        """Add arguments to the command.
+
+        Args:
+            parser (argparse.ArgumentParser):
+                The argument parser for the command.
+        """
+        parser.add_argument(
+            'filename',
+            metavar='NAME',
+            nargs='*',
+            help='The name of the file to load.')
+
+    def handle(self, **options):
         """Handle the command.
 
         Args:
             **options (dict, unused):
-                Options parsed on the command line. For this command, no
-                options are available.
+                Options parsed on the command line.
 
         Raises:
             django.core.management.CommandError:
                 There was an error with arguments or disabling the extension.
         """
-        if len(args) != 1:
-            raise CommandError(
-                _('You must specify a filename on the command line.'))
+        self.stderr.write('\n')
+        self.stderr.write(textwrap.fill(
+            "dumpdb and loaddb are no longer supported. They weren't meant "
+            "for production installs, and we weren't able to retain "
+            "compatibility with the version of Django now used by "
+            "Review Board. We recommend using your database's "
+            "native SQL dumping and loading tools instead.\n"))
+        self.stderr.write('\n')
+        self.stderr.write(textwrap.fill(
+            "If you need this functionality, or assistance with "
+            "transitioning databases, you can contact us at "
+            "support@beanbaginc.com for options."))
 
-        filename = args[0]
-
-        if not os.path.exists(filename):
-            raise CommandError(_('%s does not exist.') % filename)
-
-        try:
-            importlib.import_module('django_reset')
-        except ImportError:
-            raise CommandError(_('Before using this command, you need to '
-                                 'install the "django-reset" package'))
-
-        confirm = input("""
-This will wipe out your existing database prior to loading. It is highly
-recommended that you have a full SQL database dump in case things go wrong.
-
-You should only use this if you're migrating from one type of database to
-another, with the same version of Review Board on each.
-
-Are you sure you want to continue?"
-
-Type 'yes' to continue, or 'no' to cancel: """)
-
-        if confirm != 'yes':
-            return
-
-        apps = [app.__name__.split('.')[-2] for app in get_apps()]
-
-        os.system('./reviewboard/manage.py reset --noinput %s'
-                  % ' '.join(apps))
-
-        transaction_setup = False
-
-        try:
-            with open(filename, 'r') as f:
-                line = f.readline()
-
-                m = re.match(r'^# dbdump v(\d+) - (\d+) objects$', line)
-
-                if not m:
-                    raise CommandError(_('Unknown dump format'))
-
-                version = int(m.group(1))
-                totalobjs = int(m.group(2))
-                i = 0
-                prev_pct = -1
-
-                if version != 1:
-                    raise CommandError(_('Unknown dump version'))
-
-                transaction.commit_unless_managed()
-                transaction.enter_transaction_management()
-                transaction.managed(True)
-                transaction_setup = True
-
-                self.stdout.write(
-                    _('Importing new style dump format (v%s)')
-                    % version)
-
-                for line in f:
-                    if line[0] == '{':
-                        for obj in serializers.deserialize('json',
-                                                           '[%s]' % line):
-                            try:
-                                obj.save()
-                            except Exception as e:
-                                self.stderr.write(_('Error: %s') % e)
-                                self.stderr.write(_('Line %s: "%s"')
-                                                  % (i, line))
-                    elif line[0] != '#':
-                        self.stderr.write(_('Junk data on line %s') % i)
-
-                    db.reset_queries()
-
-                    i += 1
-                    pct = (i * 100 / totalobjs)
-
-                    if pct != prev_pct:
-                        self.stdout.write('  [%s%%]\r' % pct)
-                        self.stdout.flush()
-                        prev_pct = pct
-
-            transaction.commit()
-            transaction.leave_transaction_management()
-        except Exception as e:
-            if transaction_setup:
-                transaction.rollback()
-                transaction.leave_transaction_management()
-
-            raise CommandError(_('Problem installing "%s": %s')
-                               % (filename, e))
-
-        self.stdout.write(_('\nDone.'))
+        sys.exit(1)
