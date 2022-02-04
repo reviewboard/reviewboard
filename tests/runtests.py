c5a3dd433f93d26bb505137bbb965bf516d7dfcd
Christian Hammond <christian@beanbaginc.com>
2022-02-03 17:06:47 -0800

    Enable support for pytest as our test runner.
    This switches us to using pytest for running unit tests, along with a
    couple of suitable plugins. pytest is maintained, as a wide ecosystem of
    plugins, and offers us options for rethinking our testing infrastructure
    down the road.
    
    For the moment, we're still building unit tests around Python's
    `unittest` library, Django's `django.test`, and Djblets's
    `djblets.testing`. We'll probably continue to do this for the
    foreseeable future, since there are benefits to the `unittest`-based
    approach still, and switching everything over to pytest's view of
    testing is a big project.
    
    The configuration has a number of warnings that we disable. As we finish
    the move to Django 3.2, we'll be able to remove many of these.
    
    One important thing to note is that the `./reviewboard/manage.py test`
    command will still attempt to use the old test runner. Extensions use
    that test runner as well, making that a tricky thing to cleanly switch
    out.
    
    So technically, we currently have two test runners we're using, but
    pytest will be the official way to run the test suite for Review Board
    going forward.
    
    Testing Done:
    Ran pytest.
    
    Unit tests are currently broken on `release-5.0.x`, but pytest at least
    has the same errors (at least until I had to kill the suite).
    
    This change was originally on `release-4.0.x`, where it passed the entire
    suite.
    
    Reviewed at https://reviews.reviewboard.org/r/12024/

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,33 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from __future__ import print_function, unicode_literals
-
-import os
 import sys
+
+import pytest
 
 
 if __name__ == '__main__':
-    os.chdir(os.path.join(os.path.dirname(__file__), '..'))
-    sys.path.insert(0, os.getcwd())
-
-    import django
-    import djblets
-    import reviewboard
-
-    print('Review Board %s' % reviewboard.get_version_string())
-    print('Djblets %s' % djblets.get_version_string())
-    print('Django %s' % django.__version__)
-    print('Python %s.%s.%s' % sys.version_info[:3])
-    print()
-
-    # We're just wrapping the manage script. Both that script and the test
-    # runner are expecting sys.argv to be set. Fake it here so we don't have
-    # to shell out to another process just to get a proper set of arguments.
-    sys.argv = [sys.argv[0], 'test', '--'] + sys.argv[1:]
-
-    os.environ[str('RB_RUNNING_TESTS')] = str('1')
-    os.environ[str('RBSSH_STORAGE_BACKEND')] = \
-        str('reviewboard.ssh.storage.FileSSHStorage')
-
-    from reviewboard.manage import run as run_manage
-    run_manage()
+    sys.exit(pytest.main(sys.argv[1:]))
