c3694d03b8911aea231d9acab422d1962d241b26
Christian Hammond <christian@beanbaginc.com>
2022-09-13 23:37:54 -0700

    Add stale Ship It! styling and ARIA support to the Ship It!/Issues column.
    This updates the column to help identify when a review request has been
    updated since receiving one or more Ship It! reviews. Now, if a review
    request has been updated, the Ship It! count still remains, but the
    color is grey instead of green, making it stand out and helping
    reviewers know that they should look at the change again.
    
    This also makes this column accessible. The indicator has an ARIA label
    and title that convey the contents, with the icon and count being hidden
    from the accessibility tree.
    
    The text is localized based on the number of items, so there's a
    different string for 1 Ship It or issue vs. many.
    
    Testing Done:
    Tested with every combination of Ship It/Issue state and stale state,
    verifying the tooltip and presentation.
    
    Unit tests pass.
    
    Bugs closed: 2106
    
    Reviewed at https://reviews.reviewboard.org/r/12593/

--- reviewboard/datagrids/tests.py
+++ reviewboard/datagrids/tests.py
@@ -1,5 +1,7 @@
 """Unit tests for reviewboard.datagrids."""
 
+from datetime import timedelta
+
 import kgb
 from django.contrib.auth.models import AnonymousUser, User
 from django.core.cache import cache
@@ -15,7 +17,9 @@ from reviewboard.accounts.models import (LocalSiteProfile,
                                          Profile,
                                          ReviewRequestVisit)
 from reviewboard.datagrids.builtin_items import UserGroupsItem, UserProfileItem
-from reviewboard.datagrids.columns import (FullNameColumn, SummaryColumn,
+from reviewboard.datagrids.columns import (FullNameColumn,
+                                           ShipItColumn,
+                                           SummaryColumn,
                                            UsernameColumn)
 from reviewboard.datagrids.grids import DashboardDataGrid
 from reviewboard.diffviewer.models import DiffSet
@@ -3282,6 +3286,217 @@ class FullNameColumnTests(BaseColumnTestCase):
         self.assertEqual(rendered,
                          '&lt;script&gt;alert(&quot;unsafe&quot;)'
                          '&lt;/script&gt; &quot;&quot;')
+
+
+class ShipItColumnTests(BaseColumnTestCase):
+    """Testing reviewboard.datagrids.columns.ShipItColumn."""
+
+    column = ShipItColumn()
+
+    def test_render_data_with_none(self):
+        """Testing ShipItColumn.render_data with 0 Ship It!'s, 0 issues"""
+        review_request = self.create_review_request(publish=True)
+
+        self.assertEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '')
+
+    def test_render_data_with_one_shipit(self):
+        """Testing ShipItColumn.render_data with 1 Ship It!"""
+        review_request = self.create_review_request(publish=True)
+        review_request.shipit_count = 1
+        review_request.last_review_activity_timestamp = (
+            review_request.last_updated + timedelta(hours=1))
+
+        self.assertHTMLEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '<div class="shipit-count-container"'
+            '     title="1 Ship It!"'
+            '     aria-label="1 Ship It!">'
+            ' <span aria-hidden="true" class="shipit-count">'
+            '  <span class="rb-icon rb-icon-datagrid-shipit"></span>'
+            '  1'
+            ' </span>'
+            '</div>')
+
+    def test_render_data_with_many_shipits(self):
+        """Testing ShipItColumn.render_data with many Ship It!'s"""
+        review_request = self.create_review_request(publish=True)
+        review_request.shipit_count = 2
+        review_request.last_review_activity_timestamp = (
+            review_request.last_updated + timedelta(hours=1))
+
+        self.assertHTMLEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '<div class="shipit-count-container"'
+            '     title="2 Ship It\'s!"'
+            '     aria-label="2 Ship It\'s!">'
+            ' <span aria-hidden="true" class="shipit-count">'
+            '  <span class="rb-icon rb-icon-datagrid-shipit"></span>'
+            '  2'
+            ' </span>'
+            '</div>')
+
+    def test_render_data_with_shipit_stale(self):
+        """Testing ShipItColumn.render_data with Ship It!'s and stale"""
+        review_request = self.create_review_request(publish=True)
+        review_request.shipit_count = 1
+        review_request.last_review_activity_timestamp = (
+            review_request.last_updated - timedelta(hours=1))
+
+        self.assertHTMLEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '<div class="shipit-count-container -is-stale"'
+            '     title="1 Ship It! (New updates to review)"'
+            '     aria-label="1 Ship It! (New updates to review)">'
+            ' <span aria-hidden="true" class="shipit-count">'
+            '  <span class="rb-icon rb-icon-datagrid-shipit"></span>'
+            '  1'
+            ' </span>'
+            '</div>')
+
+    def test_render_data_with_one_open_issue(self):
+        """Testing ShipItColumn.render_data with one open issue"""
+        review_request = self.create_review_request(publish=True)
+        review_request.issue_open_count = 1
+        review_request.shipit_count = 1
+
+        self.assertHTMLEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '<div class="issue-count-container"'
+            '     title="1 issue opened"'
+            '     aria-label="1 issue opened">'
+            ' <span aria-hidden="true" class="issue-count">'
+            '  <span class="rb-icon rb-icon-datagrid-open-issues"></span>'
+            '  1'
+            ' </span>'
+            '</div>')
+
+    def test_render_data_with_many_open_issues(self):
+        """Testing ShipItColumn.render_data with many open issues"""
+        review_request = self.create_review_request(publish=True)
+        review_request.issue_open_count = 2
+        review_request.shipit_count = 1
+
+        self.assertHTMLEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '<div class="issue-count-container"'
+            '     title="2 issues opened"'
+            '     aria-label="2 issues opened">'
+            ' <span aria-hidden="true" class="issue-count">'
+            '  <span class="rb-icon rb-icon-datagrid-open-issues"></span>'
+            '  2'
+            ' </span>'
+            '</div>')
+
+    def test_render_data_with_one_issue_pending_verification(self):
+        """Testing ShipItColumn.render_data with one issue pending verification
+        """
+        review_request = self.create_review_request(publish=True)
+        review_request.issue_verifying_count = 1
+        review_request.shipit_count = 1
+
+        self.assertHTMLEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '<div class="issue-count-container"'
+            '     title="1 issue requiring verification"'
+            '     aria-label="1 issue requiring verification">'
+            ' <span aria-hidden="true" class="issue-count">'
+            '  <span class="rb-icon rb-icon-datagrid-issue-verifying"></span>'
+            '  1'
+            ' </span>'
+            '</div>')
+
+    def test_render_data_with_many_pending_verification(self):
+        """Testing ShipItColumn.render_data with many issues pending
+        verification
+        """
+        review_request = self.create_review_request(publish=True)
+        review_request.issue_verifying_count = 2
+        review_request.shipit_count = 1
+
+        self.assertHTMLEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '<div class="issue-count-container"'
+            '     title="2 issues requiring verification"'
+            '     aria-label="2 issues requiring verification">'
+            ' <span aria-hidden="true" class="issue-count">'
+            '  <span class="rb-icon rb-icon-datagrid-issue-verifying"></span>'
+            '  2'
+            ' </span>'
+            '</div>')
+
+    def test_render_data_with_one_issue_and_one_pending_verification(self):
+        """Testing ShipItColumn.render_data with 1 open issue and pending
+        verification
+        """
+        review_request = self.create_review_request(publish=True)
+        review_request.issue_open_count = 1
+        review_request.issue_verifying_count = 1
+        review_request.shipit_count = 1
+
+        self.assertHTMLEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '<div class="issue-count-container"'
+            '     title="1 issue opened, 1 requiring verification"'
+            '     aria-label="1 issue opened, 1 requiring verification">'
+            ' <span aria-hidden="true" class="issue-count">'
+            '  <span class="rb-icon rb-icon-datagrid-open-issues"></span>'
+            '  1'
+            ' </span>'
+            ' <span aria-hidden="true" class="issue-verifying-count">'
+            '  <span class="rb-icon rb-icon-datagrid-issue-verifying"></span>'
+            '  1'
+            ' </span>'
+            '</div>')
+
+    def test_render_data_with_many_issues_and_one_pending_verification(self):
+        """Testing ShipItColumn.render_data with many open issues and 1
+        pending verification
+        """
+        review_request = self.create_review_request(publish=True)
+        review_request.issue_open_count = 5
+        review_request.issue_verifying_count = 1
+        review_request.shipit_count = 1
+
+        self.assertHTMLEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '<div class="issue-count-container"'
+            '     title="5 issues opened, 1 requiring verification"'
+            '     aria-label="5 issues opened, 1 requiring verification">'
+            ' <span aria-hidden="true" class="issue-count">'
+            '  <span class="rb-icon rb-icon-datagrid-open-issues"></span>'
+            '  5'
+            ' </span>'
+            ' <span aria-hidden="true" class="issue-verifying-count">'
+            '  <span class="rb-icon rb-icon-datagrid-issue-verifying"></span>'
+            '  1'
+            ' </span>'
+            '</div>')
+
+    def test_render_data_with_many_issues_and_many_pending_verification(self):
+        """Testing ShipItColumn.render_data with many open issues and many
+        pending verification
+        """
+        review_request = self.create_review_request(publish=True)
+        review_request.issue_open_count = 5
+        review_request.issue_verifying_count = 3
+        review_request.shipit_count = 1
+
+        self.assertHTMLEqual(
+            self.column.render_data(self.stateful_column, review_request),
+            '<div class="issue-count-container"'
+            '     title="5 issues opened, 3 requiring verification"'
+            '     aria-label="5 issues opened, 3 requiring verification">'
+            ' <span aria-hidden="true" class="issue-count">'
+            '  <span class="rb-icon rb-icon-datagrid-open-issues"></span>'
+            '  5'
+            ' </span>'
+            ' <span aria-hidden="true" class="issue-verifying-count">'
+            '  <span class="rb-icon rb-icon-datagrid-issue-verifying"></span>'
+            '  3'
+            ' </span>'
+            '</div>')
 
 
 class SummaryColumnTests(BaseColumnTestCase):
