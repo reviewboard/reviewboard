4362f5ec16941b800628cb704e374523f062e5bc
Christian Hammond <christian@beanbaginc.com>
2022-10-04 17:35:12 -0700

    Release Review Board 5.0.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -10,7 +10,7 @@ Review Board. They're largely used for packaging purposes.
 #:
 #: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #:
-VERSION = (5, 0, 0, 0, 'rc', 2, False)
+VERSION = (5, 0, 0, 0, 'final', 0, True)
 
 
 def get_version_string():
