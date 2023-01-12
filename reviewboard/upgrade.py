df54664dd078e8970f34a1f77e31a146dbb9ed5a
Christian Hammond <christian@beanbaginc.com>
2023-01-11 22:58:55 -0800

    Avoid querying IntegrationConfig from older databases during upgrade.
    The database upgrade code looks for SCMTool-related state to migrate
    over to SCMTool IDs, and one of the places where it looks is
    `IntegrationConfig`.
    
    The problem is, this assumes `IntegrationConfig` was installed in the
    database prior to upgrade. That breaks upgrades from Review Board 2.5
    and older.
    
    This change fixes up that assumption by first querying a list of all
    known tables in the database as the first pre-upgrade step. Any code
    that's sensitive to the presence of tables (ones that haven't been
    around since 1.0) now checks for presence before doing anything that
    would touch the database.
    
    Existing code that used `DatabaseError` to determine if tables existed
    have been updated to use this mechanism. This leaves `DatabaseError`
    free to bubble up, so we can catch other types of errors.
    
    Testing Done:
    Tested upgrading from 2.5, 3.0, 4.0, and 5.0 databases using both
    `./reviewboard/manage.py` and `rb-site upgrade`.
    
    Tested installing a new database.
    
    Reviewed at https://reviews.reviewboard.org/r/12772/

--- reviewboard/upgrade.py
+++ reviewboard/upgrade.py
@@ -13,12 +13,13 @@ Version Added:
 
 from __future__ import annotations
 
-from typing import Dict, List, Set, TYPE_CHECKING, Tuple
+from typing import Dict, List, Set, TYPE_CHECKING, Tuple, Type
 
 from django.db import DatabaseError
 from typing_extensions import TypedDict
 
 if TYPE_CHECKING:
+    from django.db.models import Model
     from reviewboard.cmdline.utils.console import Console
 
 
@@ -40,6 +41,14 @@ class UpgradeState(TypedDict, total=False):
     Version Added:
         5.0.2
     """
+
+    #: A set of all table names currently in the database.
+    #:
+    #: This is always available to all pre/post-upgrade steps.
+    #:
+    #: Type:
+    #:     set
+    tables: Set[str]
 
     #: Whether SCMTool IDs need to be migrated.
     #:
@@ -74,6 +83,60 @@ class UpgradeState(TypedDict, total=False):
     conditions_for_scmtool_migration: Set[int]
 
 
+def _had_model(
+    upgrade_state: UpgradeState,
+    model_cls: Type[Model],
+) -> bool:
+    """Return whether a model was installed in the pre-upgrade database.
+
+    Version Added:
+        5.0.2
+
+    Args:
+        upgrade_state (dict):
+            Upgrade state that can be used by pre-upgrade/post-upgrade steps.
+
+        console (reviewboard.cmdline.utils.console.Console, unused):
+            The console output wrapper.
+
+    Returns:
+        bool:
+        ``True`` if the model was installed pre-upgrade. ``False`` if it was
+        not.
+    """
+    table_names = upgrade_state.get('tables', set())
+
+    # As of Jan 6, 2023, pyright fails to find attributes on Type[Model].
+    # In this case, Model._meta. To avoid issues, ignore the type.
+    return model_cls._meta.db_table in table_names  # type: ignore
+
+
+def pre_upgrade_gather_db_state(
+    upgrade_state: UpgradeState,
+    console: Console,
+) -> None:
+    """Store a list of all table names in the database.
+
+    These are used by subsequent steps to better calculate which pieces of
+    the database need to be upgraded or queried.
+
+    Version Added:
+        5.0.2
+
+    Args:
+        upgrade_state (dict):
+            Upgrade state that can be used by pre-upgrade/post-upgrade steps.
+
+        console (reviewboard.cmdline.utils.console.Console, unused):
+            The console output wrapper.
+    """
+    from django.db import connection
+
+    with connection.cursor() as cursor:
+        upgrade_state['tables'] = \
+            set(connection.introspection.table_names(cursor))
+
+
 def pre_upgrade_reset_oauth2_provider(
     upgrade_state: UpgradeState,
     console: Console,
@@ -96,18 +159,31 @@ def pre_upgrade_reset_oauth2_provider(
 
         console (reviewboard.cmdline.utils.console.Console, unused):
             The console output wrapper.
+
+    Raises:
+        django.db.DatabaseError:
+            There was an error querying the database.
     """
     from django.db import connection
     from django_evolution.consts import UpgradeMethod
     from django_evolution.models import Evolution, Version
     from django_evolution.utils.migrations import unrecord_applied_migrations
 
+    if not _had_model(upgrade_state, Version):
+        # There's no table. This is a brand-new installation. We have nothing
+        # to do at this stage.
+        return
+
     try:
         version = Version.objects.current_version()
-    except (DatabaseError, Version.DoesNotExist):
-        # There's no recorded versions, or no table. This is a brand-new
-        # installation. We have nothing to do at this stage.
+    except Version.DoesNotExist:
+        # There's no recorded versions. This is a brand-new installation.
+        # We have nothing to do at this stage.
         return
+    except DatabaseError as e:
+        console.error('Unexpected error trying to query the "%s" table: %s'
+                      % (Version._meta.db_table, e))
+        raise
 
     project_sig = version.signature
     app_sig = project_sig.get_app_sig('oauth2_provider')
@@ -197,19 +273,29 @@ def pre_upgrade_store_scmtool_data(
 
         console (reviewboard.cmdline.utils.console.Console):
             The console output wrapper.
+
+    Raises:
+        django.db.DatabaseError:
+            There was an error querying the database.
     """
     from django_evolution.models import Evolution
     from reviewboard.scmtools.models import Repository
+
+    if not _had_model(upgrade_state, Evolution):
+        # The evolution tables don't yet exist. This is a brand-new
+        # installation. We have nothing to do at this stage.
+        return
 
     try:
         evolution = Evolution.objects.filter(
             app_label='scmtools', label='repository_scmtool_id')
 
         has_evolution = evolution.exists()
-    except DatabaseError:
-        # The evolution tables don't yet exist. This is a brand-new
-        # installation. We have nothing to do at this stage.
-        return
+    except DatabaseError as e:
+        console.error('Unexpected error trying to query for the '
+                      '"scmtools.repository_scmtool_id" evolution: %s'
+                      % e)
+        raise
 
     # We'll need to store state for an upgrade if we haven't yet added the
     # Repository.scmtool_id field or if we have any repositories remaining
@@ -275,7 +361,7 @@ def pre_upgrade_store_scmtool_data(
                 for repository in tool.repositories.all()
             ]
 
-        errors = []
+        errors: List[List[str]] = []
 
         if missing_tools:
             errors.append([
@@ -343,18 +429,20 @@ def post_upgrade_apply_scmtool_data(
         UpgradeStateError:
             The upgrade state had missing or bad data.
     """
-    if upgrade_state.get('needs_scmtool_id_migration'):
-        from reviewboard.scmtools.models import Repository
+    if not upgrade_state.get('needs_scmtool_id_migration'):
+        return
 
-        try:
-            scmtool_id_data = upgrade_state['scmtool_id_data']
-        except KeyError:
-            raise UpgradeStateError(
-                '`scmtool_id_data` is missing in the pre-upgrade state!')
+    from reviewboard.scmtools.models import Repository
 
-        for scmtool_id, repository_ids in scmtool_id_data.items():
-            repositories = Repository.objects.filter(pk__in=repository_ids)
-            repositories.update(scmtool_id=scmtool_id)
+    try:
+        scmtool_id_data = upgrade_state['scmtool_id_data']
+    except KeyError:
+        raise UpgradeStateError(
+            '`scmtool_id_data` is missing in the pre-upgrade state!')
+
+    for scmtool_id, repository_ids in scmtool_id_data.items():
+        repositories = Repository.objects.filter(pk__in=repository_ids)
+        repositories.update(scmtool_id=scmtool_id)
 
 
 def pre_upgrade_store_condition_tool_info(
@@ -379,37 +467,43 @@ def pre_upgrade_store_condition_tool_info(
         console (reviewboard.cmdline.utils.console.Console, unused):
             The console output wrapper.
     """
-    if upgrade_state.get('needs_scmtool_id_migration'):
-        from reviewboard.integrations.models import IntegrationConfig
-        from reviewboard.scmtools.models import Tool
+    from reviewboard.integrations.models import IntegrationConfig
 
-        tool_pks: Set[int] = set()
-        affected_configs: Set[int] = set()
+    if (not upgrade_state.get('needs_scmtool_id_migration') or
+        not _had_model(upgrade_state, IntegrationConfig)):
+        # This doesn't need SCMTool ID migration, so we can skip this step.
+        return
 
-        configs = IntegrationConfig.objects.only('pk', 'settings')
+    from reviewboard.scmtools.models import Tool
 
-        for config in configs:
-            # We need to directly work with the underlying settings dictionary.
-            # If we go through config.get(), and the setting doesn't exist, it
-            # will result in looking up the integration instance, which may not
-            # be available at this point (due to being part of an extension) or
-            # may not even be installed, and that will crash the upgrade
-            # process.
-            conditions = config.settings.get('conditions')
+    # This was a Review Board 3.0+ installation.
+    tool_pks: Set[int] = set()
+    affected_configs: Set[int] = set()
 
-            if conditions:
-                for condition in conditions['conditions']:
-                    if condition['choice'] == 'repository_type':
-                        tool_pks.update(condition['value'])
-                        affected_configs.add(config.pk)
+    configs = IntegrationConfig.objects.only('pk', 'settings')
 
-        tools = Tool.objects.filter(pk__in=tool_pks).only('pk', 'class_name')
+    for config in configs:
+        # We need to directly work with the underlying settings dictionary.
+        # If we go through config.get(), and the setting doesn't exist, it
+        # will result in looking up the integration instance, which may not
+        # be available at this point (due to being part of an extension) or
+        # may not even be installed, and that will crash the upgrade
+        # process.
+        conditions = config.settings.get('conditions')
 
-        upgrade_state['tool_pk_to_scmtool_id'] = {
-            tool.pk: tool.scmtool_id
-            for tool in tools
-        }
-        upgrade_state['conditions_for_scmtool_migration'] = affected_configs
+        if conditions:
+            for condition in conditions['conditions']:
+                if condition['choice'] == 'repository_type':
+                    tool_pks.update(condition['value'])
+                    affected_configs.add(config.pk)
+
+    tools = Tool.objects.filter(pk__in=tool_pks).only('pk', 'class_name')
+
+    upgrade_state['tool_pk_to_scmtool_id'] = {
+        tool.pk: tool.scmtool_id
+        for tool in tools
+    }
+    upgrade_state['conditions_for_scmtool_migration'] = affected_configs
 
 
 def post_upgrade_apply_condition_tool_info(
@@ -436,42 +530,46 @@ def post_upgrade_apply_condition_tool_info(
         UpgradeStateError:
             The upgrade state had missing or bad data.
     """
-    if upgrade_state.get('needs_scmtool_id_migration'):
-        from reviewboard.integrations.models import IntegrationConfig
+    from reviewboard.integrations.models import IntegrationConfig
 
-        try:
-            tool_pk_to_scmtool_id = upgrade_state['tool_pk_to_scmtool_id']
-            config_pks = upgrade_state['conditions_for_scmtool_migration']
-        except KeyError as e:
-            raise UpgradeStateError(
-                '`%s` is missing in the pre-upgrade state!'
-                % e)
+    if (not upgrade_state.get('needs_scmtool_id_migration') or
+        not _had_model(upgrade_state, IntegrationConfig)):
+        # This doesn't need SCMTool ID migration, so we can skip this step.
+        return
 
-        configs = (
-            IntegrationConfig.objects
-            .filter(pk__in=config_pks)
-            .only('pk', 'settings')
-        )
+    try:
+        tool_pk_to_scmtool_id = upgrade_state['tool_pk_to_scmtool_id']
+        config_pks = upgrade_state['conditions_for_scmtool_migration']
+    except KeyError as e:
+        raise UpgradeStateError(
+            '`%s` is missing in the pre-upgrade state!'
+            % e)
 
-        for config in configs:
-            # See the note above about how we access the settings for
-            # configurations.
-            #
-            # It should be safe to use .get(), since we've already determined
-            # the correct integrations above, but as this is the upgrade
-            # process we'll want to be extra careful.
-            conditions = config.settings.get('conditions')
+    configs = (
+        IntegrationConfig.objects
+        .filter(pk__in=config_pks)
+        .only('pk', 'settings')
+    )
 
-            if conditions:
-                for condition in conditions['conditions']:
-                    if condition['choice'] == 'repository_type':
-                        condition['value'] = [
-                            tool_pk_to_scmtool_id[pk]
-                            for pk in condition['value']
-                        ]
+    for config in configs:
+        # See the note above about how we access the settings for
+        # configurations.
+        #
+        # It should be safe to use .get(), since we've already determined
+        # the correct integrations above, but as this is the upgrade
+        # process we'll want to be extra careful.
+        conditions = config.settings.get('conditions')
 
-                config.set('conditions', conditions)
-                config.save(update_fields=('settings',))
+        if conditions:
+            for condition in conditions['conditions']:
+                if condition['choice'] == 'repository_type':
+                    condition['value'] = [
+                        tool_pk_to_scmtool_id[pk]
+                        for pk in condition['value']
+                    ]
+
+            config.set('conditions', conditions)
+            config.save(update_fields=('settings',))
 
 
 def run_pre_upgrade_tasks(
@@ -491,7 +589,12 @@ def run_pre_upgrade_tasks(
 
         console (reviewboard.cmdline.utils.console.Console):
             The console output wrapper.
+
+    Raises:
+        django.db.DatabaseError:
+            There was an error querying the database.
     """
+    pre_upgrade_gather_db_state(upgrade_state, console)
     pre_upgrade_reset_oauth2_provider(upgrade_state, console)
     pre_upgrade_store_scmtool_data(upgrade_state, console)
     pre_upgrade_store_condition_tool_info(upgrade_state, console)
@@ -516,6 +619,9 @@ def run_post_upgrade_tasks(
     Raises:
         UpgradeStateError:
             The upgrade state had missing or bad data.
+
+        django.db.DatabaseError:
+            There was an error querying the database.
     """
     post_upgrade_reset_oauth2_provider(upgrade_state, console)
     post_upgrade_apply_scmtool_data(upgrade_state, console)
