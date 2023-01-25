1b71ca7e0038dfc4dbc05e87166978fde12a4a49
Melroy van den Berg <melroy@melroy.org>
2023-01-25 23:45:25 +0100

    Use LOGGING_LEVEL in Docker
    In docker LOGGING_LEVEL was not used, and always set to DEBUG (hard-coded). 
    
    Even in production! So I was unable to manage this, causing unnecessary calls to syslog as well (if you log docker to syslog).

--- contrib/docker/scripts/serve.sh
+++ contrib/docker/scripts/serve.sh
@@ -3,7 +3,7 @@
 gunicorn \
     --bind=0.0.0.0:8080 \
     --worker-tmp-dir=/dev/shm \
-    --log-level=debug \
+    --log-level=${LOGGING_LEVEL} \
     --log-file=- \
     --timeout=${REQUEST_TIMEOUT} \
     --workers=${NUM_WORKERS} \
