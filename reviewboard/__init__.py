84c0539055a51cc9e2e59f5673a8dd69787f5433
Christian Hammond <christian@beanbaginc.com>
2023-06-20 15:27:11 -0700

    Bump to 5.0.6 dev.

--- reviewboard/__init__.py
+++ reviewboard/__init__.py
@@ -10,7 +10,7 @@ Review Board. They're largely used for packaging purposes.
 #:
 #: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #:
-VERSION = (5, 0, 5, 0, 'final', 0, True)
+VERSION = (5, 0, 6, 0, 'alpha', 0, False)
 
 
 def get_version_string():
