fb00f86ca51c65183b33d6f28c25efdaa32c3035
Christian Hammond <christian@beanbaginc.com>
2023-10-12 13:11:32 -0700

    Add DiffCommit.commit_message_body for getting the body of a commit.
    This property returns the commit message's body, which is the part after
    the summary, if it's available. It will be used as part of a fix for the
    diff commit list.
    
    Unit tests are added. The old test suite had setup code that was very
    specific to the tests that were initially written, so some cleanup was
    done to move that setup code into a dedicated, optional function.
    
    Testing Done:
    Unit tests pass.
    
    Made use of this function in some upcoming code.
    
    Reviewed at https://reviews.reviewboard.org/r/13332/

--- reviewboard/diffviewer/tests/test_diffcommit.py
+++ reviewboard/diffviewer/tests/test_diffcommit.py
@@ -1,6 +1,10 @@
 """Tests for reviewboard.diffviewer.models.diffcommit."""
 
-from django.utils.timezone import now
+from __future__ import annotations
+
+from typing import Sequence, Tuple
+
+from django.utils import timezone
 
 from reviewboard.diffviewer.models import DiffCommit, DiffSet
 from reviewboard.testing import TestCase
@@ -11,41 +15,11 @@ class DiffCommitTests(TestCase):
 
     fixtures = ['test_scmtools']
 
-    def setUp(self):
-        super(DiffCommitTests, self).setUp()
-
-        repository = self.create_repository()
-        self.diffset = DiffSet.objects.create_empty(repository=repository)
-
-        self.commits = [
-            DiffCommit.objects.create(
-                diffset=self.diffset,
-                filename='diff',
-                author_name='Author Name',
-                author_email='author@example.com',
-                commit_message='Message',
-                author_date=now(),
-                commit_id='a' * 40,
-                parent_id='0' * 40),
-            DiffCommit.objects.create(
-                diffset=self.diffset,
-                filename='diff',
-                author_name='Author Name',
-                author_email='author@example.com',
-                commit_message='Message',
-                author_date=now(),
-                commit_id='b' * 40,
-                parent_id='a' * 40),
-        ]
-
-        self.create_filediff(self.diffset, commit=self.commits[0],
-                             diff=self.DEFAULT_GIT_FILEDIFF_DATA_DIFF)
-        self.create_filediff(self.diffset, commit=self.commits[1],
-                             diff=self.DEFAULT_GIT_FILEDIFF_DATA_DIFF)
-
     def test_get_line_counts_raw(self):
         """Testing DiffCommit.get_line_counts() returns correct raw counts"""
-        self.assertEqual(self.diffset.get_total_line_counts(), {
+        diffset, commits = self._populate_commits()
+
+        self.assertEqual(diffset.get_total_line_counts(), {
             'raw_insert_count': 2,
             'raw_delete_count': 2,
             'insert_count': 2,
@@ -55,7 +29,7 @@ class DiffCommitTests(TestCase):
             'total_line_count': None,
         })
 
-        self.assertEqual(self.commits[0].get_total_line_counts(), {
+        self.assertEqual(commits[0].get_total_line_counts(), {
             'raw_insert_count': 1,
             'raw_delete_count': 1,
             'insert_count': 1,
@@ -64,7 +38,7 @@ class DiffCommitTests(TestCase):
             'equal_count': None,
             'total_line_count': None,
         })
-        self.assertEqual(self.commits[1].get_total_line_counts(), {
+        self.assertEqual(commits[1].get_total_line_counts(), {
             'raw_insert_count': 1,
             'raw_delete_count': 1,
             'insert_count': 1,
@@ -78,7 +52,9 @@ class DiffCommitTests(TestCase):
         """Testing DiffCommit.get_total_line_counts() returns correct computed
         line counts
         """
-        for filediff in self.diffset.files.all():
+        diffset, commits = self._populate_commits()
+
+        for filediff in diffset.files.all():
             filediff.extra_data.update({
                 'insert_count': 0,
                 'delete_count': 0,
@@ -88,7 +64,7 @@ class DiffCommitTests(TestCase):
             })
             filediff.save(update_fields=('extra_data',))
 
-        self.assertEqual(self.diffset.get_total_line_counts(), {
+        self.assertEqual(diffset.get_total_line_counts(), {
             'raw_insert_count': 2,
             'raw_delete_count': 2,
             'insert_count': 0,
@@ -98,7 +74,7 @@ class DiffCommitTests(TestCase):
             'total_line_count': 4,
         })
 
-        self.assertEqual(self.commits[0].get_total_line_counts(), {
+        self.assertEqual(commits[0].get_total_line_counts(), {
             'raw_insert_count': 1,
             'raw_delete_count': 1,
             'insert_count': 0,
@@ -108,7 +84,7 @@ class DiffCommitTests(TestCase):
             'total_line_count': 2,
         })
 
-        self.assertEqual(self.commits[1].get_total_line_counts(), {
+        self.assertEqual(commits[1].get_total_line_counts(), {
             'raw_insert_count': 1,
             'raw_delete_count': 1,
             'insert_count': 0,
@@ -120,5 +96,102 @@ class DiffCommitTests(TestCase):
 
     def test_ordering(self):
         """Testing DiffCommits are returned in the correct order"""
-        commits = list(DiffCommit.objects.all())
-        self.assertEqual(commits, self.commits)
+        commits = self._populate_commits()[1]
+
+        self.assertEqual(list(DiffCommit.objects.all()),
+                         commits)
+
+    def test_commit_message_body(self) -> None:
+        """Testing DiffCommit.commit_message_body"""
+        diffcommit = DiffCommit(commit_message=(
+            'This is a summary line.\n'
+            '\n'
+            'This is a multi-line\n'
+            'description.\n'
+        ))
+
+        self.assertEqual(
+            diffcommit.commit_message_body,
+            (
+                'This is a multi-line\n'
+                'description.'
+            ))
+
+    def test_commit_message_body_with_excess_newlines(self) -> None:
+        """Testing DiffCommit.commit_message_body with newlines before and
+        after body
+        """
+        diffcommit = DiffCommit(commit_message=(
+            'This is a summary line.\n'
+            '\n'
+            '\n'
+            'This is a multi-line\n'
+            'description.\n'
+            '\n'
+        ))
+
+        self.assertEqual(
+            diffcommit.commit_message_body,
+            (
+                'This is a multi-line\n'
+                'description.'
+            ))
+
+    def test_commit_message_body_with_only_summary(self) -> None:
+        """Testing DiffCommit.commit_message_body with only summary"""
+        diffcommit = DiffCommit(commit_message=(
+            'This is a summary line.\n'
+        ))
+
+        self.assertIsNone(diffcommit.commit_message_body)
+
+    def _populate_commits(self) -> Tuple[DiffSet, Sequence[DiffCommit]]:
+        """Populate and return commits used for testing.
+
+        Returns:
+            tuple:
+            A 2-tuple of:
+
+            Tuple:
+                0 (reviewboard.diffviewer.models.diffset.DiffSet):
+                    The parent diffset for the commits.
+
+                1 (list):
+                    The list of commits.
+        """
+        repository = self.create_repository()
+        diffset = DiffSet.objects.create_empty(repository=repository)
+
+        now = timezone.now()
+
+        commits = [
+            DiffCommit.objects.create(
+                diffset=diffset,
+                filename='diff',
+                author_name='Author Name',
+                author_email='author@example.com',
+                commit_message='Message',
+                author_date=now,
+                commit_id='a' * 40,
+                parent_id='0' * 40),
+            DiffCommit.objects.create(
+                diffset=diffset,
+                filename='diff',
+                author_name='Author Name',
+                author_email='author@example.com',
+                commit_message='Message',
+                author_date=now,
+                commit_id='b' * 40,
+                parent_id='a' * 40),
+        ]
+
+        diff_data = self.DEFAULT_GIT_FILEDIFF_DATA_DIFF
+
+        self.create_filediff(diffset,
+                             commit=commits[0],
+                             diff=diff_data)
+        self.create_filediff(diffset,
+                             commit=commits[1],
+                             diff=diff_data)
+
+        return diffset, commits
