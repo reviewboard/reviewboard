e69a51d30dbdb3bbc7da96e7f5901b57ff4ddb26
Christian Hammond <christian@beanbaginc.com>
2023-10-27 18:09:41 -0700

    Expand the Local Site query checks in the Groups datagrid tests.
    This splits most of the unit tests in the Groups datagrid test suite
    into a global site test, a Local Site test, and a global site + Local
    Sites in the database test.
    
    This gives us thorough query coverage, which unveils a number of
    sub-optimal queries that will need to be addressed in upcoming changes.
    
    The queries are intentionally verbose right now, with no query-building
    being shared across tests, and with the query-by-query breakdowns
    removed. This is intentional, as upcoming work is expected to heavily
    impact these queries.
    
    Testing Done:
    Unit tests pass.
    
    Reviewed at https://reviews.reviewboard.org/r/13362/

--- reviewboard/datagrids/tests/test_groups_list.py
+++ reviewboard/datagrids/tests/test_groups_list.py
@@ -6,200 +6,38 @@ Version Added:
 
 from __future__ import annotations
 
+from typing import Optional
+
+from django.contrib.auth.models import Permission, User
 from django.db.models import Count, Q
 from djblets.testing.decorators import add_fixtures
 
+from reviewboard.accounts.models import LocalSiteProfile, Profile
 from reviewboard.datagrids.tests.base import BaseViewTestCase
 from reviewboard.reviews.models import Group, ReviewRequest
+from reviewboard.site.models import LocalSite
 
 
 class GroupListViewTests(BaseViewTestCase):
     """Unit tests for the group_list view."""
 
+    datagrid_url = '/groups/'
+
     @add_fixtures(['test_users'])
     def test_with_access(self):
         """Testing group_list view"""
-        for i in range(10):
-            # We're using %x instead of %d so that "a" will come after "9".
-            # Otherwise "10" comes between "1" and "2".
-            self.create_review_group(name='group-%x' % (i + 1))
+        self._test_with_access()
 
-        self._prefetch_cached()
+    @add_fixtures(['test_site', 'test_users'])
+    def test_with_access_with_local_site(self):
+        """Testing group_list view on a Local Site"""
+        self._test_with_access(with_local_site=True,
+                               local_sites_in_db=True)
 
-        # 13 queries:
-        #
-        # 1. Fetch total number of results
-        # 2. Fetch IDs of page of results
-        # 3. Fetch data for page of results
-        # 4. Fetch review request count on group-1
-        # 5. Fetch review request count on group-2
-        # 6. Fetch review request count on group-3
-        # 7. Fetch review request count on group-4
-        # 8. Fetch review request count on group-5
-        # 9. Fetch review request count on group-6
-        # 10. Fetch review request count on group-7
-        # 11. Fetch review request count on group-8
-        # 12. Fetch review request count on group-9
-        # 13. Fetch review request count on group-a
-        queries = [
-            {
-                'model': Group,
-                'annotations': {'__count': Count('*')},
-            },
-            {
-                'model': Group,
-                'values_select': ('pk',),
-                'where': (Q(invite_only=False) &
-                          Q(visible=True) &
-                          Q(local_site=None)),
-                'order_by': ('name',),
-                'distinct': True,
-                'limit': 10,
-            },
-            {
-                'model': Group,
-                'where': Q(pk__in=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-            },
-            {
-                'model': ReviewRequest,
-                'num_joins': 1,
-                'tables': {
-                    'reviews_reviewrequest',
-                    'reviews_reviewrequest_target_groups',
-                },
-                'annotations': {'__count': Count('*')},
-                'where': (Q(target_groups__id=1) &
-                          Q(public=True) &
-                          Q(status='P')),
-            },
-            {
-                'model': ReviewRequest,
-                'num_joins': 1,
-                'tables': {
-                    'reviews_reviewrequest',
-                    'reviews_reviewrequest_target_groups',
-                },
-                'annotations': {'__count': Count('*')},
-                'where': (Q(target_groups__id=2) &
-                          Q(public=True) &
-                          Q(status='P')),
-            },
-            {
-                'model': ReviewRequest,
-                'num_joins': 1,
-                'tables': {
-                    'reviews_reviewrequest',
-                    'reviews_reviewrequest_target_groups',
-                },
-                'annotations': {'__count': Count('*')},
-                'where': (Q(target_groups__id=3) &
-                          Q(public=True) &
-                          Q(status='P')),
-            },
-            {
-                'model': ReviewRequest,
-                'num_joins': 1,
-                'tables': {
-                    'reviews_reviewrequest',
-                    'reviews_reviewrequest_target_groups',
-                },
-                'annotations': {'__count': Count('*')},
-                'where': (Q(target_groups__id=4) &
-                          Q(public=True) &
-                          Q(status='P')),
-            },
-            {
-                'model': ReviewRequest,
-                'num_joins': 1,
-                'tables': {
-                    'reviews_reviewrequest',
-                    'reviews_reviewrequest_target_groups',
-                },
-                'annotations': {'__count': Count('*')},
-                'where': (Q(target_groups__id=5) &
-                          Q(public=True) &
-                          Q(status='P')),
-            },
-            {
-                'model': ReviewRequest,
-                'num_joins': 1,
-                'tables': {
-                    'reviews_reviewrequest',
-                    'reviews_reviewrequest_target_groups',
-                },
-                'annotations': {'__count': Count('*')},
-                'where': (Q(target_groups__id=6) &
-                          Q(public=True) &
-                          Q(status='P')),
-            },
-            {
-                'model': ReviewRequest,
-                'num_joins': 1,
-                'tables': {
-                    'reviews_reviewrequest',
-                    'reviews_reviewrequest_target_groups',
-                },
-                'annotations': {'__count': Count('*')},
-                'where': (Q(target_groups__id=7) &
-                          Q(public=True) &
-                          Q(status='P')),
-            },
-            {
-                'model': ReviewRequest,
-                'num_joins': 1,
-                'tables': {
-                    'reviews_reviewrequest',
-                    'reviews_reviewrequest_target_groups',
-                },
-                'annotations': {'__count': Count('*')},
-                'where': (Q(target_groups__id=8) &
-                          Q(public=True) &
-                          Q(status='P')),
-            },
-            {
-                'model': ReviewRequest,
-                'num_joins': 1,
-                'tables': {
-                    'reviews_reviewrequest',
-                    'reviews_reviewrequest_target_groups',
-                },
-                'annotations': {'__count': Count('*')},
-                'where': (Q(target_groups__id=9) &
-                          Q(public=True) &
-                          Q(status='P')),
-            },
-            {
-                'model': ReviewRequest,
-                'num_joins': 1,
-                'tables': {
-                    'reviews_reviewrequest',
-                    'reviews_reviewrequest_target_groups',
-                },
-                'annotations': {'__count': Count('*')},
-                'where': (Q(target_groups__id=10) &
-                          Q(public=True) &
-                          Q(status='P')),
-            },
-        ]
-
-        with self.assertQueries(queries):
-            response = self.client.get('/groups/')
-
-        self.assertEqual(response.status_code, 200)
-
-        datagrid = self._get_context_var(response, 'datagrid')
-        self.assertTrue(datagrid)
-        self.assertEqual(len(datagrid.rows), 10)
-        self.assertEqual(datagrid.rows[0]['object'].name, 'group-1')
-        self.assertEqual(datagrid.rows[1]['object'].name, 'group-2')
-        self.assertEqual(datagrid.rows[2]['object'].name, 'group-3')
-        self.assertEqual(datagrid.rows[3]['object'].name, 'group-4')
-        self.assertEqual(datagrid.rows[4]['object'].name, 'group-5')
-        self.assertEqual(datagrid.rows[5]['object'].name, 'group-6')
-        self.assertEqual(datagrid.rows[6]['object'].name, 'group-7')
-        self.assertEqual(datagrid.rows[7]['object'].name, 'group-8')
-        self.assertEqual(datagrid.rows[8]['object'].name, 'group-9')
-        self.assertEqual(datagrid.rows[9]['object'].name, 'group-a')
+    @add_fixtures(['test_site', 'test_users'])
+    def test_with_access_with_local_sites_in_db(self):
+        """Testing group_list view with Local Sites in the database"""
+        self._test_with_access(local_sites_in_db=True)
 
     @add_fixtures(['test_users'])
     def test_as_anonymous_and_redirect(self):
@@ -209,3 +47,224 @@ class GroupListViewTests(BaseViewTestCase):
             response = self.client.get('/groups/')
 
         self.assertEqual(response.status_code, 302)
+
+    def _test_with_access(
+        self,
+        *,
+        with_local_site: bool = False,
+        local_sites_in_db: bool = False,
+    ) -> None:
+        """Common tests for a user with access to the datagrid.
+
+        This accesses the groups datagrid as a logged-in user with access
+        to the global site or Local Site (depending on the test), checking
+        queries and datagrid row results.
+
+        Args:
+            with_local_site (bool, optional):
+                Whether to test with a Local Site for the query and objects.
+
+            local_sites_in_db (bool, optional):
+                Whether to expect Local Sites in the database.
+
+        Raises:
+            AssertionError:
+                One of the checks failed.
+        """
+        local_site: Optional[LocalSite]
+
+        if with_local_site:
+            local_site = self.get_local_site(name=self.local_site_name)
+        else:
+            local_site = None
+
+        self.client.login(username='doc', password='doc')
+        user = User.objects.get(username='doc')
+        profile = user.get_profile()
+
+        for i in range(10):
+            self.create_review_group(
+                local_site=local_site,
+                name='group-%02d' % (i + 1))
+
+        self._prefetch_cached()
+
+        queries = [
+            {
+                'model': User,
+                'where': Q(pk=user.pk),
+            },
+            {
+                'model': Profile,
+                'where': Q(user=user),
+            },
+        ]
+
+        if local_site:
+            queries += [
+                {
+                    'model': LocalSite,
+                    'tables': {
+                        'site_localsite',
+                    },
+                    'where': Q(name=local_site.name),
+                },
+                {
+                    'extra': {
+                        'a': ('1', []),
+                    },
+                    'limit': 1,
+                    'model': User,
+                    'num_joins': 1,
+                    'tables': {
+                        'auth_user',
+                        'site_localsite_users',
+                    },
+                    'where': (Q(local_site__id=local_site.pk) &
+                              Q(pk=user.pk)),
+                },
+            ]
+
+        queries += [
+            {
+                'model': Permission,
+                'num_joins': 2,
+                'tables': {
+                    'auth_permission',
+                    'auth_user_user_permissions',
+                    'django_content_type',
+                },
+                'values_select': ('content_type__app_label', 'codename'),
+                'where': Q(user__id=user.pk),
+            },
+            {
+                'model': Permission,
+                'num_joins': 4,
+                'tables': {
+                    'auth_group',
+                    'auth_group_permissions',
+                    'auth_permission',
+                    'auth_user_groups',
+                    'django_content_type',
+                },
+                'values_select': ('content_type__app_label', 'codename'),
+                'where': Q(group__user=user),
+            },
+        ]
+
+        if local_site:
+            queries += [
+                {
+                    'extra': {
+                        'a': ('1', []),
+                    },
+                    'limit': 1,
+                    'model': User,
+                    'num_joins': 1,
+                    'tables': {
+                        'site_localsite_admins',
+                        'auth_user',
+                    },
+                    'where': (Q(local_site_admins__id=local_site.pk) &
+                              Q(pk=user.pk)),
+                },
+                {
+                    'model': LocalSiteProfile,
+                    'where': (Q(local_site=local_site) &
+                              Q(profile=profile) &
+                              Q(user=user)),
+                },
+            ]
+
+        queries += [
+            {
+                'model': Profile,
+                'type': 'UPDATE',
+                'where': Q(pk=profile.pk),
+            },
+            {
+                'annotations': {'__count': Count('*')},
+                'model': Group,
+            },
+            {
+                'distinct': True,
+                'limit': 10,
+                'model': Group,
+                'num_joins': 1,
+                'order_by': ('name',),
+                'tables': {
+                    'reviews_group',
+                    'reviews_group_users',
+                },
+                'values_select': ('pk',),
+                'where': (((Q(invite_only=False) &
+                            Q(visible=True)) |
+                           Q(users=user.pk)) &
+                          Q(local_site=local_site)),
+            },
+            {
+                'model': Group,
+                'num_joins': 1,
+                'tables': {
+                    'accounts_profile_starred_groups',
+                    'reviews_group',
+                },
+                'values_select': ('pk',),
+                'where': (Q(starred_by__id=profile.pk) &
+                          Q(pk__in=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])),
+            },
+            {
+                'model': Group,
+                'where': Q(pk__in=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            },
+        ]
+
+        # NOTE: This represents a performance bug due to a bad query.
+        #       It's being tracked and will be resolved in a future
+        #       change.
+        for i in range(10):
+            if local_site:
+                queries += [
+                    {
+                        'model': LocalSite,
+                        'tables': {
+                            'site_localsite',
+                        },
+                        'where': Q(id=local_site.pk)
+                    }
+                ]
+
+            queries += [
+                {
+                    'annotations': {'__count': Count('*')},
+                    'model': ReviewRequest,
+                    'num_joins': 1,
+                    'tables': {
+                        'reviews_reviewrequest',
+                        'reviews_reviewrequest_target_groups',
+                    },
+                    'where': (Q(target_groups__id=i + 1) &
+                              Q(public=True) &
+                              Q(status='P')),
+                },
+            ]
+
+        with self.assertQueries(queries):
+            response = self.client.get(
+                self.get_datagrid_url(local_site=local_site))
+
+        self.assertEqual(response.status_code, 200)
+
+        datagrid = self._get_context_var(response, 'datagrid')
+        assert datagrid is not None
+        self.assertEqual(len(datagrid.rows), 10)
+        self.assertEqual(datagrid.rows[0]['object'].name, 'group-01')
+        self.assertEqual(datagrid.rows[1]['object'].name, 'group-02')
+        self.assertEqual(datagrid.rows[2]['object'].name, 'group-03')
+        self.assertEqual(datagrid.rows[3]['object'].name, 'group-04')
+        self.assertEqual(datagrid.rows[4]['object'].name, 'group-05')
+        self.assertEqual(datagrid.rows[5]['object'].name, 'group-06')
+        self.assertEqual(datagrid.rows[6]['object'].name, 'group-07')
+        self.assertEqual(datagrid.rows[7]['object'].name, 'group-08')
+        self.assertEqual(datagrid.rows[8]['object'].name, 'group-09')
+        self.assertEqual(datagrid.rows[9]['object'].name, 'group-10')
