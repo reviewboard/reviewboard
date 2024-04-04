5f816851a17bd38195de2d9c394175e87469525c
David Trowbridge <trowbrds@gmail.com>
2024-04-04 17:25:43 -0600

    Port comments hint view to Ink.Alert
    This change ports the comments hint shown in the diffviewer when a draft
    review has comments on other revisions over to the new Ink.Alert
    component.
    
    Testing Done:
    Viewed the comments hint and saw that everything worked correctly.
    
    Reviewed at https://reviews.reviewboard.org/r/13700/

--- reviewboard/static/rb/js/reviews/views/diffCommentsHintView.ts
+++ reviewboard/static/rb/js/reviews/views/diffCommentsHintView.ts
@@ -2,6 +2,7 @@
  * A view which gives the user hints about comments in other revisions.
  */
 
+import { paint, renderInto } from '@beanbag/ink';
 import {
     BaseView,
     type EventsHash,
@@ -20,111 +21,132 @@ export class DiffCommentsHintView extends BaseView<DiffCommentsHint> {
         'change': 'render',
     };
 
-    static template = _.template(dedent`
-        <div class="box-container">
-         <div class="box important">
-          <div class="box-inner comments-hint">
-           <h1><%- unpublishedCommentsHeader %></h1>
-           <p><%- unpublishedCommentsText %></p>
-           <ul>
-           </ul>
-          </div>
-         </div>
-        </div>
-    `);
-
     /**
      * Render the view.
      */
     onRender() {
         const model = this.model;
 
-        if (model.get('hasOtherComments')) {
-            this.$el.html(DiffCommentsHintView.template({
-                unpublishedCommentsHeader: _`
-                    You have unpublished comments on other revisions.
-                `,
-                unpublishedCommentsText: _`
-                    Your review consists of comments on the following
-                    revisions:
-                `,
-            }));
+        if (!model.get('hasOtherComments')) {
+            this.$el.empty();
 
-            const $ul = this.$('ul');
+            return;
+        }
 
-            model.get('diffsetsWithComments').forEach(diffset => {
-                const $li = $('<li>')
-                    .addClass('revision')
-                    .text(_`Revision ${diffset.revision}`)
-                    .appendTo($ul);
+        const headerText = _`
+            You have unpublished comments on other revisions.
+        `;
+        const bodyText = _`
+            Your review consists of comments on the following
+            revisions:
+        `;
 
-                if (diffset.isCurrent) {
-                    $li.addClass('current');
-                } else {
-                    $li.on('click', () => {
+        renderInto(
+            this.el,
+            paint`
+                <Ink.Alert type="warning">
+                 <Ink.Alert.Heading>
+                  ${headerText}
+                 </Ink.Alert.Heading>
+                 <Ink.Alert.Content>
+                  <p>${bodyText}</p>
+                  <ul></ul>
+                 </Ink.Alert.Content>
+                </Ink.Alert>
+            `,
+            {empty: true});
+
+        const $ul = this.$('ul');
+
+        model.get('diffsetsWithComments').forEach(diffset => {
+            const $li = $('<li>').appendTo($ul);
+            const text = _`Revision ${diffset.revision}`;
+
+            if (diffset.isCurrent) {
+                $li
+                    .text(text)
+                    .addClass('-is-current');
+            } else {
+                $('<a href="#">')
+                    .text(text)
+                    .appendTo($li)
+                    .on('click', (e: Event) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+
                         this.trigger('revisionSelected',
                                      [0, diffset.revision]);
                     });
-                }
-            });
+            }
+        });
 
-            model.get('commitsWithComments').forEach(commit => {
-                const $li = $('<li>')
-                    .addClass('commit')
-                    .appendTo($ul);
+        model.get('commitsWithComments').forEach(commit => {
+            const $li = $('<li>').appendTo($ul);
+            let text: string;
 
-                if (commit.baseCommitID === null ||
-                    commit.baseCommitID === commit.tipCommitID) {
-                    $li.text(_`
-                        Revision ${commit.revision},
-                        commit ${commit.tipCommitID.substring(0, 8)}
-                    `);
-                } else if (commit.tipCommitID === null) {
-                    $li.text(_`
-                        Revision ${commit.revision},
-                        commit ${commit.baseCommitID.substring(0, 8)}
-                    `);
-                } else {
-                    $li.text(_`
-                        Revision ${commit.revision},
-                        commits ${commit.baseCommitID.substring(0, 8)} -
-                        ${commit.tipCommitID.substring(0, 8)}
-                    `);
-                }
+            if (commit.baseCommitID === null ||
+                commit.baseCommitID === commit.tipCommitID) {
+                text = _`
+                    Revision ${commit.revision},
+                    commit ${commit.tipCommitID.substring(0, 8)}
+                `;
+            } else if (commit.tipCommitID === null) {
+                text = _`
+                    Revision ${commit.revision},
+                    commit ${commit.baseCommitID.substring(0, 8)}
+                `;
+            } else {
+                text = _`
+                    Revision ${commit.revision},
+                    commits ${commit.baseCommitID.substring(0, 8)} -
+                    ${commit.tipCommitID.substring(0, 8)}
+                `;
+            }
 
-                if (commit.isCurrent) {
-                    $li.addClass('current');
-                } else {
-                    $li.on('click', () => {
+            if (commit.isCurrent) {
+                $li
+                    .text(text)
+                    .addClass('-is-current');
+            } else {
+                $('<a href="#">')
+                    .text(text)
+                    .appendTo($li)
+                    .on('click', (e: Event) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+
                         this.trigger('commitRangeSelected',
                                      commit.revision,
                                      commit.baseCommitPK,
                                      commit.tipCommitPK);
                     });
-                }
-            });
+            }
+        });
 
-            model.get('interdiffsWithComments').forEach(interdiff => {
-                const $li = $('<li>')
-                    .addClass('interdiff')
-                    .text(_`
-                        Interdiff revision ${interdiff.oldRevision} -
-                        ${interdiff.newRevision}
-                    `)
-                    .appendTo($ul);
+        model.get('interdiffsWithComments').forEach(interdiff => {
+            const $li = $('<li>').appendTo($ul);
+            const text = _`
+                Interdiff revision ${interdiff.oldRevision} -
+                ${interdiff.newRevision}
+            `;
 
-                if (interdiff.isCurrent) {
-                    $li.addClass('current');
-                } else {
-                    $li.on('click', () => {
+            if (interdiff.isCurrent) {
+                $li
+                    .text(text)
+                    .addClass('-is-current');
+            } else {
+                $('<a href="#">')
+                    .text(text)
+                    .appendTo($li)
+                    .on('click', (e: Event) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+
                         this.trigger(
                             'revisionSelected',
                             [interdiff.oldRevision, interdiff.newRevision]);
                     });
-                }
-            });
-        } else {
-            this.$el.empty();
-        }
+            }
+        });
     }
 }
