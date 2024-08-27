c4538b426aa12b5a05ea2fb2ad5676a04f89d032
David Trowbridge <trowbrds@gmail.com>
2024-08-27 15:33:26 -0600

    Do some cleanup and modernization of hosting service/API code.
    In preparation for fixing issues with the HostingServiceAccountResource
    create method, this change does some cleanup and type annotation for
    code I'm touching.
    
    Testing Done:
    Ran unit tests.
    
    Reviewed at https://reviews.reviewboard.org/r/14127/

--- reviewboard/hostingsvcs/models.py
+++ reviewboard/hostingsvcs/models.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import logging
-from typing import ClassVar
+from typing import ClassVar, TYPE_CHECKING
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -14,11 +14,18 @@ from reviewboard.hostingsvcs.errors import MissingHostingServiceError
 from reviewboard.hostingsvcs.managers import HostingServiceAccountManager
 from reviewboard.site.models import LocalSite
 
+if TYPE_CHECKING:
+    from django.contrib.auth.models import User
+
+    from reviewboard.hostingsvcs.base.hosting_service import BaseHostingService
+    from reviewboard.scmtools.certs import Certificate
+
 
 logger = logging.getLogger(__name__)
 
 
 class HostingServiceAccount(models.Model):
+    """An account for a hosting service."""
 
     service_name = models.CharField(
         max_length=128,
@@ -51,17 +58,23 @@ class HostingServiceAccount(models.Model):
     objects: ClassVar[HostingServiceAccountManager] = \
         HostingServiceAccountManager()
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return a string representation of the hosting service account.
+
+        Returns:
+            str:
+            A string representation of the object.
+        """
         if self.hosting_url:
             # Show the hosting URL, so that users can distinguish between
             # the accounts across different self-hosted servers of a given
             # type.
-            return '%s (%s)' % (self.username, self.hosting_url)
+            return f'{self.username} ({self.hosting_url})'
         else:
             return self.username
 
     @property
-    def service(self):
+    def service(self) -> BaseHostingService:
         """The hosting service associated with this account.
 
         Type:
@@ -89,7 +102,12 @@ class HostingServiceAccount(models.Model):
         return self._service
 
     @property
-    def is_authorized(self):
+    def is_authorized(self) -> bool:
+        """Whether the service is authorized.
+
+        Type:
+            bool
+        """
         service = self.service
 
         if service:
@@ -97,25 +115,50 @@ class HostingServiceAccount(models.Model):
         else:
             return False
 
-    def is_accessible_by(self, user):
-        """Returns whether or not the user has access to the account.
+    def is_accessible_by(
+        self,
+        user: User,
+    ) -> bool:
+        """Return whether or not the user has access to the account.
 
         The account is accessible by the user if the user has access to the
         local site.
+
+        Args:
+            user (django.contrib.auth.models.User):
+                The user to check.
+
+        Returns:
+            bool:
+            True if the user has access to the hosting service account.
         """
         return not self.local_site or self.local_site.is_accessible_by(user)
 
-    def is_mutable_by(self, user):
-        """Returns whether or not the user can modify or delete the account.
+    def is_mutable_by(
+        self,
+        user: User,
+    ) -> bool:
+        """Return whether or not the user can modify or delete the account.
 
         The account is mutable by the user if the user is an administrator
         with proper permissions or the account is part of a LocalSite and
         the user has permissions to modify it.
+
+        Args:
+            user (django.contrib.auth.models.User):
+                The user to check.
+
+        Returns:
+            bool:
+            True if the user can modify the hosting service account.
         """
         return user.has_perm('hostingsvcs.change_hostingserviceaccount',
                              self.local_site)
 
-    def accept_certificate(self, certificate):
+    def accept_certificate(
+        self,
+        certificate: Certificate,
+    ) -> None:
         """Accept the SSL certificate for the linked hosting URL.
 
         Args:
@@ -133,6 +176,8 @@ class HostingServiceAccount(models.Model):
         self.data['ssl_cert'] = certificate.pem_data
 
     class Meta:
+        """Metadata for the HostingServiceAccount model."""
+
         db_table = 'hostingsvcs_hostingserviceaccount'
         verbose_name = _('Hosting Service Account')
         verbose_name_plural = _('Hosting Service Accounts')
--- reviewboard/webapi/tests/test_hosting_service_account.py
+++ reviewboard/webapi/tests/test_hosting_service_account.py
@@ -1,3 +1,11 @@
+"""Unit tests for the HostingServiceAccountResource."""
+
+from __future__ import annotations
+
+from typing import Any, Optional, Sequence, TYPE_CHECKING
+
+from djblets.webapi.testing.decorators import webapi_test_template
+
 from reviewboard.hostingsvcs.models import HostingServiceAccount
 from reviewboard.webapi.resources import resources
 from reviewboard.webapi.tests.base import BaseWebAPITestCase
@@ -9,37 +17,114 @@ from reviewboard.webapi.tests.urls import (
     get_hosting_service_account_item_url,
     get_hosting_service_account_list_url)
 
-
-def _compare_item(self, item_rsp, account):
-    self.assertEqual(item_rsp['id'], account.id)
-    self.assertEqual(item_rsp['username'], account.username)
-    self.assertEqual(item_rsp['service'], account.service.hosting_service_id)
+if TYPE_CHECKING:
+    from django.contrib.auth.models import User
+    from djblets.util.typing import JSONDict
 
 
 class ResourceListTests(BaseWebAPITestCase, metaclass=BasicTestsMetaclass):
-    """Testing the HostingServiceAccountResource list APIs."""
+    """Unit tests for the HostingServiceAccountResource list APIs."""
+
     sample_api_url = 'hosting-services-accounts/'
     resource = resources.hosting_service_account
     fixtures = ['test_users']
 
     basic_post_use_admin = True
 
-    compare_item = _compare_item
+    def compare_item(
+        self,
+        item_rsp: JSONDict,
+        account: HostingServiceAccount,
+    ) -> None:
+        """Compare an API response to an item.
 
-    def setup_http_not_allowed_item_test(self, user):
+        Args:
+            item_rsp (dict):
+                The API response.
+
+            account (reviewboard.hostingsvcs.models.HostingServiceAccount):
+                The account object to compare to.
+        """
+        self.assertEqual(item_rsp['id'], account.pk)
+        self.assertEqual(item_rsp['username'], account.username)
+        self.assertEqual(item_rsp['service'],
+                         account.service.hosting_service_id)
+
+    def setup_http_not_allowed_item_test(
+        self,
+        user: User,
+    ) -> str:
+        """Set up the HTTP not allowed test for item access.
+
+        Args:
+            user (django.contrib.auth.models.User):
+                The user performing the API requests.
+
+        Returns:
+            str:
+            The URL to use for accessing the resource.
+        """
         return get_hosting_service_account_list_url()
 
-    def setup_http_not_allowed_list_test(self, user):
+    def setup_http_not_allowed_list_test(
+        self,
+        user: User,
+    ) -> str:
+        """Set up the HTTP not allowed test for list access.
+
+        Args:
+            user (django.contrib.auth.models.User):
+                The user performing the API requests.
+
+        Returns:
+            str:
+            The URL to use for accessing the resource.
+        """
         return get_hosting_service_account_list_url()
 
     #
     # HTTP GET tests
     #
 
-    def setup_basic_get_test(self, user, with_local_site, local_site_name,
-                             populate_items):
+    def setup_basic_get_test(
+        self,
+        user: User,
+        with_local_site: bool,
+        local_site_name: Optional[str],
+        populate_items: bool,
+    ) -> tuple[str, str, list[HostingServiceAccount]]:
+        """Set up a basic HTTP GET test.
+
+        Args:
+            user (django.contrib.auth.models.User):
+                The user performing the API requests.
+
+            with_local_site (bool):
+                Whether to run the tests against a Local Site.
+
+            local_site_name (str):
+                The name of the Local Site to use.
+
+            populate_items (bool):
+                Whether to populate items in the database.
+
+        Returns:
+            tuple:
+            A 3-tuple of:
+
+            Tuple:
+                0 (str):
+                    The URL to use for accessing the resource.
+
+                1 (str):
+                    The expected mimetype for responses.
+
+                2 (list):
+                    A list of accounts that are expected to be in the response.
+        """
         if populate_items:
             if with_local_site:
+                assert local_site_name is not None
                 local_site = self.get_local_site(name=local_site_name)
             else:
                 local_site = None
@@ -48,7 +133,7 @@ class ResourceListTests(BaseWebAPITestCase, metaclass=BasicTestsMetaclass):
                 HostingServiceAccount.objects.create(
                     service_name='googlecode',
                     username='bob',
-                    local_site=local_site)
+                    local_site=local_site),
             ]
         else:
             accounts = []
@@ -57,8 +142,9 @@ class ResourceListTests(BaseWebAPITestCase, metaclass=BasicTestsMetaclass):
                 hosting_service_account_list_mimetype,
                 accounts)
 
-    def test_get_with_service(self):
-        """Testing the GET hosting-service-accounts/ API with service="""
+    @webapi_test_template
+    def test_get_with_service(self) -> None:
+        """Testing the GET <URL>?service= API"""
         HostingServiceAccount.objects.create(
             service_name='googlecode',
             username='bob')
@@ -71,12 +157,15 @@ class ResourceListTests(BaseWebAPITestCase, metaclass=BasicTestsMetaclass):
             get_hosting_service_account_list_url(),
             data={'service': 'github'},
             expected_mimetype=hosting_service_account_list_mimetype)
+        assert rsp is not None
+
         self.assertEqual(rsp['stat'], 'ok')
         self.assertEqual(len(rsp['hosting_service_accounts']), 1)
         self.compare_item(rsp['hosting_service_accounts'][0], account)
 
-    def test_get_with_username(self):
-        """Testing the GET hosting-service-accounts/ API with username="""
+    @webapi_test_template
+    def test_get_with_username(self) -> None:
+        """Testing the GET <URL>?username= API"""
         account = HostingServiceAccount.objects.create(
             service_name='googlecode',
             username='bob')
@@ -89,6 +178,8 @@ class ResourceListTests(BaseWebAPITestCase, metaclass=BasicTestsMetaclass):
             get_hosting_service_account_list_url(),
             data={'username': 'bob'},
             expected_mimetype=hosting_service_account_list_mimetype)
+        assert rsp is not None
+
         self.assertEqual(rsp['stat'], 'ok')
         self.assertEqual(len(rsp['hosting_service_accounts']), 1)
         self.compare_item(rsp['hosting_service_accounts'][0], account)
@@ -97,10 +188,48 @@ class ResourceListTests(BaseWebAPITestCase, metaclass=BasicTestsMetaclass):
     # HTTP POST tests
     #
 
-    def setup_basic_post_test(self, user, with_local_site, local_site_name,
-                              post_valid_data):
+    def setup_basic_post_test(
+        self,
+        user: User,
+        with_local_site: bool,
+        local_site_name: Optional[str],
+        post_valid_data: bool,
+    ) -> tuple[str, str, JSONDict, Sequence[Any]]:
+        """Set up a basic HTTP POST test.
+
+        Args:
+            user (django.contrib.auth.models.User):
+                The user performing the API requests.
+
+            with_local_site (bool):
+                Whether to run the tests against a Local Site.
+
+            local_site_name (str):
+                The name of the Local Site to use.
+
+            post_valid_data (bool):
+                Whether to use valid data for the POST body.
+
+        Returns:
+            tuple:
+            A 4-tuple of:
+
+            Tuple:
+                0 (str):
+                    The URL to use for accessing the resource.
+
+                1 (str):
+                    The expected mimetype of the response.
+
+                2 (dict):
+                    The data to send in the POST request.
+
+                3 (list):
+                    Additional positional arguments to pass to
+                    :py:meth:`check_post_result`.
+        """
         if post_valid_data:
-            post_data = {
+            post_data: JSONDict = {
                 'username': 'bob',
                 'service_id': 'googlecode',
             }
@@ -112,27 +241,86 @@ class ResourceListTests(BaseWebAPITestCase, metaclass=BasicTestsMetaclass):
                 post_data,
                 [])
 
-    def check_post_result(self, user, rsp):
-        HostingServiceAccount.objects.get(
-            pk=rsp['hosting_service_account']['id'])
+    def check_post_result(
+        self,
+        user: User,
+        rsp: JSONDict,
+    ) -> None:
+        """Check the result of a POST operation.
+
+        Args:
+            user (django.contrib.auth.models.User):
+                The user performing the API requests.
+
+            rsp (dict):
+                The response content.
+        """
+        item_rsp = rsp['hosting_service_account']
+        account = HostingServiceAccount.objects.get(pk=item_rsp['id'])
+
+        self.compare_item(item_rsp, account)
 
 
 class ResourceItemTests(BaseWebAPITestCase, metaclass=BasicTestsMetaclass):
-    """Testing the HostingServiceAccountResource item APIs."""
+    """Unit tests for the HostingServiceAccountResource item APIs."""
+
     fixtures = ['test_users']
     sample_api_url = 'hosting-service-accounts/<id>/'
     resource = resources.hosting_service_account
 
-    compare_item = _compare_item
+    def compare_item(
+        self,
+        item_rsp: JSONDict,
+        account: HostingServiceAccount,
+    ) -> None:
+        """Compare an API response to an item.
 
-    def setup_http_not_allowed_item_test(self, user):
+        Args:
+            item_rsp (dict):
+                The API response.
+
+            account (reviewboard.hostingsvcs.models.HostingServiceAccount):
+                The account object to compare to.
+        """
+        self.assertEqual(item_rsp['id'], account.pk)
+        self.assertEqual(item_rsp['username'], account.username)
+        self.assertEqual(item_rsp['service'],
+                         account.service.hosting_service_id)
+
+    def setup_http_not_allowed_item_test(
+        self,
+        user: User,
+    ) -> str:
+        """Set up the HTTP not allowed test for item access.
+
+        Args:
+            user (django.contrib.auth.models.User):
+                The user performing the API requests.
+
+        Returns:
+            str:
+            The URL to use for accessing the resource.
+        """
         account = HostingServiceAccount.objects.create(
             service_name='googlecode',
             username='bob')
 
         return get_hosting_service_account_item_url(account.pk)
 
-    def setup_http_not_allowed_list_test(self, user):
+    def setup_http_not_allowed_list_test(
+        self,
+        user: User,
+    ) -> str:
+        """Set up the HTTP not allowed test for list access.
+
+        Args:
+            user (django.contrib.auth.models.User):
+                The user performing the API requests.
+
+        Returns:
+            str:
+            The URL to use for accessing the resource.
+        """
         account = HostingServiceAccount.objects.create(
             service_name='googlecode',
             username='bob')
@@ -143,8 +331,40 @@ class ResourceItemTests(BaseWebAPITestCase, metaclass=BasicTestsMetaclass):
     # HTTP GET tests
     #
 
-    def setup_basic_get_test(self, user, with_local_site, local_site_name):
+    def setup_basic_get_test(
+        self,
+        user: User,
+        with_local_site: bool,
+        local_site_name: Optional[str],
+    ) -> tuple[str, str, HostingServiceAccount]:
+        """Set up a basic HTTP GET test.
+
+        Args:
+            user (django.contrib.auth.models.User):
+                The user performing the API requests.
+
+            with_local_site (bool):
+                Whether to run the tests against a Local Site.
+
+            local_site_name (str):
+                The name of the Local Site to use.
+
+        Returns:
+            tuple:
+            A 3-tuple of:
+
+            Tuple:
+                0 (str):
+                    The URL to use for making API requests.
+
+                1 (str):
+                    The expected mimetype of the response.
+
+                2 (reviewboard.hostingsvcs.models.HostingServiceAccount):
+                    The hosting service account.
+        """
         if with_local_site:
+            assert local_site_name is not None
             local_site = self.get_local_site(name=local_site_name)
         else:
             local_site = None
