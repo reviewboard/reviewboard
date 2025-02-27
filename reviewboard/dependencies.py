6477b095d79603484eb880a8160259dd06b6eb9b
Christian Hammond <christian@beanbaginc.com>
2025-02-27 15:28:22 -0800

    Bump the required Djblets version to 5.3.dev.
    We now depend explicitly on the 5.3 development releases for
    Review Board 7.1.x. The Djblets development tree just recently
    had a version bump.
    
    This is a trivial change.

--- reviewboard/dependencies.py
+++ reviewboard/dependencies.py
@@ -41,7 +41,7 @@ djblets_doc_major_version = '5.x'
 django_version = '~=4.2.17'
 
 #: The version range required for Djblets.
-djblets_version = '~=5.2.0'
+djblets_version = '~=5.3.0a0.dev0'
 
 
 ###########################################################################
