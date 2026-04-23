a55a9774e97f487f3d6df7dbc6fb299970f71cff
Michelle Aubin <michelle@beanbaginc.com>
2026-04-23 10:16:57 +0700

    Fix a bug with the Review Summary column.
    In our recent datagrid updates, we gave the `ReviewSummaryColumn` a
    `link_css_class` of `review-request-link`. This is the class used for
    review request info boxes, which takes the cell's `href` target and uses
    that to create the review request info box. The `href` target for review
    summary cells is the review URL, but the review request info box
    requires a review request URL to work properly. This was causing a bug
    where the info box was populated with a poorly formatted review request
    page.
    
    Previously we didn't set the `link_css_class` for the
    `ReviewSummaryColumn`, which meant that the review request info box
    wouldn't show up when hovering over the review request summary of the
    column. This change fixes the bug and improves the behaviour, we now
    supply the review request URL to the info box through the column's
    render context and a custom template for the review summary cells.
    
    Testing Done:
    - Ran unit tests.
    - Hovered over review summary column cells, saw the bug was fixed.
    - Hovered over review request summary column cells.
    
    Reviewed at https://reviews.reviewboard.org/r/15015/

--- reviewboard/static/rb/js/ui/views/reviewRequestInfoboxView.es6.js
+++ reviewboard/static/rb/js/ui/views/reviewRequestInfoboxView.es6.js
@@ -3,6 +3,31 @@
  */
 RB.ReviewRequestInfoboxView = RB.BaseInfoboxView.extend({
     infoboxID: 'review-request-infobox',
+
+    /**
+     * Return the infobox contents URL.
+     *
+     * This will use the ``review-request-url`` data attribute if it exists on
+     * the target, otherwise this will use the ``href`` argument.
+     *
+     * Version Added:
+     *     8.0
+     *
+     * Args:
+     *     $target (jQuery):
+     *         The target element the infobox is being shown for.
+     *
+     * Returns:
+     *     string:
+     *     The URL for the contents of the infobox.
+     */
+    getURLForTarget($target) {
+        /* Review summary columns will set review-request-url. */
+        const url = $target.data('review-request-url') ||
+                    $target.attr('href');
+
+        return `${url}infobox/`;
+    },
 });
 
 
