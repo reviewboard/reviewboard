cde48fa42793dbb5651d4cf84cf79d1adb6176a3
Christian Hammond <christian@beanbaginc.com>
2026-05-27 22:18:23 -0700

    Bump to 8.0.1 dev.

--- reviewboard/_version.py
+++ reviewboard/_version.py
@@ -25,7 +25,7 @@ if TYPE_CHECKING:
 #:
 #: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #:
-VERSION: _Version = (8, 0, 0, 0, 'final', 0, True)
+VERSION: _Version = (8, 0, 1, 0, 'alpha', 0, False)
 
 
 def get_version_string() -> str:
