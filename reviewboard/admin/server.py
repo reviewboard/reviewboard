f468d2255d8294c8ab1feee4d607c56d64fe3463
Christian Hammond <christian@beanbaginc.com>
2026-08-20 22:32:40 -0700

    Add is_nfs_path() to check if a path may be in an NFS mount.
    There are certain operations that aren't considered safe on all NFS
    paths. These often involve operations where file locking is required
    or the oepration is sensitive to proper synchronization. In these cases,
    it can be helpful or important to log some information to help
    administrators if something were to act up. This is going to be true for
    the local broker.
    
    This change introduces `reviewboard.admin.server.is_nfs_path()` to help
    perform this check. This takes a path and checks to see if there's a
    known mount point that it may live in. This is a best-effort check and
    is only capable of checking Linux's `/proc/mounts`. On other sytsems (or
    if there is an issue accessing this), this will return `False`.
    
    The result is cached in an LRU cache to avoid repeated lookups for the
    same paths. In practice, this is going to be used only with very few
    hard-coded paths (such as a local queue path or parts of the data
    directory).
    
    Testing Done:
    Unit tests pass.
    
    Reviewed at https://reviews.reviewboard.org/r/15191/

--- reviewboard/admin/server.py
+++ reviewboard/admin/server.py
@@ -4,19 +4,35 @@ from __future__ import annotations
 
 import os
 import socket
+from functools import lru_cache
+from typing import TYPE_CHECKING
 from urllib.parse import urljoin
 
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import gettext as _
+from djblets.util.functional import lazy_re_compile
 from djblets.siteconfig.models import SiteConfiguration
 
 from reviewboard.site.urlresolvers import local_site_reverse
 
+if TYPE_CHECKING:
+    from re import Pattern
+    from typing import Final
+
 
 #: A cached path containing the site's data directory.
 _data_dir: (str | None) = None
+
+
+#: A regex for parsing registered Linux mount paths.
+#:
+#: Version Added:
+#:     9.0
+_MOUNT_RE: Final[Pattern[str]] = lazy_re_compile(
+    r'[^ ]+ (?P<mount>[^ ]+) (?P<fs_type>[^ ]+) .+'
+)
 
 
 def get_server_url(local_site_name=None, local_site=None, request=None):
@@ -106,3 +122,68 @@ def get_data_dir() -> str:
                   'working site directory.') % _data_dir)
 
     return _data_dir
+
+
+@lru_cache
+def is_nfs_path(
+    path: str,
+    *,
+    _mounts_path: str = '/proc/mounts',
+) -> bool:
+    """Return whether a path appears to be on an NFS mount.
+
+    This makes a best-effort check to determine if a path is an NFS mount
+    point. It does this by looking for a :file:`/proc/mounts` file and
+    parsing it to find an entry that would be a parent of the provided path.
+
+    It can be used to warn if using an NFS mount path for operations that
+    aren't always safe on NFS.
+
+    This is only expected to work on Linux systems. It cannot check on other
+    systems.
+
+    Results are cached and may be stale if mount points change in important
+    ways while the process is running.
+
+    Version Added:
+        9.0
+
+    Args:
+        path (str):
+            The path to check.
+
+        _mounts_path (str, optional):
+            The path to the mounts file.
+
+            Callers shouldn't override this. It should only be explicitly
+            set in unit tests.
+
+    Returns:
+        bool:
+        ``True`` if the path appears to be on an NFS mount, or
+        ``False`` otherwise.
+    """
+    is_nfs = False
+
+    try:
+        with open(_mounts_path, 'r') as fp:
+            norm_abs_path = os.path.realpath(path) + os.sep
+            best_match_len = 0
+
+            for line in fp:
+                if m := _MOUNT_RE.match(line):
+                    mount_point = m.group('mount')
+
+                    if (norm_abs_path.startswith(mount_point + os.sep) and
+                        (match_len := len(mount_point)) > best_match_len):
+                        # This is the best match so far. Consider it the
+                        # most relevant parent, and key off the NFS flag
+                        # from its filesystem type.
+                        best_match_len = match_len
+                        is_nfs = m.group('fs_type').startswith('nfs')
+    except OSError:
+        # We likely couldn't find it or read it. This is harmless. We will
+        # fall back to a False result.
+        pass
+
+    return is_nfs
